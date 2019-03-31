a93b1c905b7cd145ac9e8f6b2facc689acad066d
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2019-03-31 09:46:06 +0000

    Localization updated
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@1018 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/ImtCoreLoc/Translations/ImtCore_de_DE.ts
+++ Impl/ImtCoreLoc/Translations/ImtCore_de_DE.ts
@@ -1,4 +1,226 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
+<context>
+    <name>Attribute</name>
+    <message>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/ImtCoreLoc/CImtCoreLoc.cpp" line="+29"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/ImtCoreLoc/CImtCoreLoc.cpp" line="+29"/>
+        <source>Report</source>
+        <translation>Bericht</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/ImtCoreLoc/CImtCoreLoc.cpp" line="+2"/>
+        <source>Report Files</source>
+        <translation>Berichte</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/ImtCoreLoc/CImtCoreLoc.cpp" line="+4"/>
+        <source>(C) 2018 ImagingTools GmbH. All rights reserved.</source>
+        <translation>(C) 2018 ImagingToolsGmbH. Alle Rechte vorbehalten</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/ImtCoreLoc/CImtCoreLoc.cpp" line="-2"/>
+        <source>ImagingTools GmbH</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>CReportDocumentViewComp</name>
+    <message>
+        <location filename="../../../Include/imtreportgui/CReportDocumentViewComp.ui" line="+14"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtreportgui/ui_CReportDocumentViewComp.h" line="+165"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtreportgui/ui_CReportDocumentViewComp.h" line="+162"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtreportgui/ui_CReportDocumentViewComp.h" line="+165"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtreportgui/ui_CReportDocumentViewComp.h" line="+162"/>
+        <source>Main Window</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>CThumbnailDecoratorGuiComp</name>
+    <message>
+        <location filename="../../../Include/imtgui/CThumbnailDecoratorGuiComp.ui" line="+14"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+522"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+521"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+522"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+521"/>
+        <source>Main Window</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+162"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Welcome</source>
+        <translation>Willkommen</translation>
+    </message>
+    <message>
+        <location line="+79"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Log out</source>
+        <translation>Ausloggen</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Quit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+2"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+2"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+172"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+2"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+2"/>
+        <source>User</source>
+        <translation>Benutzer</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Password</source>
+        <translation>Passwort</translation>
+    </message>
+    <message>
+        <location line="+29"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Login</source>
+        <translation>Einloggen</translation>
+    </message>
+</context>
+<context>
+    <name>imtgui::CThumbnailDecoratorGuiComp</name>
+    <message>
+        <location filename="../../../Include/imtgui/CThumbnailDecoratorGuiComp.cpp" line="+206"/>
+        <location line="+213"/>
+        <source>Log-in</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location line="-157"/>
+        <source>Quit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Do you really want to quit?</source>
+        <translation>Möchten Sie die Applikation beenden?</translation>
+    </message>
+    <message>
+        <location line="+81"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Wrong password</source>
+        <translation>Falsches Passwort</translation>
+    </message>
+    <message>
+        <location line="+206"/>
+        <location line="+125"/>
+        <source>&lt;unnamed&gt;</source>
+        <translation>&lt;unbenannt&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>imtreportgui::CReportDocumentViewComp</name>
+    <message>
+        <location filename="../../../Include/imtreportgui/CReportDocumentViewComp.cpp" line="+73"/>
+        <location line="+5"/>
+        <location line="+4"/>
+        <source>Report document</source>
+        <translation>Bericht</translation>
+    </message>
+    <message>
+        <location line="+49"/>
+        <source>Export to PDF...</source>
+        <translation>PDF-Export...</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>PDF...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Export current report to PDF file</source>
+        <translation>Report in ein PDF-Dokument exportieren</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Export to TIFF...</source>
+        <translation>TIFF-Export...</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>TIFF...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Export current report to TIFF file</source>
+        <translation>Bericht in ein TIFF-Bild exportieren</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Show/Hide Grid</source>
+        <translation>Gitter zeigen/ausblenden</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Grid</source>
+        <translation>Gitter</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Set grid visible/unvisible</source>
+        <translation>Gitter zeigen/ausblenden</translation>
+    </message>
+</context>
 </TS>
--- Impl/ImtCoreLoc/Translations/ImtCore_pl_PL.ts
+++ Impl/ImtCoreLoc/Translations/ImtCore_pl_PL.ts
@@ -1,4 +1,226 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="pl_POL">
+<context>
+    <name>Attribute</name>
+    <message>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/ImtCoreLoc/CImtCoreLoc.cpp" line="+29"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/ImtCoreLoc/CImtCoreLoc.cpp" line="+29"/>
+        <source>Report</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/ImtCoreLoc/CImtCoreLoc.cpp" line="+2"/>
+        <source>Report Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/ImtCoreLoc/CImtCoreLoc.cpp" line="+4"/>
+        <source>(C) 2018 ImagingTools GmbH. All rights reserved.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/ImtCoreLoc/CImtCoreLoc.cpp" line="-2"/>
+        <source>ImagingTools GmbH</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CReportDocumentViewComp</name>
+    <message>
+        <location filename="../../../Include/imtreportgui/CReportDocumentViewComp.ui" line="+14"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtreportgui/ui_CReportDocumentViewComp.h" line="+165"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtreportgui/ui_CReportDocumentViewComp.h" line="+162"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtreportgui/ui_CReportDocumentViewComp.h" line="+165"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtreportgui/ui_CReportDocumentViewComp.h" line="+162"/>
+        <source>Main Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CThumbnailDecoratorGuiComp</name>
+    <message>
+        <location filename="../../../Include/imtgui/CThumbnailDecoratorGuiComp.ui" line="+14"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+522"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+521"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+522"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+521"/>
+        <source>Main Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+162"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Welcome</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+79"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Log out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+2"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+2"/>
+        <source>Ctrl+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+172"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+2"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+2"/>
+        <source>User</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+29"/>
+        <location filename="../../../AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC12_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <location filename="../../../Install/Temp/AuxInclude/Qt5_VC15_64/GeneratedFiles/imtgui/ui_CThumbnailDecoratorGuiComp.h" line="+1"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>imtgui::CThumbnailDecoratorGuiComp</name>
+    <message>
+        <location filename="../../../Include/imtgui/CThumbnailDecoratorGuiComp.cpp" line="+206"/>
+        <location line="+213"/>
+        <source>Log-in</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="-157"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Do you really want to quit?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+81"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Wrong password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+206"/>
+        <location line="+125"/>
+        <source>&lt;unnamed&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>imtreportgui::CReportDocumentViewComp</name>
+    <message>
+        <location filename="../../../Include/imtreportgui/CReportDocumentViewComp.cpp" line="+73"/>
+        <location line="+5"/>
+        <location line="+4"/>
+        <source>Report document</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+49"/>
+        <source>Export to PDF...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>PDF...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Export current report to PDF file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Export to TIFF...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>TIFF...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Export current report to TIFF file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Show/Hide Grid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Grid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Set grid visible/unvisible</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
