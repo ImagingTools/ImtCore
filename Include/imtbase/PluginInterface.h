d071f704a152805339b5e219e4f3bf578db5db93
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2019-10-31 09:27:04 +0000

    New plugin macros introduced.
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@1292 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtbase/PluginInterface.h
+++ Include/imtbase/PluginInterface.h
@@ -2,32 +2,43 @@
 
 
 #if defined _MSC_VER || defined __MINGW32__ || defined __MINGW64__
-	#define PLUGIN_FUNCTION_EXPORT __declspec(dllexport)
+	#define IMT_PLUGIN_FUNCTION_EXPORT __declspec(dllexport)
 #elif defined __GNUC__
-	#define PLUGIN_FUNCTION_EXPORT __attribute__ ((visibility("default")))
+	#define IMT_PLUGIN_FUNCTION_EXPORT __attribute__ ((visibility("default")))
 #else
-	#define PLUGIN_FUNCTION_EXPORT
+	#define IMT_PLUGIN_FUNCTION_EXPORT
 #endif
 
+#define IMT_MAKE_STRING(value) #value
 
 #ifndef QT_NO_DEBUG
-	#define CREATE_PLUGIN_INSTANCE_FUNCTION(suffix) CreatePluginInstanceDebug#suffix
-	#define CREATE_PLUGIN_INSTANCE_FUNCTION_NAME(suffix) "CreatePluginInstanceDebug_"##suffix
-	#define DESTROY_PLUGIN_INSTANCE_FUNCTION(suffix) DestroyPluginInstanceDebug#suffix
-	#define DESTROY_PLUGIN_INSTANCE_FUNCTION_NAME(suffix) "DestroyPluginInstanceDebug_"##suffix
+	#define IMT_CREATE_PLUGIN_INSTANCE_FUNCTION(suffix) CreatePluginInstanceDebug_##suffix
+	#define IMT_CREATE_PLUGIN_INSTANCE_FUNCTION_NAME(suffix) "CreatePluginInstanceDebug_"##IMT_MAKE_STRING(suffix)
+#define IMT_DESTROY_PLUGIN_INSTANCE_FUNCTION(suffix) DestroyPluginInstanceDebug_##suffix
+	#define IMT_DESTROY_PLUGIN_INSTANCE_FUNCTION_NAME(suffix) "DestroyPluginInstanceDebug_"##IMT_MAKE_STRING(suffix)
 #else // !QT_NO_DEBUG
-	#define CREATE_PLUGIN_INSTANCE_FUNCTION(suffix) CreatePluginInstance#suffix
-	#define CREATE_PLUGIN_INSTANCE_FUNCTION_NAME(suffix) "CreatePluginInstance_"##suffix
-	#define DESTROY_PLUGIN_INSTANCE_FUNCTION(suffix) DestroyPluginInstance#suffix
-	#define DESTROY_PLUGIN_INSTANCE_FUNCTION_NAME(suffix) "DestroyPluginInstance_"##suffix
+	#define IMT_CREATE_PLUGIN_INSTANCE_FUNCTION(suffix) CreatePluginInstance_#suffix
+	#define IMT_CREATE_PLUGIN_INSTANCE_FUNCTION_NAME(suffix) "CreatePluginInstance_"##IMT_MAKE_STRING(suffix)
+	#define IMT_DESTROY_PLUGIN_INSTANCE_FUNCTION(suffix) DestroyPluginInstance_#suffix
+	#define IMT_DESTROY_PLUGIN_INSTANCE_FUNCTION_NAME(suffix) "DestroyPluginInstance_"##IMT_MAKE_STRING(suffix)
 #endif // !QT_NO_DEBUG
 
 
-#define DECLARE_PLUGIN_INTERFACE(PluginInterface)\
-class PluginInterface;\
+#define IMT_DECLARE_PLUGIN_INTERFACE(PluginName, PluginInterface)\
 typedef PluginInterface* (*CreatePluginInstanceFunction)();\
 typedef void (*DestroyPluginInstanceFunction)(PluginInterface*);\
-extern "C" PLUGIN_FUNCTION_EXPORT PluginInterface* CREATE_PLUGIN_INSTANCE_FUNCTION();\
-extern "C" PLUGIN_FUNCTION_EXPORT void DESTROY_PLUGIN_INSTANCE_FUNCTION(PluginInterface* pluginInstancePtr);
+extern "C" IMT_PLUGIN_FUNCTION_EXPORT PluginInterface* IMT_CREATE_PLUGIN_INSTANCE_FUNCTION(PluginName);\
+extern "C" IMT_PLUGIN_FUNCTION_EXPORT void IMT_DESTROY_PLUGIN_INSTANCE_FUNCTION(PluginName)(PluginInterface* pluginInstancePtr);\
+
+
+#define IMT_REGISTER_PLUGIN(PluginInterface, PluginImplementation, PluginName)\
+	PluginInterface* IMT_CREATE_PLUGIN_INSTANCE_FUNCTION(PluginName)()\
+	{\
+		return new PluginImplementation(#PluginName);\
+	}\
+	void IMT_DESTROY_PLUGIN_INSTANCE_FUNCTION(PluginName)(PluginInterface* pluginInstancePtr)\
+	{\
+		delete pluginInstancePtr;\
+	}\
 
 
