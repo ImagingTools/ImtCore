6d799e5317fa2bf221b85e34ec03b1199f50fa8c
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2019-11-15 21:16:45 +0000

    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@1370 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Build/VC16_64/CopyDllToBin_Debug.bat
@@ -0,0 +1,1 @@
+copy /Y %OPENCVDIR_3_3_1%\bin\DebugVC16_64\*.dll ..\..\Bin\DebugVC16_64\TestPlugins
---
+++ Build/VC16_64/CopyDllToBin_Release.bat
@@ -0,0 +1,1 @@
+copy /Y %OPENCVDIR_3_3_1%\bin\ReleaseVC16_64\*.dll ..\..\Bin\ReleaseVC16_64\TestPlugins
