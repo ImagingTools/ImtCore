251ec0779042970a821139a54014cfe4578f7c9e
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2020-02-11 08:25:21 +0000

    IWidgetProvider added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@1678 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtgui/IWidgetProvider.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+// Qt includes
+#include <QtCore/QWidget>
+
+
+namespace imtgui
+{
+
+
+class IWidgetProvider: virtual public istd::IPolymorphic
+{
+public:
+	/**
+		Get information about the list of widgets in the container.
+	*/
+	virtual const imtbase::ICollectionInfo* GetWidgetList() const = 0;
+
+	/**
+		Get widget from the container according to the given ID.
+	*/
+	virtual QWidget* GetWidget(const QByteArray& widgetId) const;
+};
+
+
+} // namespace imtgui
+
+
