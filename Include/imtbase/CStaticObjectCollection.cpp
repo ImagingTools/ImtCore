b63a725b1b95774938963ada95377380272013fb
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2020-02-17 23:14:58 +0000

    Some refactoring. Static object collection added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@1741 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtbase/CStaticObjectCollection.cpp
@@ -0,0 +1,89 @@
+#include <imtbase/CStaticObjectCollection.h>
+
+
+// ACF includes
+#include <istd/TDelPtr.h>
+#include <istd/CChangeNotifier.h>
+
+
+namespace imtbase
+{
+
+
+// public methods
+
+QByteArray CStaticObjectCollection::RegisterObject(
+			const QByteArray& objectId,
+			const QByteArray& typeId,
+			const QString& name,
+			const QString& description,
+			istd::IChangeable& object)
+{
+	ObjectInfo info;
+
+	info.objectPtr.SetPtr(&object, false);
+	info.description = description;
+	info.name = name;
+	info.typeId = typeId;
+	info.flags = OF_SUPPORT_RENAME | OF_SUPPORT_EDIT;
+
+	istd::CChangeNotifier changeNotifier(this);
+
+	if (InsertObjectIntoCollection(info)){
+		changeNotifier.Abort();
+
+		return info.id;
+	}
+
+	return QByteArray();
+}
+
+
+// reimplemented (IObjectCollection)
+
+int CStaticObjectCollection::GetOperationFlags(const QByteArray & objectId) const
+{
+	return OF_SUPPORT_EDIT | OF_SUPPORT_RENAME;
+}
+
+
+QByteArray CStaticObjectCollection::InsertNewObject(
+			const QByteArray& typeId,
+			const QString& name,
+			const QString& description,
+			const istd::IChangeable* defaultValuePtr)
+{
+	return QByteArray();
+}
+
+
+// reimplemented (istd::IChangeable)
+
+int CStaticObjectCollection::GetSupportedOperations() const
+{
+	return SO_CLONE | SO_COPY | SO_RESET;
+}
+
+
+istd::IChangeable* CStaticObjectCollection::CloneMe(CompatibilityMode mode) const
+{
+	istd::TDelPtr<CStaticObjectCollection> clonePtr(new CStaticObjectCollection());
+	if (clonePtr->CopyFrom(*this, mode)){
+		return clonePtr.PopPtr();
+	}
+
+	return nullptr;
+}
+
+
+// protected methods
+
+istd::IChangeable* CStaticObjectCollection::CreateObjectInstance(const QByteArray& typeId) const
+{
+	return nullptr;
+}
+
+
+} // namespace imtbase
+
+
---
+++ Include/imtbase/CStaticObjectCollection.h
@@ -0,0 +1,48 @@
+#pragma once
+
+
+// ImtCore includes
+#include <imtbase/CObjectCollectionBase.h>
+
+
+namespace imtbase
+{
+
+
+/**
+	Implementation of a static object collection.
+	\ingroup Collection
+*/
+class CStaticObjectCollection: public CObjectCollectionBase
+{
+public:
+	typedef CObjectCollectionBase BaseClass;
+
+	virtual QByteArray RegisterObject(
+				const QByteArray& objectId,
+				const QByteArray& typeId,
+				const QString& name,
+				const QString& description,
+				istd::IChangeable& object);
+
+	// reimplemented (IObjectCollection)
+	virtual int GetOperationFlags(const QByteArray& objectId = QByteArray()) const override;
+	virtual QByteArray InsertNewObject(
+				const QByteArray& typeId,
+				const QString& name,
+				const QString& description,
+				const istd::IChangeable* defaultValuePtr = nullptr) override;
+
+	// reimplemented (istd::IChangeable)
+	virtual int GetSupportedOperations() const override;
+	virtual IChangeable* CloneMe(CompatibilityMode mode = CM_WITHOUT_REFS) const override;
+
+protected:
+	// reimplemented (CObjectCollectionBase)
+	virtual istd::IChangeable* CreateObjectInstance(const QByteArray& typeId) const;
+};
+
+
+} // namespace imtbase
+
+
