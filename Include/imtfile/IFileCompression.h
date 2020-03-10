383cdeff90322b625a852b2614ac2a5cb608c333
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2020-03-10 08:18:12 +0000

    imtfile lib added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@1853 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtfile/IFileCompression.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+// Qt includes
+#include <QtCore/QString>
+
+// ACF includes
+#include <istd/IPolymorphic.h>
+
+
+namespace imtfile
+{
+
+
+class IFileCompression: virtual public istd::IPolymorphic
+{
+public:
+	/**
+		Compress a single file.
+	*/
+	virtual bool CompressFile(const QString& sourceFilePath, const QString& targetFilePath) const = 0;
+
+	/**
+		Decompress a single file.
+	*/
+	virtual bool DescompressFile(const QString& sourceFilePath, const QString& targetFilePath) const = 0;
+
+	/**
+		Compress a folder will all files inside.
+	*/
+	virtual bool CompressFolder(const QString& sourceDirPath, const QString& targetFilePath) const = 0;
+
+	/**
+		Decompress a folder.
+	*/
+	virtual bool DecompressFolder(const QString& sourceFilePath, const QString& targetFolderPath) const = 0;
+};
+
+
+} // namespace imtfile
+
+
