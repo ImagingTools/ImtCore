3c0ebb01def999fc36ebaa29ed0407287588d206
vc <vc@21205d9f-8918-2240-a3d5-c39064a2c852>
2020-11-06 07:20:35 +0000

    Added IEventStatistics
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@2526 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtbase/IEventStatistics.h
@@ -0,0 +1,65 @@
+#pragma once
+
+
+// STL includes
+#include <vector>
+
+// Acf includes
+#include <istd/IChangeable.h>
+
+// Qt includes
+#include <QtCore/QDateTime>
+
+
+namespace imtbase
+{
+
+
+class IEventStatistics: virtual public istd::IChangeable
+{
+public:
+	struct EventsInfo
+	{
+		quint64 count;
+		quint64 oks;
+		quint64 warnings;
+		quint64 noks;
+
+		/**
+			Number of system errors (or not processed objects)
+		*/
+		quint64 errors;
+
+		EventsInfo()
+			:count(0),
+			oks(0),
+			warnings(0),
+			noks(0),
+			errors(0)
+		{
+		}
+	};
+
+	struct StatisticsItem
+	{
+		EventsInfo info;
+		qint64 time;
+	};
+
+	typedef std::vector<StatisticsItem> StatisticsTimeline;
+
+	/**
+		Get statistics item count.
+	*/
+	virtual quint64 GetStatisticsItemCount() const = 0;
+
+	/**
+		Get statistics item.
+	*/
+	virtual const StatisticsItem& GetStatisticsItem(quint64 index) const = 0;
+};
+
+
+} // namespace aculastat
+
+
