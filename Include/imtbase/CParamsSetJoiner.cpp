91dbf41edc5bae3abd41d3c91669a46b93ec94d6
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-02-01 17:18:41 +0000

    Parameter joiner added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@2675 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtbase/CParamsSetJoiner.cpp
@@ -0,0 +1,90 @@
+#include <imtbase/CParamsSetJoiner.h>
+
+
+namespace imtbase
+{
+
+
+CParamsSetJoiner::CParamsSetJoiner(iprm::IParamsSet* paramsSet1, iprm::IParamsSet* paramsSet2)
+	:m_paramsSet1(paramsSet1),
+	m_paramsSet2(paramsSet2)
+{
+}
+
+
+CParamsSetJoiner::CParamsSetJoiner(const iprm::IParamsSet* paramsSet1, const iprm::IParamsSet* paramsSet2)
+	:m_paramsSet1(const_cast<iprm::IParamsSet*>(paramsSet1)),
+	m_paramsSet2(const_cast<iprm::IParamsSet*>(paramsSet2))
+{
+}
+
+
+// reimplemented (iprm::IParamsSet)
+
+iprm::IParamsSet::Ids CParamsSetJoiner::GetParamIds(bool editableOnly) const
+{
+	iprm::IParamsSet::Ids retVal;
+	
+	if (m_paramsSet1 != nullptr){
+		retVal = m_paramsSet1->GetParamIds(editableOnly);
+	}
+
+	if (m_paramsSet2 != nullptr){
+		retVal += m_paramsSet2->GetParamIds(editableOnly);
+	}
+
+	return retVal;
+}
+
+
+const iser::ISerializable* CParamsSetJoiner::GetParameter(const QByteArray& id) const
+{
+	if (m_paramsSet1 != nullptr){
+		const iser::ISerializable* parameterPtr = m_paramsSet1->GetParameter(id);
+		if (parameterPtr != nullptr){
+			return parameterPtr;
+		}
+	}
+
+	if (m_paramsSet2 != nullptr){
+		const iser::ISerializable* parameterPtr = m_paramsSet2->GetParameter(id);
+		if (parameterPtr != nullptr){
+			return parameterPtr;
+		}
+	}
+
+	return nullptr;
+}
+
+
+iser::ISerializable* CParamsSetJoiner::GetEditableParameter(const QByteArray& id)
+{
+	if (m_paramsSet1 != nullptr){
+		iser::ISerializable* parameterPtr = m_paramsSet1->GetEditableParameter(id);
+		if (parameterPtr != nullptr){
+			return parameterPtr;
+		}
+	}
+
+	if (m_paramsSet2 != nullptr){
+		iser::ISerializable* parameterPtr = m_paramsSet2->GetEditableParameter(id);
+		if (parameterPtr != nullptr){
+			return parameterPtr;
+		}
+	}
+
+	return nullptr;
+}
+
+
+// reimplement (iser::ISerializable)
+
+bool CParamsSetJoiner::Serialize(iser::IArchive& /*archive*/)
+{
+	return false;
+}
+
+
+} // namespace imtbase
+
+
---
+++ Include/imtbase/CParamsSetJoiner.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+// ACF includes
+#include <iprm/IParamsSet.h>
+
+
+namespace imtbase
+{
+
+
+/**
+	Implementation of the multiple selection.
+*/
+class CParamsSetJoiner: virtual public iprm::IParamsSet
+{
+public:
+	CParamsSetJoiner(iprm::IParamsSet* paramsSet1, iprm::IParamsSet* paramsSet2);
+	CParamsSetJoiner(const iprm::IParamsSet* paramsSet1, const iprm::IParamsSet* paramsSet2);
+
+	// reimplemented (iprm::IParamsSet)
+	virtual Ids GetParamIds(bool editableOnly = false) const override;
+	virtual const iser::ISerializable * GetParameter(const QByteArray & id) const override;
+	virtual iser::ISerializable * GetEditableParameter(const QByteArray & id) override;
+
+	// reimplement (iser::ISerializable)
+	virtual bool Serialize(iser::IArchive& archive) override;
+
+private:
+	iprm::IParamsSet* m_paramsSet1;
+	iprm::IParamsSet* m_paramsSet2;
+};
+
+
+} // namespace imtbase
+
+
