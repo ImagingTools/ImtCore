39eec663855697524eaf0974c9f401fa69a5fa73
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-02-06 19:42:25 +0000

    CLicenseManager implements IObjectCollection interface now
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@2705 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtbase/CLicenseManager.cpp
+++ Include/imtbase/CLicenseManager.cpp
@@ -35,6 +35,125 @@ const imtbase::ILicenseInfo* CLicenseManager::GetLicenseInfo(const QByteArray& l
 }
 
 
+// reimplemented (IObjectCollection)
+
+const imtbase::IRevisionController* CLicenseManager::GetRevisionController() const
+{
+	return nullptr;
+}
+
+
+int CLicenseManager::GetOperationFlags(const QByteArray& objectId) const
+{
+	return m_licenses.GetOperationFlags(objectId);
+}
+
+
+bool CLicenseManager::GetDataMetaInfo(const QByteArray& objectId, MetaInfoPtr& metaInfoPtr) const
+{
+	return m_licenses.GetDataMetaInfo(objectId, metaInfoPtr);
+}
+
+
+QByteArray CLicenseManager::InsertNewObject(
+			const QByteArray& typeId,
+			const QString& name,
+			const QString& description,
+			const istd::IChangeable* defaultValuePtr,
+			const QByteArray& proposedObjectId)
+{
+	return m_licenses.InsertNewObject(typeId, name, description, defaultValuePtr, proposedObjectId);
+}
+
+
+bool CLicenseManager::RemoveObject(const QByteArray& objectId)
+{
+	return m_licenses.RemoveObject(objectId);
+}
+
+
+const istd::IChangeable * CLicenseManager::GetObjectPtr(const QByteArray& objectId) const
+{
+	return m_licenses.GetObjectPtr(objectId);
+}
+
+
+bool CLicenseManager::GetObjectData(const QByteArray& objectId, DataPtr& dataPtr) const
+{
+	return m_licenses.GetObjectData(objectId, dataPtr);
+}
+
+
+bool CLicenseManager::SetObjectData(const QByteArray & objectId, const istd::IChangeable& object, CompatibilityMode mode)
+{
+	return m_licenses.SetObjectData(objectId, object, mode);
+}
+
+
+void CLicenseManager::SetObjectName(const QByteArray & objectId, const QString & objectName)
+{
+	m_licenses.SetObjectName(objectId, objectName);
+}
+
+
+void CLicenseManager::SetObjectDescription(const QByteArray& objectId, const QString& objectDescription)
+{
+	m_licenses.SetObjectDescription(objectId, objectDescription);
+}
+
+
+void CLicenseManager::SetObjectEnabled(const QByteArray& objectId, bool isEnabled)
+{
+	m_licenses.SetObjectEnabled(objectId, isEnabled);
+}
+
+
+bool CLicenseManager::RegisterEventHandler(imtbase::IObjectCollectionEventHandler* eventHandler)
+{
+	return m_licenses.RegisterEventHandler(eventHandler);
+}
+
+
+bool CLicenseManager::UnregisterEventHandler(imtbase::IObjectCollectionEventHandler* eventHandler)
+{
+	return m_licenses.UnregisterEventHandler(eventHandler);
+}
+
+
+// reimplemented (IObjectCollectionInfo)
+
+bool CLicenseManager::GetCollectionItemMetaInfo(const QByteArray& objectId, idoc::IDocumentMetaInfo& metaInfo) const
+{
+	return m_licenses.GetCollectionItemMetaInfo(objectId, metaInfo);
+}
+
+
+const iprm::IOptionsList* CLicenseManager::GetObjectTypesInfo() const
+{
+	return m_licenses.GetObjectTypesInfo();
+}
+
+
+ICollectionInfo::Id CLicenseManager::GetObjectTypeId(const QByteArray & objectId) const
+{
+	return m_licenses.GetObjectTypeId(objectId);
+}
+
+
+// reimplemented (ICollectionInfo)
+
+ICollectionInfo::Ids CLicenseManager::GetElementIds() const
+{
+	return m_licenses.GetElementIds();
+}
+
+
+QVariant CLicenseManager::GetElementInfo(const QByteArray& elementId, int infoType) const
+{
+	return m_licenses.GetElementInfo(elementId, infoType);
+}
+
+
 // reimplemented (iser::ISerializable)
 
 bool CLicenseManager::Serialize(iser::IArchive& archive)
--- Include/imtbase/CLicenseManager.h
+++ Include/imtbase/CLicenseManager.h
@@ -16,10 +16,11 @@ namespace imtbase
 
 
 /**
-	Common status manager implementation.
+	License manager implementation.
 */
 class CLicenseManager:
 			virtual public imtbase::ILicenseInfoProvider,
+			virtual public imtbase::IObjectCollection,
 			virtual public iser::ISerializable
 {
 public:
@@ -28,6 +29,35 @@ public:
 	// reimplemented (imtbase::ILicenseInfoProvider)
 	virtual const imtbase::ICollectionInfo& GetLicenseList() const override;
 	virtual const imtbase::ILicenseInfo* GetLicenseInfo(const QByteArray & licenseId) const override;
+
+	// reimplemented (IObjectCollection)
+	virtual const imtbase::IRevisionController* GetRevisionController() const override;
+	virtual int GetOperationFlags(const QByteArray& objectId = QByteArray()) const override;
+	virtual bool GetDataMetaInfo(const QByteArray& objectId, MetaInfoPtr& metaInfoPtr) const override;
+	virtual QByteArray InsertNewObject(
+				const QByteArray& typeId,
+				const QString& name,
+				const QString& description,
+				const istd::IChangeable* defaultValuePtr = nullptr,
+				const QByteArray& proposedObjectId = QByteArray()) override;
+	virtual bool RemoveObject(const QByteArray& objectId) override;
+	virtual const istd::IChangeable* GetObjectPtr(const QByteArray& objectId) const override;
+	virtual bool GetObjectData( const QByteArray& objectId, DataPtr& dataPtr) const override;
+	virtual bool SetObjectData( const QByteArray& objectId, const istd::IChangeable& object, CompatibilityMode mode = CM_WITHOUT_REFS) override;
+	virtual void SetObjectName(const QByteArray& objectId, const QString& objectName) override;
+	virtual void SetObjectDescription(const QByteArray& objectId, const QString& objectDescription) override;
+	virtual void SetObjectEnabled(const QByteArray& objectId, bool isEnabled = true) override;
+	virtual bool RegisterEventHandler(imtbase::IObjectCollectionEventHandler* eventHandler) override;
+	virtual bool UnregisterEventHandler(imtbase::IObjectCollectionEventHandler* eventHandler) override;
+
+	// reimplemented (IObjectCollectionInfo)
+	virtual bool GetCollectionItemMetaInfo(const QByteArray& objectId, idoc::IDocumentMetaInfo& metaInfo) const override;
+	virtual const iprm::IOptionsList* GetObjectTypesInfo() const override;
+	virtual Id GetObjectTypeId(const QByteArray& objectId) const override;
+
+	// reimplemented (ICollectionInfo)
+	virtual Ids GetElementIds() const override;
+	virtual QVariant GetElementInfo(const QByteArray& elementId, int infoType) const override;
 
 	// reimplemented (iser::ISerializable)
 	virtual bool Serialize(iser::IArchive& archive) override;
