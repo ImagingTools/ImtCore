eb9e48e86a6c66dde070e25c40ec5c42b71345c1
vc <vc@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-02-26 18:11:12 +0000

    Continue working on Feature Collection Gui
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@2829 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtlicgui/CFeatureCollectionGuiComp.cpp
+++ Include/imtlicgui/CFeatureCollectionGuiComp.cpp
@@ -16,24 +16,59 @@ namespace imtlicgui
 // public methods
 
 CFeatureCollectionGuiComp::CFeatureCollectionGuiComp()
+	:m_showCollectionEditorCommand("Show Collection Editor", 100, ibase::ICommand::CF_GLOBAL_MENU | ibase::ICommand::CF_TOOLBAR | ibase::ICommand::CF_EXCLUSIVE, 1977),
+	m_showDependenciesEditorCommand("Show Feature Dependency Editor", 100, ibase::ICommand::CF_GLOBAL_MENU | ibase::ICommand::CF_TOOLBAR | ibase::ICommand::CF_EXCLUSIVE, 1977)
 {
-}
+	m_rootCommands.InsertChild(&m_showCollectionEditorCommand);
+	m_rootCommands.InsertChild(&m_showDependenciesEditorCommand);
 
-
-// reimplemented (ibase::ICommandsProvider)
-
-const ibase::IHierarchicalCommand* CFeatureCollectionGuiComp::GetCommands() const
-{
-	//ibase::ICommandsProvider* commandsProviderPtr = dynamic_cast<ibase::ICommandsProvider*>(m_licenseCollectionGuiCompPtr.GetPtr());
-	//if (commandsProviderPtr != nullptr){
-	//	commandsProviderPtr->GetCommands();
-	//}
-	
-	return nullptr;
+	connect(&m_showCollectionEditorCommand, &QAction::triggered, this, &CFeatureCollectionGuiComp::OnShowCollectionEditor);
+	connect(&m_showDependenciesEditorCommand, &QAction::triggered, this, &CFeatureCollectionGuiComp::OnShowFeatureDependencyEditor);
 }
 
 
 // protected methods
+
+// reimplemented(ibase::ICommandsProvider)
+
+const ibase::IHierarchicalCommand* CFeatureCollectionGuiComp::GetCommands() const
+{
+	return &m_rootCommands;
+}
+
+
+// reimplemented (iqtgui::CGuiComponentBase)
+
+void CFeatureCollectionGuiComp::OnGuiCreated()
+{
+	BaseClass::OnGuiCreated();
+
+	if (m_objectCollectionViewCompPtr.IsValid()){
+		m_objectCollectionViewCompPtr->CreateGui(objectCollectionView);
+	}
+}
+
+
+void CFeatureCollectionGuiComp::OnGuiDestroyed()
+{
+	if (m_objectCollectionViewCompPtr.IsValid() && m_objectCollectionViewCompPtr->IsGuiCreated()){
+		m_objectCollectionViewCompPtr->DestroyGui();
+	}
+
+	BaseClass::OnGuiDestroyed();
+}
+
+
+void CFeatureCollectionGuiComp::OnGuiRetranslate()
+{
+	BaseClass::OnGuiRetranslate();
+
+	m_showCollectionEditorCommand.SetVisuals(tr("Show Collection Editor"), tr("Collection Editor"), tr("Show Collection Editor"), QIcon(":/Icons/Lamp"));
+	m_showDependenciesEditorCommand.SetVisuals(tr("Show Feature Dependency Editor"), tr("Dependency Editor"), tr("Show Feature Dependency Editor"), QIcon(":/Icons/Lamp"));
+
+	m_showCollectionEditorCommand.setChecked(true);
+}
+
 
 // reimplemented (iqtgui::TGuiObserverWrap)
 
@@ -80,6 +115,20 @@ void CFeatureCollectionGuiComp::UpdateModel() const
 {
 	//imtlic::IProductLicensingInfo* licenseInfoPtr = GetObservedObject();
 	//Q_ASSERT(licenseInfoPtr != nullptr);
+}
+
+
+// protected slots
+
+void CFeatureCollectionGuiComp::OnShowCollectionEditor()
+{
+	pages->setCurrentIndex(0);
+}
+
+
+void CFeatureCollectionGuiComp::OnShowFeatureDependencyEditor()
+{
+	pages->setCurrentIndex(1);
 }
 
 
--- Include/imtlicgui/CFeatureCollectionGuiComp.h
+++ Include/imtlicgui/CFeatureCollectionGuiComp.h
@@ -4,10 +4,11 @@
 // ACF includes
 #include <ibase/ICommandsProvider.h>
 #include <iqtgui/TDesignerGuiObserverCompBase.h>
+#include <iqtgui/CHierarchicalCommand.h>
 
 // ImtCore includes
 #include <imtlic/IFeatureInfoProvider.h>
-#include <GeneratedFiles/imtlicgui/ui_CFeatureInfoProviderGuiComp.h>
+#include <GeneratedFiles/imtlicgui/ui_CFeatureCollectionGuiComp.h>
 
 
 namespace imtlicgui
@@ -24,26 +25,46 @@ class CFeatureCollectionGuiComp:
 			virtual public ibase::ICommandsProvider
 {
 	Q_OBJECT
-
 public:
 	typedef iqtgui::TDesignerGuiObserverCompBase<
 				Ui::CFeatureCollectionGuiComp, imtlic::IFeatureInfoProvider> BaseClass;
 
 	I_BEGIN_COMPONENT(CFeatureCollectionGuiComp);
-		I_REGISTER_INTERFACE(ibase::ICommandsProvider)
+		I_REGISTER_INTERFACE(ibase::ICommandsProvider);
+		I_ASSIGN(m_objectCollectionViewCompPtr, "ObjectCollectionView", "Object collection view", true, "ObjectCollectionView");
 	I_END_COMPONENT;
 
 	CFeatureCollectionGuiComp();
 
+protected:
 	// reimplemented (ibase::ICommandsProvider)
 	virtual const ibase::IHierarchicalCommand* GetCommands() const override;
 
-protected:
+	// reimplemented (iqtgui::CGuiComponentBase)
+	virtual void OnGuiCreated();
+	virtual void OnGuiDestroyed();
+	virtual void OnGuiRetranslate() override;
+
 	// reimplemented (iqtgui::TGuiObserverWrap)
 	virtual void UpdateGui(const istd::IChangeable::ChangeSet& changeSet) override;
 	virtual void OnGuiModelAttached() override;
 	virtual void OnGuiModelDetached() override;
 	virtual void UpdateModel() const;
+
+protected Q_SLOTS:
+	virtual void OnShowCollectionEditor();
+	virtual void OnShowFeatureDependencyEditor();
+
+protected:
+	iqtgui::CHierarchicalCommand m_rootCommands;
+	iqtgui::CHierarchicalCommand m_showCollectionEditorCommand;
+	iqtgui::CHierarchicalCommand m_showDependenciesEditorCommand;
+
+private:
+	void UpdateCommands();
+
+private:
+	I_REF(iqtgui::IGuiObject, m_objectCollectionViewCompPtr);
 };
 
 
