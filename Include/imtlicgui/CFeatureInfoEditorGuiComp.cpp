1c5ba799d5c61262da8c76b05a1d8c57dc28087f
vc <vc@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-03-09 13:05:18 +0000

    Remove dependencies from feature editor
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@2884 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtlicgui/CFeatureInfoEditorGuiComp.cpp
+++ Include/imtlicgui/CFeatureInfoEditorGuiComp.cpp
@@ -12,26 +12,7 @@ namespace imtlicgui
 {
 
 
-// public methods
-
-CFeatureInfoEditorGuiComp::CFeatureInfoEditorGuiComp()
-{
-}
-
-
 // protected methods
-
-void CFeatureInfoEditorGuiComp::UpdateFeaturePackageModel()
-{
-	DoUpdateModel();
-}
-
-
-void CFeatureInfoEditorGuiComp::FeatureTreeItemChanged()
-{
-	Q_EMIT EmitFeatureTreeItemChanged();
-}
-
 
 // reimplemented (iqtgui::TGuiObserverWrap)
 
@@ -42,97 +23,6 @@ void CFeatureInfoEditorGuiComp::UpdateGui(const istd::IChangeable::ChangeSet& /*
 
 	IdEdit->setText(featureInfoPtr->GetFeatureId());
 	NameEdit->setText(featureInfoPtr->GetFeatureName());
-
-	m_selectedFeatureId = featureInfoPtr->GetFeatureId();
-
-	m_features.clear();
-	m_dependencies.clear();
-
-	const imtlic::IFeatureInfoProvider* featureInfoProviderPtr = GetObservedObject()->GetFeaturePackage();
-	if (featureInfoProviderPtr != nullptr){
-		imtbase::ICollectionInfo::Ids ids = featureInfoProviderPtr->GetFeatureList().GetElementIds();
-
-		for (const QByteArray& id : ids){
-			const imtlic::IFeatureDependenciesProvider* dependenciesProviderPtr = featureInfoProviderPtr->GetDependenciesInfoProvider();
-			const imtlic::IFeatureInfo* featureInfoPtr = featureInfoProviderPtr->GetFeatureInfo(id);
-
-			if (featureInfoPtr != nullptr && dependenciesProviderPtr != nullptr){
-				QByteArray featureId = featureInfoPtr->GetFeatureId();
-				m_dependencies[featureId] = dependenciesProviderPtr->GetFeatureDependencies(featureId);
-
-				FeatureDescription desc;
-				desc.name = featureInfoPtr->GetFeatureName();
-				desc.id = featureId;
-				m_features.append(desc);
-			}
-		}
-	}
-
-	//m_dependencies.clear();
-
-	//const imtlic::IFeatureInfoProvider* featureInfoProviderPtr = GetObservedObject()->GetFeaturePackage();
-	//if (featureInfoProviderPtr != nullptr){
-	//	const imtlic::IFeatureDependenciesProvider* dependenciesProviderPtr = featureInfoProviderPtr->GetDependenciesInfoProvider();
-	//	if (dependenciesProviderPtr != nullptr){
-	//		m_dependencies[m_selectedFeatureId] = dependenciesProviderPtr->GetFeatureDependencies(m_selectedFeatureId);
-	//	}
-	//}
-
-	m_isGuiModelInitialized = true;
-
-	if (m_isGuiModelInitialized && m_isCollectionRepresentationInitialized){
-		EnumerateMissingDependencies();
-		UpdateFeatureTree();
-		UpdateFeatureTreeCheckStates();
-		DoUpdateModel();
-	}
-}
-
-
-void CFeatureInfoEditorGuiComp::OnGuiModelAttached()
-{
-	BaseClass::OnGuiModelAttached();
-
-	m_isGuiModelInitialized = false;
-	m_isCollectionRepresentationInitialized = false;
-
-	m_treeWidgetPtr = FeatureTree;
-
-	const imtlic::IFeatureInfoProvider* featurePackagePtr = dynamic_cast<const imtlic::IFeatureInfoProvider*>(
-				GetObservedObject()->GetFeaturePackage());
-
-	if (featurePackagePtr != nullptr){
-		imod::IModel* featurePackageCollectionModelPtr = dynamic_cast<imod::IModel*>(
-					const_cast<imtbase::IObjectCollection*>(featurePackagePtr->GetFeaturePackages()));
-
-		if (featurePackageCollectionModelPtr != nullptr){
-			featurePackageCollectionModelPtr->AttachObserver(&m_packageCollectionObserver);
-		}
-	}
-
-	connect(this, &CFeatureInfoEditorGuiComp::EmitFeatureTreeItemChanged, this, &CFeatureInfoEditorGuiComp::OnFeatureTreeItemChanged, Qt::QueuedConnection);
-}
-
-
-void CFeatureInfoEditorGuiComp::OnGuiModelDetached()
-{
-	disconnect(this, &CFeatureInfoEditorGuiComp::EmitFeatureTreeItemChanged, this, &CFeatureInfoEditorGuiComp::OnFeatureTreeItemChanged);
-
-	const imtlic::IFeatureInfoProvider* featurePackagePtr = dynamic_cast<const imtlic::IFeatureInfoProvider*>(
-			GetObservedObject()->GetFeaturePackage());
-
-	if (featurePackagePtr != nullptr){
-		imod::IModel* featurePackageCollectionModelPtr = dynamic_cast<imod::IModel*>(
-					const_cast<imtbase::IObjectCollection*>(featurePackagePtr->GetFeaturePackages()));
-
-		if (featurePackageCollectionModelPtr != nullptr){
-			if (featurePackageCollectionModelPtr->IsAttached(&m_packageCollectionObserver)){
-				featurePackageCollectionModelPtr->DetachObserver(&m_packageCollectionObserver);
-			}
-		}
-	}
-
-	BaseClass::OnGuiModelDetached();
 }
 
 
@@ -142,32 +32,14 @@ void CFeatureInfoEditorGuiComp::UpdateModel() const
 	Q_ASSERT(featureInfoPtr != nullptr);
 
 	imtlic::CFeatureInfo featureInfo;
-
 	featureInfo.SetFeatureId(IdEdit->text().toUtf8());
 	featureInfo.SetFeatureName(NameEdit->text());
 
 	featureInfoPtr->CopyFrom(featureInfo);
-
-	const imtlic::IFeatureInfoProvider* featureInfoProviderPtr = GetObservedObject()->GetFeaturePackage();
-	if (featureInfoProviderPtr != nullptr){
-		imtlic::IFeatureDependenciesManager* dependenciesManagerPtr = dynamic_cast<imtlic::IFeatureDependenciesManager*>(
-					const_cast<imtlic::IFeatureDependenciesProvider*>(
-								featureInfoProviderPtr->GetDependenciesInfoProvider()));
-
-		if (dependenciesManagerPtr != nullptr){
-			dependenciesManagerPtr->SetFeatureDependencies(m_selectedFeatureId, m_dependencies[m_selectedFeatureId]);
-		}
-	}
 }
 
 
 // private slots
-
-void CFeatureInfoEditorGuiComp::on_FeatureTree_itemChanged(QTreeWidgetItem *item, int column)
-{
-	BaseClass2::on_FeatureTree_itemChanged(item, column);
-}
-
 
 void CFeatureInfoEditorGuiComp::on_NameEdit_editingFinished()
 {
@@ -178,17 +50,6 @@ void CFeatureInfoEditorGuiComp::on_NameEdit_editingFinished()
 void CFeatureInfoEditorGuiComp::on_IdEdit_editingFinished()
 {
 	DoUpdateModel();
-}
-
-
-void CFeatureInfoEditorGuiComp::OnFeatureTreeItemChanged()
-{
-	if (m_isGuiModelInitialized && m_isCollectionRepresentationInitialized){
-		EnumerateMissingDependencies();
-		UpdateFeatureTree();
-		UpdateFeatureTreeCheckStates();
-		DoUpdateModel();
-	}
 }
 
 
--- Include/imtlicgui/CFeatureInfoEditorGuiComp.h
+++ Include/imtlicgui/CFeatureInfoEditorGuiComp.h
@@ -6,7 +6,6 @@
 
 // ImtCore includes
 #include <imtlic/IFeatureInfo.h>
-#include <imtlicgui/CFeatureDependencyEditorBase.h>
 #include <GeneratedFiles/imtlicgui/ui_CFeatureInfoEditorGuiComp.h>
 
 
@@ -20,38 +19,24 @@ namespace imtlicgui
 */
 class CFeatureInfoEditorGuiComp:
 			public iqtgui::TDesignerGuiObserverCompBase<
-						Ui::CFeatureInfoEditorGuiComp, imtlic::IFeatureInfo>,
-			protected CFeatureDependencyEditorBase
+						Ui::CFeatureInfoEditorGuiComp, imtlic::IFeatureInfo>
 {
 	Q_OBJECT
 public:
 	typedef iqtgui::TDesignerGuiObserverCompBase<
 				Ui::CFeatureInfoEditorGuiComp, imtlic::IFeatureInfo> BaseClass;
-	typedef CFeatureDependencyEditorBase BaseClass2;
 
 	I_BEGIN_COMPONENT(CFeatureInfoEditorGuiComp);
 	I_END_COMPONENT;
 
-	CFeatureInfoEditorGuiComp();
-
-Q_SIGNALS:
-	void EmitFeatureTreeItemChanged();
-
 protected:
-	virtual void UpdateFeaturePackageModel() override;
-	virtual void FeatureTreeItemChanged() override;
-
 	// reimplemented (iqtgui::TGuiObserverWrap)
 	virtual void UpdateGui(const istd::IChangeable::ChangeSet& changeSet) override;
-	virtual void OnGuiModelAttached() override;
-	virtual void OnGuiModelDetached() override;
 	virtual void UpdateModel() const;
 
 private Q_SLOTS:
-	void on_FeatureTree_itemChanged(QTreeWidgetItem *item, int column);
 	void on_NameEdit_editingFinished();
 	void on_IdEdit_editingFinished();
-	virtual void OnFeatureTreeItemChanged();
 };
 
 
