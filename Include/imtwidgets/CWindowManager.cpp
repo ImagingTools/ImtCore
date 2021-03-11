2f368fe4c12e19083b27d3d0521804106ff4c474
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-03-11 08:38:08 +0000

    Window manager added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@2898 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtwidgets/CWindowManager.cpp
@@ -0,0 +1,175 @@
+#include <imtwidgets/CWindowManager.h>
+
+
+#ifdef Q_OS_WINDOWS
+	#include <windows.h>
+	#include <psapi.h>
+#endif
+
+
+#ifdef Q_OS_WINDOWS
+BOOL CALLBACK FindHandleCallback(HWND windowHandle, LPARAM pParam)
+{
+	static const int maxBufferSize = 256;
+
+	CWindowManager* windowManagerPtr = (CWindowManager*)pParam;
+	char name[maxBufferSize];
+
+	if (windowManagerPtr != nullptr){
+		QString title = windowManagerPtr->GetTitle();
+
+		GetWindowTextA(windowHandle, name, maxBufferSize - 1);
+
+		QString windowName(name);
+		if (title.compare(windowName, Qt::CaseInsensitive) == 0){
+			windowManagerPtr->SetHandle(windowHandle);
+
+			return FALSE;
+		}
+	}
+
+	return TRUE;
+}
+#endif
+
+
+CWindowManager::CWindowManager() 
+	:m_handle(nullptr)
+{
+}
+
+
+qint64 CWindowManager::FindProcessIdByTitle(const QString& title)
+{
+	qint64 foundProcessId = 0;
+
+#ifdef Q_OS_WINDOWS
+	DWORD processList[1024];
+	DWORD bytes = 0;
+
+	if (EnumProcesses(processList, sizeof(processList), &bytes)){
+		int count = bytes / sizeof(bytes);
+
+		for (int index = 0; index < count; ++index){
+			DWORD processId = processList[index];
+			if (processId != 0){
+				HANDLE processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
+				if (processHandle){
+					HMODULE moduleHandle;
+					DWORD currentBytes = 0;
+
+					if (EnumProcessModules(processHandle, &moduleHandle, sizeof(moduleHandle), &currentBytes)){
+						char process[MAX_PATH] = "";
+						GetModuleBaseNameA(processHandle, moduleHandle, process, sizeof(process) / sizeof(TCHAR));
+
+						QString processName(process);
+						if (processName.compare(title, Qt::CaseInsensitive) == 0){
+							CloseHandle(processHandle);
+
+							foundProcessId = processId;
+
+							break;
+						}
+					}
+
+					CloseHandle(processHandle);
+				}
+			}
+		}
+	}
+
+#endif // Q_OS_WINDOWS
+
+	return foundProcessId;
+}
+
+
+Qt::HANDLE CWindowManager::FindHandleByProcessId(qint64 processId)
+{
+	Qt::HANDLE handle = 0;
+
+#ifdef Q_OS_WINDOWS
+	HWND topWindowHandle = ::GetTopWindow(nullptr);
+
+	while(topWindowHandle != nullptr){
+		DWORD currentProcessId = 0;
+		::GetWindowThreadProcessId(topWindowHandle, &currentProcessId);
+
+		if (processId == currentProcessId){
+			HWND parentWindowHandle = ::GetParent(topWindowHandle);
+			HWND lastWindowHandle = topWindowHandle;
+
+			while(topWindowHandle != nullptr){
+				lastWindowHandle = parentWindowHandle;
+
+				parentWindowHandle = ::GetParent(lastWindowHandle);
+			}
+
+			if (::GetWindowLong(lastWindowHandle, GWL_ID) != 0){
+				handle = lastWindowHandle;
+
+				break;
+			}
+		}
+
+		topWindowHandle = ::GetNextWindow(topWindowHandle, GW_HWNDNEXT);
+	}
+#endif
+
+	return handle;
+}
+
+
+Qt::HANDLE CWindowManager::FindHandleByTitle(const QString& title)
+{
+	Qt::HANDLE handle = nullptr;
+
+#ifdef Q_OS_WINDOWS
+	m_title = title;
+
+	if (!EnumWindows((WNDENUMPROC)FindHandleCallback, (LPARAM)this)){
+		handle = m_handle;
+	}
+
+#endif
+
+	return handle;
+}
+
+
+void CWindowManager::RaiseWindowByProcessId(qint64 processId)
+{
+#ifdef Q_OS_WINDOWS
+	HWND windowHandle = (HWND)FindHandleByProcessId(processId);
+	if (windowHandle){
+		::SetForegroundWindow(windowHandle);
+
+		if (::IsIconic(windowHandle)){
+			::ShowWindow(windowHandle, SW_RESTORE);
+		}
+	}
+#endif
+}
+
+
+void CWindowManager::RaiseWindowByTitle(const QString& title)
+{
+#ifdef Q_OS_WINDOWS
+	HWND mainWindowHandle = (HWND)FindHandleByTitle(title);
+	HWND lastActiveWindowHandle = mainWindowHandle;
+
+	if (mainWindowHandle != nullptr){
+		lastActiveWindowHandle = ::GetLastActivePopup(mainWindowHandle);
+	}
+
+	if (lastActiveWindowHandle != nullptr){
+		::SetForegroundWindow(lastActiveWindowHandle);
+
+		if (::IsIconic(lastActiveWindowHandle)){
+			::ShowWindow(lastActiveWindowHandle, SW_RESTORE);
+		}
+	}
+#endif
+}
+
+
---
+++ Include/imtwidgets/CWindowManager.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+// Qt includes
+#include <QtCore/QString>
+#include <QtCore/QMutex>
+
+
+class CWindowManager
+{
+public:
+	CWindowManager();
+
+	qint64 FindProcessIdByTitle(const QString& title);
+	void RaiseWindowByProcessId(qint64 processId);
+	void RaiseWindowByTitle(const QString& title);
+
+	Qt::HANDLE GetHandle() {return m_handle;}
+	void SetHandle(Qt::HANDLE handle) {m_handle = handle;}
+	QString GetTitle() {return m_title;}
+	void SetTitle(QString title) {m_title = title;}
+
+protected:
+	Qt::HANDLE FindHandleByProcessId(qint64 processId);
+	Qt::HANDLE FindHandleByTitle(const QString& title);
+
+private:
+	Qt::HANDLE m_handle;
+	QString m_title;
+};
+
+
