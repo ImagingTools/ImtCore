503183d51b0837c54c136c05971a05ed15413ffe
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-03-22 20:40:43 +0000

    Code cleanup
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@2975 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtrest/CHttpFileBasedHandlerComp.cpp
+++ Include/imtrest/CHttpFileBasedHandlerComp.cpp
@@ -23,50 +23,51 @@ namespace imtrest
 bool CHttpFileBasedHandlerComp::ProcessRequest(const IRequest& request) const
 {
 	const IProtocolEngine& engine = request.GetProtocolEngine();
-    QByteArray errorBody = "<html><head><title>Error</title></head><body><p>The requested command could not be executed</p></body></html>";
-    QByteArray reponseTypeId = QByteArray("text/html; charset=utf-8");
+	QByteArray errorBody = "<html><head><title>Error</title></head><body><p>File resource was not found</p></body></html>";
+	QByteArray reponseTypeId = QByteArray("text/html; charset=utf-8");
 
-    istd::TDelPtr<IResponse> errorResponsePtr(engine.CreateResponse(request, IProtocolEngine::SC_RESOURCE_NOT_AVAILABLE, errorBody, reponseTypeId));
+	istd::TDelPtr<IResponse> errorResponsePtr(engine.CreateResponse(request, IProtocolEngine::SC_RESOURCE_NOT_AVAILABLE, errorBody, reponseTypeId));
 
-    if (!m_fileTemplatePathCompPtr.IsValid())
-    {
-        engine.GetResponder().SendResponse(*errorResponsePtr);
-        return false;
-    }
-    
-    QString templateFilePath = m_fileTemplatePathCompPtr->GetPath();
-    
-    if (templateFilePath.isEmpty())
-    {
-        engine.GetResponder().SendResponse(*errorResponsePtr);
-        return false;
-    }
-    
-    QFile templateFile(templateFilePath);
+	if (!m_fileTemplatePathCompPtr.IsValid()){
+		engine.GetResponder().SendResponse(*errorResponsePtr);
 
-    if (!templateFile.open(QIODevice::ReadOnly))
-    {
-        engine.GetResponder().SendResponse(*errorResponsePtr);
-        return false;
-    }
+		return false;
+	}
 
-    QByteArray body = templateFile.readAll();
+	QString templateFilePath = m_fileTemplatePathCompPtr->GetPath();
 
+	if (templateFilePath.isEmpty()){
+		engine.GetResponder().SendResponse(*errorResponsePtr);
 
-    istd::TDelPtr<IResponse> responsePtr(engine.CreateResponse(request, IProtocolEngine::SC_OK, body, reponseTypeId));
-    if (!responsePtr.IsValid())
-    {
-        return false;
-    }
-     engine.GetResponder().SendResponse(*responsePtr);
-     return true;
+		return false;
+	}
+
+	QFile templateFile(templateFilePath);
+
+	if (!templateFile.open(QIODevice::ReadOnly)){
+		engine.GetResponder().SendResponse(*errorResponsePtr);
+
+		return false;
+	}
+
+	QByteArray body = templateFile.readAll();
+
+	istd::TDelPtr<IResponse> responsePtr(engine.CreateResponse(request, IProtocolEngine::SC_OK, body, reponseTypeId));
+	if (!responsePtr.IsValid()){
+		return false;
+	}
+
+	engine.GetResponder().SendResponse(*responsePtr);
+
+	return true;
 }
 
 
 QByteArray CHttpFileBasedHandlerComp::GetSupportedCommandId() const
 {
-    return *m_commandIdAttrPtr;
+	return *m_commandIdAttrPtr;
 }
+
 
 } // namespace imtrest
 
--- Include/imtrest/CHttpFileBasedHandlerComp.h
+++ Include/imtrest/CHttpFileBasedHandlerComp.h
@@ -1,9 +1,6 @@
 #pragma once
 
 
-// Qt includes
-#include <QtCore/QMutex>
-
 // ACF includes
 #include <ilog/TLoggerCompWrap.h>
 #include <ifile/IFileNameParam.h>
@@ -23,25 +20,19 @@ class CHttpFileBasedHandlerComp:
 public:
 	typedef ilog::CLoggerComponentBase BaseClass;
 
-    I_BEGIN_COMPONENT(CHttpFileBasedHandlerComp);
+	I_BEGIN_COMPONENT(CHttpFileBasedHandlerComp);
 		I_REGISTER_INTERFACE(IRequestHandler);
-        I_ASSIGN(m_commandIdAttrPtr, "CommandId", "List of command-ID used with corresponded handlers", true, "");
-        I_ASSIGN(m_fileTemplatePathCompPtr, "FileTemplatePath", "Comment", true, "FileTemplatePath");
+		I_ASSIGN(m_commandIdAttrPtr, "CommandId", "List of command-ID used with corresponded handlers", true, "");
+		I_ASSIGN(m_fileTemplatePathCompPtr, "FileTemplatePath", "Comment", true, "FileTemplatePath");
 	I_END_COMPONENT
 
 	// reimplemented (IRequestHandler)
 	virtual bool ProcessRequest(const IRequest& request) const override;
-    virtual QByteArray GetSupportedCommandId() const override;
-
+	virtual QByteArray GetSupportedCommandId() const override;
 
 private:
-    I_ATTR(QByteArray, m_commandIdAttrPtr);
-    I_REF(ifile::IFileNameParam, m_fileTemplatePathCompPtr);
-
-	typedef QMap<QString, IRequestHandler*> HandlersMap;
-
-	HandlersMap m_handlersMap;
-	QMutex m_handlersMapMutex;
+	I_ATTR(QByteArray, m_commandIdAttrPtr);
+	I_REF(ifile::IFileNameParam, m_fileTemplatePathCompPtr);
 };
 
 
