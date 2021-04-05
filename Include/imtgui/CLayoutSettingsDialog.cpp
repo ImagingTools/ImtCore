0f6823e6472e3ddf8cb96d2ed33e1f2e28877ea8
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-04-05 07:17:37 +0000

    Cosmetics
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3028 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtgui/CLayoutSettingsDialog.cpp
+++ Include/imtgui/CLayoutSettingsDialog.cpp
@@ -26,22 +26,31 @@ void CLayoutSettingsDialog::SetLayoutProperties(const ILayout::LayoutProperties 
 {
 	FixedLayouts->setChecked(properties.isFixedLayout);
 	BorderEnabled->setChecked(properties.isBorderEnabled);
+
 	SetBorderColor(properties.borderColor);
+
 	QString string;
 	string.setNum(properties.minWidth);
 	MinWidth->setText(string);
+
 	string.setNum(properties.maxWidth);
 	MaxWidth->setText(string);
+
 	string.setNum(properties.minHeight);
 	MinHeight->setText(string);
+
 	string.setNum(properties.maxHeight);
 	MaxHeight->setText(string);
+
 	string.setNum(properties.leftMargin);
 	LeftMargin->setText(string);
+
 	string.setNum(properties.rightMargin);
 	RightMargin->setText(string);
+
 	string.setNum(properties.topMargin);
 	TopMargin->setText(string);
+
 	string.setNum(properties.bottomMargin);
 	BottomMargin->setText(string);
 }
@@ -50,6 +59,7 @@ void CLayoutSettingsDialog::SetLayoutProperties(const ILayout::LayoutProperties 
 ILayout::LayoutProperties CLayoutSettingsDialog::GetLayoutProperties() const
 {
 	ILayout::LayoutProperties retVal;
+
 	retVal.isFixedLayout = FixedLayouts->isChecked();
 	retVal.isBorderEnabled = BorderEnabled->isChecked();
 	retVal.borderColor = m_borderColor;
@@ -61,6 +71,7 @@ ILayout::LayoutProperties CLayoutSettingsDialog::GetLayoutProperties() const
 	retVal.rightMargin = RightMargin->text().toInt();
 	retVal.topMargin = TopMargin->text().toInt();
 	retVal.bottomMargin = BottomMargin->text().toInt();
+
 	return retVal;
 }
 
@@ -70,16 +81,16 @@ void CLayoutSettingsDialog::SetBorderColor(const QColor &color)
 	QPixmap pixmap(20, 20);
 	QPainter painter(&pixmap);
 	painter.fillRect(0, 0, 20, 20, color);
+
 	BorderColor->setIcon(pixmap);
+
 	m_borderColor = color;
 }
 
 
 void CLayoutSettingsDialog::OnBorderColorChanged()
 {
-	//const QColorDialog::ColorDialogOptions options = QFlag(colorDialogOptionsWidget->value());
 	const QColor color = QColorDialog::getColor(m_borderColor, this, "Select Color");
-
 	if (color.isValid()){
 		SetBorderColor(color);
 	}
