d087cfde906ababe88e2130e7d6f4cdd8cac9b79
vc <vc@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-04-05 03:45:33 +0000

    Addded "Export License" command
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3024 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtlicgui/CProductInstanceInfoViewDelegateComp.cpp
+++ Include/imtlicgui/CProductInstanceInfoViewDelegateComp.cpp
@@ -1,8 +1,42 @@
 #include <imtlicgui/CProductInstanceInfoViewDelegateComp.h>
 
 
+// ImtCore includes
+#include <imtrepo/IFileObjectCollection.h>
+
+
 namespace imtlicgui
 {
+
+
+// public methods
+
+CProductInstanceInfoViewDelegateComp::CProductInstanceInfoViewDelegateComp()
+	:m_exportLicenseCommand(tr("Export License"), 100, ibase::ICommand::CF_GLOBAL_MENU | ibase::ICommand::CF_TOOLBAR, CG_EDIT)
+{
+}
+
+
+// reimplemented (imtgui::ICollectionViewDelegate)
+
+void CProductInstanceInfoViewDelegateComp::UpdateItemSelection(
+			const imtbase::ICollectionInfo::Ids& selectedItems,
+			const QByteArray& selectedTypeId)
+{
+	BaseClass2::UpdateItemSelection(selectedItems, selectedTypeId);
+
+	m_exportLicenseCommand.setEnabled(selectedItems.count() == 1);
+}
+
+
+// reimplemented (ibase::TLocalizableWrap)
+
+void CProductInstanceInfoViewDelegateComp::OnLanguageChanged()
+{
+	BaseClass2::OnLanguageChanged();
+
+	m_exportLicenseCommand.SetVisuals(tr("Export License"), tr("Export License"), tr("Export existing license"), QIcon(":/Icons/Export"));
+}
 
 
 // protected methods
@@ -14,6 +48,26 @@ void CProductInstanceInfoViewDelegateComp::SetupSummaryInformation()
 	BaseClass::SetupSummaryInformation();
 
 	m_summaryInformationTypes.RemoveItem("TypeId");
+}
+
+
+void CProductInstanceInfoViewDelegateComp::SetupCommands()
+{
+	BaseClass2::SetupCommands();
+
+	imtrepo::IFileObjectCollection* fileCollectionPtr = dynamic_cast<imtrepo::IFileObjectCollection*>(m_collectionPtr);
+	if (fileCollectionPtr != nullptr) {
+		connect(&m_exportLicenseCommand, &QAction::triggered, this, &CProductInstanceInfoViewDelegateComp::OnExportLicense);
+		m_editCommands.InsertChild(&m_exportLicenseCommand);
+	}
+}
+
+
+// protected slots
+
+void CProductInstanceInfoViewDelegateComp::OnExportLicense()
+{
+
 }
 
 
--- Include/imtlicgui/CProductInstanceInfoViewDelegateComp.h
+++ Include/imtlicgui/CProductInstanceInfoViewDelegateComp.h
@@ -19,9 +19,24 @@ public:
 	I_BEGIN_COMPONENT(CProductInstanceInfoViewDelegateComp);
 	I_END_COMPONENT;
 
+	CProductInstanceInfoViewDelegateComp();
+
 protected:
 	// reimplemented (imtgui::CObjectCollectionViewDelegate)
 	virtual void SetupSummaryInformation() override;
+	virtual void SetupCommands() override;
+
+	// reimplemented (imtgui::ICollectionViewDelegate)
+	virtual void UpdateItemSelection(const imtbase::ICollectionInfo::Ids& selectedItems, const QByteArray& selectedTypeId) override;
+
+	// reimplemented (ibase::TLocalizableWrap)
+	virtual void OnLanguageChanged() override;
+
+protected Q_SLOTS:
+	virtual void OnExportLicense();
+
+private:
+	iqtgui::CHierarchicalCommand m_exportLicenseCommand;
 };
 
 
