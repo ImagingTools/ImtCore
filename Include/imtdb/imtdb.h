a7c91db3b94c0d83005a77247f110492899ca1a7
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-04-06 21:19:19 +0000

    Preparing work on MongoDB-based collection implementation
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3061 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtdb/imtdb.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+/**
+	Interfaces and basic implementations for general database management.
+	This package is system independent.
+*/
+namespace imtdb
+{
+
+
+} // namespace imtdb
+
+
---
+++ Include/imtmongo/imtmongo.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+/**
+	Containing implementations for operations on MongoDB database.
+*/
+namespace imtmongo
+{
+
+
+} // namespace imtmongo
+
+
