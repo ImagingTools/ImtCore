6cc4e6fa3d6061b5b2ee1061c8a557f9366fde01
sz <sz@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-04-23 13:32:01 +0000

    Readded CJsonModelBasedHandlerComp
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3172 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtrest/CJsonModelBasedHandlerComp.cpp
@@ -0,0 +1,94 @@
+﻿#include "CJsonModelBasedHandlerComp.h"
+
+// ACF includes
+#include <ilog/TLoggerCompWrap.h>
+#include <ifile/IFileNameParam.h>
+
+// ImtCore includes
+#include <imtrest/IRequestHandler.h>
+
+
+namespace imtrest
+{
+
+
+CJsonModelBasedHandlerComp::CJsonModelBasedHandlerComp()
+{
+
+}
+
+// reimplemented (IRequestHandler)
+
+IRequestHandler::ConstResponsePtr CJsonModelBasedHandlerComp::ProcessRequest(const IRequest& request) const
+{
+	const IProtocolEngine& engine = request.GetProtocolEngine();
+	QByteArray errorBody = "<html><head><title>Error</title></head><body><p>File resource was not found</p></body></html>";
+	QByteArray reponseTypeId = QByteArray("text/html; charset=utf-8");
+
+	ConstResponsePtr errorResponsePtr(engine.CreateResponse(request, IProtocolEngine::SC_RESOURCE_NOT_AVAILABLE, errorBody, reponseTypeId));
+
+	auto generateErrorResponsePtr = [&request, &engine, reponseTypeId](QByteArray errorBody){
+		qCritical() << __FILE__ << __LINE__ << "Error occurred" << errorBody;
+		QByteArray generatedErrorBody = "<html><head><title>Error</title></head><body><p>";
+		generatedErrorBody.append(errorBody);
+		generatedErrorBody.append("</p></body></html>");
+		return ConstResponsePtr(engine.CreateResponse(request, IProtocolEngine::SC_INTERNAL_ERROR, generatedErrorBody, reponseTypeId));
+	};
+
+
+
+	QByteArray body;
+	QByteArray commandId = request.GetCommandId();
+	QByteArray commandIdBase = *this->m_commandIdAttrPtr;
+	if(commandIdBase.endsWith('*'))
+	{
+		commandIdBase.chop(1);
+	}
+	QString modelName = commandId.replace(commandIdBase, "");
+	if(modelName.endsWith('/'))
+	{
+		modelName.chop(1);
+	}
+	while(modelName.startsWith('/'))
+	{
+		modelName.remove(0,1);
+	}
+
+	if(modelName == "__HELLO_MODEL__")
+	{
+		reponseTypeId = "application/json";
+		body = QByteArray(R"({"I'm seyin to you": "HELLO WORLD"})");
+	}
+	else if(modelName == "__EXAMPLE_1__")
+	{
+		reponseTypeId = "application/json";
+		body = QByteArray(R"({"adresses":[{"city":"Moscou","country":"Russia","postalCode":644099,"street":"Lenina 10"},{"city":"Munic","country":"Germany","postalCode":123456,"street":"Street St"}],"firstName":"Ivan","lastName":"Ivanov","nicName":"NicIvan"})");
+	}
+	else if(modelName == "__EXAMPLE_2__")
+	{
+		reponseTypeId = "application/json";
+		body = QByteArray(R"({"firstName":"Ivan","lastName":"Ivanov","nicName":"NicIvan","adresses":[{"country":"Russia","city":"Moscow","postalCode":644099,"street":"Lenina 10"},{"country":"Germany","city":"Munic","postalCode":123456,"street":"Street St"}]})");
+	}
+	else if(m_jsonModelProcessor.IsValid())
+	{
+//		m_jsonModelProcessor->GetModel(modelName);
+		reponseTypeId = "application/json";
+	}
+	else
+	{
+		generateErrorResponsePtr(QByteArray("Cannot init ModelProcessor OffLine OR Not created OR not setted"));
+	}
+
+	ConstResponsePtr responsePtr(engine.CreateResponse(request, IProtocolEngine::SC_OK, body, reponseTypeId));
+
+	return responsePtr;
+}
+
+QByteArray CJsonModelBasedHandlerComp::GetSupportedCommandId() const
+{
+	return *m_commandIdAttrPtr;
+}
+
+
+
+} // namespace imtrest
---
+++ Include/imtrest/CJsonModelBasedHandlerComp.h
@@ -0,0 +1,56 @@
+#pragma once
+
+// ACF includes
+#include <ifile/IFileNameParam.h>
+#include <ilog/TLoggerCompWrap.h>
+
+// ImtCore includes
+#include <imtrest/IRequestHandler.h>
+
+// Qt includes
+#include <QtCore/QDir>
+#include <QtCore/QFile>
+#include <QtCore/QTextStream>
+
+// ImtCore includes
+#include <imtrest/IRequest.h>
+#include <imtrest/IResponder.h>
+#include <imtrest/IResponse.h>
+#include <imtrest/IProtocolEngine.h>
+
+namespace imtrest
+{
+
+/**
+ * \brief The CJsonModelBasedHandlerComp class generates JSON-model
+ */
+class CJsonModelBasedHandlerComp:
+			public ilog::CLoggerComponentBase,
+			virtual public IRequestHandler
+{
+public:
+	typedef ilog::CLoggerComponentBase BaseClass;
+
+	CJsonModelBasedHandlerComp();
+
+	I_BEGIN_COMPONENT(CJsonModelBasedHandlerComp);
+		I_REGISTER_INTERFACE(IRequestHandler);
+		I_ASSIGN(m_jsonModelProcessor, "ModelProcessor", "description", false, "ClassName");
+		I_ASSIGN(m_commandIdAttrPtr, "CommandId", "List of command-ID used with corresponded handlers", true, "");
+	I_END_COMPONENT
+
+
+	// reimplemented (IRequestHandler)
+	virtual ConstResponsePtr ProcessRequest(const IRequest& request) const override;
+	virtual QByteArray GetSupportedCommandId() const override;
+
+private:
+	I_REF(ifile::IFileNameParam, m_jsonModelProcessor);
+	I_ATTR(QByteArray, m_commandIdAttrPtr);
+
+};
+
+
+} // namespace imtrest
+
+
