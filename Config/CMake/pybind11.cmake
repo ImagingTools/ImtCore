7d47c7b95a041362a4c40f5f2d85955122819f3e
alexey.lucenko <alexey.lucenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-04-27 11:50:32 +0000

    Improved file
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3200 21205d9f-8918-2240-a3d5-c39064a2c852

--- Config/CMake/pybind11.cmake
+++ Config/CMake/pybind11.cmake
@@ -1,13 +1,12 @@
 include_directories("${CMAKE_CURRENT_LIST_DIR}/../../../3rdParty/pybind11/include")
-#INCLUDEPATH += $$PWD/../../../3rdParty/pybind11/include
+
 
 include_directories("${CMAKE_CURRENT_LIST_DIR}/../../../3rdParty/Python/3.8/include")
-#INCLUDEPATH += $$(PYTHON38DIR)/include
+
 
 #target_link_libraries(${PROJECT_NAME} ${CMAKE_CURRENT_LIST_DIR}/../../../../3rdParty/Python/3.8/libs/python38.lib)
 target_link_libraries(${PROJECT_NAME} python38)
-#LIBS += $$(PYTHON38DIR)/libs/Python38.lib
 
-#QMAKE_CXXFLAGS += /bigobj
+
 
 
