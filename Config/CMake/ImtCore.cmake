99b15336434d6279926b3b123be6fba703b0f2f9
alexey.lucenko <alexey.lucenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-04-27 10:32:24 +0000

    Update file
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3191 21205d9f-8918-2240-a3d5-c39064a2c852

--- Config/CMake/ImtCore.cmake
+++ Config/CMake/ImtCore.cmake
@@ -1,20 +1,6 @@
-include(${ACFSLNDIR}/Config/CMake/AcfSln.cmake)
+include(${ACFDIR}/Config/CMake/AcfStd.cmake)
 include(${IACFDIR}/Config/CMake/Iacf.cmake)
-#include($ENV{IACFDIR}/Config/CMake/Iacf.cmake)
 
-##include_directories("$ENV{IMTCOREDIR}/$$AUXINCLUDEDIR")
-#include($(ACFSLNDIR)/Config/QMake/AcfSln.pri)
-#include($(IACFDIR)/Config/QMake/Iacf.pri)
-
-#INCLUDEPATH += $(IMTCOREDIR)/Include
-#INCLUDEPATH += $(IMTCOREDIR)/Impl
-#INCLUDEPATH += $(IMTCOREDIR)/$$AUXINCLUDEDIR
-#LIBS += -L$$PWD/../../Lib/$$COMPILER_DIR
-
-
-#win32{
 target_link_libraries(${PROJECT_NAME} advapi32 user32)
-#	LIBS += -ladvapi32 -luser32
-#}
 
 
