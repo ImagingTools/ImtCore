31951d27838c74cb8605a07b53bcf0c98432a399
vs <vs@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-05-26 09:01:30 +0000

    Code corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3338 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtbase/CJsonHelper.h
+++ Include/imtbase/CJsonHelper.h
@@ -13,16 +13,16 @@ namespace imtbase
 class CJsonHelper
 {
 public:
-	static bool CJsonHelper::GetBoolValue(const QJsonObject& object, const QString& key, bool &value);
-	static bool CJsonHelper::GetDoubleValue(const QJsonObject& object, const QString& key, double &value);
-	static bool CJsonHelper::GetStringValue(const QJsonObject& object, const QString& key, QString &value);
-	static bool CJsonHelper::GetArrayValue(const QJsonObject& object, const QString& key, QJsonArray &value);
-	static bool CJsonHelper::GetObjectValue(const QJsonObject& object, const QString& key, QJsonObject &value);
-	static bool CJsonHelper::GetBoolValue(const QJsonArray& array, int index, bool &value);
-	static bool CJsonHelper::GetDoubleValue(const QJsonArray& array, int index, double &value);
-	static bool CJsonHelper::GetStringValue(const QJsonArray& array, int index, QString &value);
-	static bool CJsonHelper::GetArrayValue(const QJsonArray& array, int index, QJsonArray &value);
-	static bool CJsonHelper::GetObjectValue(const QJsonArray& array, int index, QJsonObject &value);
+	static bool GetBoolValue(const QJsonObject& object, const QString& key, bool &value);
+	static bool GetDoubleValue(const QJsonObject& object, const QString& key, double &value);
+	static bool GetStringValue(const QJsonObject& object, const QString& key, QString &value);
+	static bool GetArrayValue(const QJsonObject& object, const QString& key, QJsonArray &value);
+	static bool GetObjectValue(const QJsonObject& object, const QString& key, QJsonObject &value);
+	static bool GetBoolValue(const QJsonArray& array, int index, bool &value);
+	static bool GetDoubleValue(const QJsonArray& array, int index, double &value);
+	static bool GetStringValue(const QJsonArray& array, int index, QString &value);
+	static bool GetArrayValue(const QJsonArray& array, int index, QJsonArray &value);
+	static bool GetObjectValue(const QJsonArray& array, int index, QJsonObject &value);
 };
 
 
