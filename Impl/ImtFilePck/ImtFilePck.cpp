bdf991d9ecddf41ee3ca05de0e5aaad52a44403e
sz <sz@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-07-07 06:54:10 +0000

    ImtFilePck added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3463 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Impl/ImtFilePck/ImtFilePck.cpp
@@ -0,0 +1,26 @@
+#include "ImtFilePck.h"
+
+
+// ACF includes
+#include <icomp/export.h>
+
+
+namespace ImtFilePck
+{
+
+
+I_EXPORT_PACKAGE(
+			"ImtFilePck",
+			"ImagingTools file framework component package",
+			IM_PROJECT("\"ImagingTools File Core Framework\"") IM_COMPANY("ImagingTools"));
+
+I_EXPORT_COMPONENT(
+			FileProvider,
+			"File provider component",
+			"File Provider");
+
+
+
+} // namespace ImtFilePck
+
+
---
+++ Impl/ImtFilePck/ImtFilePck.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+// ACF includes
+
+// ImtCore includes
+
+#include <imtfile/CFileProviderComp.h>
+
+
+/**
+	ImtFilePck package
+*/
+namespace ImtFilePck
+{
+
+
+typedef imtfile::CFileProviderComp FileProvider;
+
+
+} // namespace ImtFilePck
+
+
