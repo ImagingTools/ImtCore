d18a090b3733d22291390043aaf06604b5f87dd1
sz <sz@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-08-17 06:28:46 +0000

    architecture, scheme rethinking and some refactoring 2_of_3
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3577 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/DesignTokenCreatorExe/Main.cpp
+++ Impl/DesignTokenCreatorExe/Main.cpp
@@ -9,66 +9,9 @@
 #include <ifile/IFileNameParam.h>
 
 // project includes
-#include <imtstyle/CDesignTokenArgumentsParser.h>
+#include <imtstyle/IDesignTokenArgumentsParser.h>
 
 #include <GeneratedFiles/DesignTokenCreatorExe/CDesignTokenCreatorExe.h>
-
-static bool SetParams(int argc, char** argv, iprm::IParamsSet& paramSet, QString& errorString)
-{
-	using imtstyle::CDesignTokenArgumentsParser;
-	bool retval = false;
-	QByteArrayList arguments;
-	for (int i = 0; i < argc; ++i){
-		arguments << argv[i];
-	}
-	CDesignTokenArgumentsParser parser(argc, argv);
-
-	if (parser.CheckArgument(CDesignTokenArgumentsParser::AA_HELP)){
-		QByteArray paramId = "HelpParam";
-
-		iprm::IEnableableParam* templateIconColorParamPtr = dynamic_cast<iprm::IEnableableParam*>(paramSet.GetEditableParameter(paramId));
-		Q_ASSERT(templateIconColorParamPtr != nullptr);
-
-		templateIconColorParamPtr->SetEnabled(true);
-	}
-	if (parser.CheckArgument(CDesignTokenArgumentsParser::AA_TEMPLATE_ICON_COLOR)){QByteArray paramId = "TemplateIconColor";
-		QByteArray paramValue = parser.GetArgumentValue(CDesignTokenArgumentsParser::AA_TEMPLATE_ICON_COLOR);
-
-		iprm::IIdParam* templateIconColorParamPtr = dynamic_cast<iprm::IIdParam*>(paramSet.GetEditableParameter(paramId));
-		Q_ASSERT(templateIconColorParamPtr != nullptr);
-
-		templateIconColorParamPtr->SetId(paramValue);
-	}
-	if (parser.CheckArgument(CDesignTokenArgumentsParser::AA_TEMPLATE_PRIMARY_COLOR)){
-		QByteArray paramId = "TemplatePrimaryColor";
-		QByteArray paramValue = parser.GetArgumentValue(CDesignTokenArgumentsParser::AA_TEMPLATE_PRIMARY_COLOR);
-
-		iprm::IIdParam* templateIconColorParamPtr = dynamic_cast<iprm::IIdParam*>(paramSet.GetEditableParameter(paramId));
-		Q_ASSERT(templateIconColorParamPtr != nullptr);
-
-		templateIconColorParamPtr->SetId(paramValue);
-	}
-	if (parser.CheckArgument(CDesignTokenArgumentsParser::AA_TEMPLATE_SECONDARY_COLOR)){
-		QByteArray paramId = "TemplateSecondaryColor";
-		QByteArray paramValue = parser.GetArgumentValue(CDesignTokenArgumentsParser::AA_TEMPLATE_SECONDARY_COLOR);
-
-		iprm::IIdParam* templateIconColorParamPtr = dynamic_cast<iprm::IIdParam*>(paramSet.GetEditableParameter(paramId));
-		Q_ASSERT(templateIconColorParamPtr != nullptr);
-
-		templateIconColorParamPtr->SetId(paramValue);
-	}
-	if (parser.CheckArgument(CDesignTokenArgumentsParser::AA_DESIGN_TOKEN_FILE_PATH)){
-		QByteArray paramId = "DesignTokenFilePath";
-		QByteArray paramValue = parser.GetArgumentValue(CDesignTokenArgumentsParser::AA_DESIGN_TOKEN_FILE_PATH);
-
-		ifile::IFileNameParam* designTokenFilePathParamPtr = dynamic_cast<ifile::IFileNameParam*>(paramSet.GetEditableParameter(paramId));
-		Q_ASSERT(designTokenFilePathParamPtr != nullptr);
-
-		designTokenFilePathParamPtr->SetPath(paramValue);
-	}
-	return retval;
-}
-
 
 int main(int argc, char *argv[])
 {
@@ -76,17 +19,14 @@ int main(int argc, char *argv[])
 
 	QString errorString = "unknown error";
 
-	iprm::IParamsSet* paramSetPtr = instance.GetInterface<iprm::IParamsSet>("ConversionParams");
-	if (paramSetPtr == nullptr){
+	imtstyle::IDesignTokenArgumentsParser* argumentsParser = instance.GetInterface<imtstyle::IDesignTokenArgumentsParser>();
+	if (argumentsParser == nullptr){
 		std::cerr << "Component configuration failed" << std::endl;
 		return -1;
 	}
-	if (!SetParams(argc, argv, *paramSetPtr, errorString)){
-		std::cerr << errorString.toLocal8Bit().constData();
-		return -2;
+	else {
+		argumentsParser->SetArguments(argc, argv);
 	}
-
-	/// \todo DO NOT forget check DesignTokenProcessor!!! DesignTokenProcessor->Check();
 
 	ibase::IApplication* applicationPtr = instance.GetInterface<ibase::IApplication>();
 	if (applicationPtr != NULL){
