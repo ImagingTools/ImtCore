94eef280d14ddfc5e34e32295c05ac2c912a70a4
sz <sz@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-09-03 09:01:26 +0000

    imt style utils added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3694 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtstyle/CImtStyleUtils.cpp
@@ -0,0 +1,616 @@
+#include <imtstyle/CImtStyleUtils.h>
+
+// Qt includes
+#include <QtCore/QtCore>
+#include <QtGui/QPalette>
+
+// ACF includes
+#include <ilog/TLoggerCompWrap.h>
+#include <ifile/IFileNameParam.h>
+#include <iprm/IParamsSet.h>
+
+// ImtCore includes
+#include <imtbase/CCollectionInfo.h>
+#include <imtstyle/IDesignTokenFileParser.h>
+
+namespace imtstyle
+{
+
+const QMap<QString, QPalette::ColorGroup> CImtStyleUtils::s_colorGroupNamesMap = {
+	std::make_pair("Active", QPalette::ColorGroup::Active),
+	std::make_pair("Disabled", QPalette::ColorGroup::Disabled),
+	std::make_pair("Inactive", QPalette::ColorGroup::Inactive),
+	std::make_pair("NColorGroups", QPalette::ColorGroup::NColorGroups),
+	std::make_pair("Current", QPalette::ColorGroup::Current),
+	std::make_pair("All", QPalette::ColorGroup::All),
+	std::make_pair("Normal", QPalette::ColorGroup::Normal)
+};
+
+
+const QMap<QString, QPalette::ColorRole>CImtStyleUtils::s_colorRolesNamesMap = {
+	std::make_pair("WindowText", QPalette::ColorRole::WindowText),
+	std::make_pair("Button", QPalette::ColorRole::Button),
+	std::make_pair("Light", QPalette::ColorRole::Light),
+	std::make_pair("Midlight", QPalette::ColorRole::Midlight),
+	std::make_pair("Dark", QPalette::ColorRole::Dark),
+	std::make_pair("Mid", QPalette::ColorRole::Mid),
+	std::make_pair("Text", QPalette::ColorRole::Text),
+	std::make_pair("BrightText", QPalette::ColorRole::BrightText),
+	std::make_pair("ButtonText", QPalette::ColorRole::ButtonText),
+	std::make_pair("Base", QPalette::ColorRole::Base),
+	std::make_pair("Window", QPalette::ColorRole::Window),
+	std::make_pair("Shadow", QPalette::ColorRole::Shadow),
+	std::make_pair("Highlight", QPalette::ColorRole::Highlight),
+	std::make_pair("HighlightedText", QPalette::ColorRole::HighlightedText),
+	std::make_pair("Link", QPalette::ColorRole::Link),
+	std::make_pair("LinkVisited", QPalette::ColorRole::LinkVisited),
+	std::make_pair("AlternateBase", QPalette::ColorRole::AlternateBase),
+	std::make_pair("NoRole", QPalette::ColorRole::NoRole),
+	std::make_pair("ToolTipBase", QPalette::ColorRole::ToolTipBase),
+	std::make_pair("ToolTipText", QPalette::ColorRole::ToolTipText),
+	std::make_pair("PlaceholderText", QPalette::ColorRole::PlaceholderText),
+	std::make_pair("Foreground", QPalette::ColorRole::Foreground),
+	std::make_pair("Background", QPalette::ColorRole::Background)
+};
+
+
+const QMap<QString, QFont::HintingPreference> CImtStyleUtils::s_fontHintingPreferenceNamesMap = {
+	std::make_pair("PreferDefaultHinting", QFont::HintingPreference::PreferDefaultHinting),
+	std::make_pair("PreferFullHinting", QFont::HintingPreference::PreferFullHinting),
+	std::make_pair("PreferNoHinting", QFont::HintingPreference::PreferNoHinting),
+	std::make_pair("PreferVerticalHinting", QFont::HintingPreference::PreferVerticalHinting)
+};
+
+
+const QMap<QString, QFont::Capitalization> CImtStyleUtils::s_fontCapitalizationNamesMap = {
+	std::make_pair("AllLowercase", QFont::Capitalization::AllLowercase),
+	std::make_pair("AllUppercase", QFont::Capitalization::AllUppercase),
+	std::make_pair("Capitalize", QFont::Capitalization::Capitalize),
+	std::make_pair("MixedCase", QFont::Capitalization::MixedCase),
+	std::make_pair("SmallCaps", QFont::Capitalization::SmallCaps)
+};
+
+
+const QMap<QString, QFont::Style> CImtStyleUtils::s_fontStyleNamesMap = {
+	std::make_pair("StyleItalic", QFont::Style::StyleItalic),
+	std::make_pair("StyleNormal", QFont::Style::StyleNormal),
+	std::make_pair("StyleOblique", QFont::Style::StyleOblique)
+};
+
+
+const QMap<QString, QFont::StyleStrategy> CImtStyleUtils::s_fontStyleStrategyNamesMap = {
+	std::make_pair("ForceIntegerMetrics", QFont::StyleStrategy::ForceIntegerMetrics),
+	std::make_pair("ForceOutline", QFont::StyleStrategy::ForceOutline),
+	std::make_pair("NoAntialias", QFont::StyleStrategy::NoAntialias),
+	std::make_pair("NoFontMerging", QFont::StyleStrategy::NoFontMerging),
+	std::make_pair("NoSubpixelAntialias", QFont::StyleStrategy::NoSubpixelAntialias),
+	std::make_pair("OpenGLCompatible", QFont::StyleStrategy::OpenGLCompatible),
+	std::make_pair("PreferAntialias", QFont::StyleStrategy::PreferAntialias),
+	std::make_pair("PreferBitmap", QFont::StyleStrategy::PreferBitmap),
+	std::make_pair("PreferDefault", QFont::StyleStrategy::PreferDefault),
+	std::make_pair("PreferDevice", QFont::StyleStrategy::PreferDevice),
+	std::make_pair("PreferMatch", QFont::StyleStrategy::PreferMatch),
+	std::make_pair("PreferNoShaping", QFont::StyleStrategy::PreferNoShaping),
+	std::make_pair("PreferOutline", QFont::StyleStrategy::PreferOutline),
+	std::make_pair("PreferQuality", QFont::StyleStrategy::PreferQuality)
+};
+
+
+const QMap<QString, QFont::StyleHint> CImtStyleUtils::s_fontStyleHintNamesMap = {
+	std::make_pair("AnyStyle", QFont::StyleHint::AnyStyle),
+	std::make_pair("Courier", QFont::StyleHint::Courier),
+	std::make_pair("Cursive", QFont::StyleHint::Cursive),
+	std::make_pair("Decorative", QFont::StyleHint::Decorative),
+	std::make_pair("Fantasy", QFont::StyleHint::Fantasy),
+	std::make_pair("Helvetica", QFont::StyleHint::Helvetica),
+	std::make_pair("Monospace", QFont::StyleHint::Monospace),
+	std::make_pair("OldEnglish", QFont::StyleHint::OldEnglish),
+	std::make_pair("SansSerif", QFont::StyleHint::SansSerif),
+	std::make_pair("Serif", QFont::StyleHint::Serif),
+	std::make_pair("System", QFont::StyleHint::System),
+	std::make_pair("Times", QFont::StyleHint::Times),
+	std::make_pair("TypeWriter", QFont::StyleHint::TypeWriter)
+};
+
+
+const QMap<QString, QFont::SpacingType> CImtStyleUtils::s_fontSpacingTypeNamesMap = {
+	std::make_pair("AbsoluteSpacing", QFont::SpacingType::AbsoluteSpacing),
+	std::make_pair("PercentageSpacing", QFont::SpacingType::PercentageSpacing)
+};
+
+
+QString CImtStyleUtils::GetColorName(QPalette::ColorGroup group, QPalette::ColorRole role)
+{
+	return CImtStyleUtils::s_colorGroupNamesMap.key(group) + CImtStyleUtils::s_colorRolesNamesMap.key(role);
+}
+
+
+bool SetVariableColor(QByteArray& data, const QPalette& palette)
+{
+	/// points to '$' symbol
+	int indexOfBeginVariable = -1;
+	/// points to '}' symbol
+	int indexOfEndVariable = -1;
+
+	for(int i = 0; i < data.length(); ++i){
+
+		QChar symbol(data.at(i));
+		if(symbol == QChar('$') && i+1 < data.length()){
+
+			QChar nextSymbol = data.at(i+1);
+			if(nextSymbol == QChar('{')){
+				indexOfBeginVariable = i;
+
+				for(int j = i+1; j < data.length(); ++j){
+					QChar endSymbol = data.at(j);
+
+					if(endSymbol == QChar('}')){
+						indexOfEndVariable = j;
+						break;
+					}
+				}
+			}
+		}
+
+		if(indexOfBeginVariable > -1 && indexOfEndVariable > indexOfBeginVariable){
+
+			int lengthOfVariable = indexOfEndVariable - indexOfBeginVariable - 2;
+			QString colorName = data.mid(indexOfBeginVariable+2, lengthOfVariable);
+
+			QPalette::ColorGroup colorGroup;
+			QPalette::ColorRole colorRole;
+
+			if(CImtStyleUtils::GetColorRoleGroup(colorName, colorGroup, colorRole)){
+				QColor color = palette.color(colorGroup, colorRole);
+				QByteArray colorHex;
+				if(color.name() == "#000000"){
+					//						colorHex = m_designTokenFileParserAttrPtr->GetRawColor(m_currentTheme, colorGroup, colorRole);
+					if(!colorHex.length()){
+						colorHex = "#000000";
+					}
+				}
+				else {
+					colorHex = color.name().toUtf8();
+				}
+				data.replace(indexOfBeginVariable, lengthOfVariable+3, colorHex);
+			}
+			return true;
+		}
+	}
+	return false;
+}
+
+
+QByteArrayList CImtStyleUtils::GetVariables(const QByteArray& data, const QChar& variableBeginSymbol, const QChar& variableBeginSymbol2, const QChar& variableEndSymbol)
+{
+	QByteArrayList retval;
+
+	/// points to 'beginSymbol' symbol
+	int indexOfBeginVariable = -1;
+	/// points to 'endSymbol' symbol
+	int indexOfEndVariable = -1;
+
+	for(int i = 0; i < data.length(); ++i){
+
+		QChar symbol(data.at(i));
+		if(symbol == variableBeginSymbol && i+1 < data.length()){
+
+			QChar nextSymbol = data.at(i+1);
+			if(nextSymbol == variableBeginSymbol2){
+				indexOfBeginVariable = i;
+
+				for(int j = i+1; j < data.length(); ++j){
+					QChar endSymbol = data.at(j);
+
+					if(endSymbol == variableEndSymbol){
+						indexOfEndVariable = j;
+						break;
+					}
+				}
+			}
+		}
+		if(indexOfBeginVariable > -1 && indexOfEndVariable > indexOfBeginVariable){
+
+			int lengthOfVariable = indexOfEndVariable - indexOfBeginVariable - 2;
+			QByteArray variableName = data.mid(indexOfBeginVariable+2, lengthOfVariable);
+			retval << variableName;
+		}
+	}
+	return retval;
+}
+
+
+bool CImtStyleUtils::SetVariables(QByteArray& data, const QChar& variableBeginSymbol, const QChar& variableBeginSymbol2, const QChar& variableEndSymbol, const QMap<QByteArray, QByteArray> variables)
+{
+	bool retval = SetVariable_(data, variableBeginSymbol, variableBeginSymbol2, variableEndSymbol, variables);
+	while(SetVariable_(data, variableBeginSymbol, variableBeginSymbol2, variableEndSymbol, variables));
+	return retval;
+}
+
+
+bool CImtStyleUtils::GetColorRoleGroup(const QString& name, QPalette::ColorGroup& group, QPalette::ColorRole& role)
+{
+	const QStringList& groupNames = CImtStyleUtils::s_colorGroupNamesMap.keys();
+
+	for(const QString& groupName: groupNames){
+
+		QRegularExpression groupRegEx(QString("^" + groupName), QRegularExpression::PatternOption::CaseInsensitiveOption);
+		QRegularExpressionMatchIterator globalMatch = groupRegEx.globalMatch(name);
+
+		if(globalMatch.hasNext()){
+
+			QRegularExpressionMatch groupRegExMatch;
+			groupRegExMatch = globalMatch.next();
+
+			if(!s_colorGroupNamesMap.contains(groupName)) {
+				return false;
+			}
+
+			group = s_colorGroupNamesMap[groupName];
+
+			QString roleName = name;
+			roleName.remove(groupRegExMatch.capturedStart(), groupRegExMatch.capturedLength());
+
+			if(!s_colorRolesNamesMap.contains(roleName)) {
+				return false;
+			}
+
+			role = s_colorRolesNamesMap[roleName];
+			return true;
+		}
+	}
+	return false;
+}
+
+
+QPalette CImtStyleUtils::GetPaletteFromEntry(const QJsonValue& paletteEntry)
+{
+	QPalette palette;
+
+	const QJsonObject& paletteEntryObject = paletteEntry.toObject();
+
+	for(QJsonObject::const_iterator value = paletteEntryObject.constBegin(); value != paletteEntryObject.constEnd(); ++value){
+		QPalette::ColorGroup colorGroup;
+		QPalette::ColorRole colorRole;
+		CImtStyleUtils::GetColorRoleGroup(value.key(), colorGroup, colorRole);
+
+		QColor color;
+		if (!CImtStyleUtils::CreateColorFromGrb(value->toString(), color)){
+			color.setNamedColor(value->toString());
+		}
+		if(color.isValid()){
+			palette.setColor(colorGroup, colorRole, color);
+		}
+	}
+	return palette;
+}
+
+
+bool CImtStyleUtils::CreateColorFromGrb(const QString& rgbString, QColor& color)
+{
+	int indexOfBegin = rgbString.indexOf("rgb(");
+	if(indexOfBegin >= 0){
+		bool rFill = false, gFill = false;
+		QString r,g,b;
+
+		for(int i = indexOfBegin + 4; i < rgbString.length(); ++i){
+			QChar symbol = rgbString[i];
+			if(symbol.isDigit()){
+				if(!rFill){
+					r += symbol;
+				}
+				else if(!gFill){
+					g += symbol;
+				}
+				else {
+					b += symbol;
+				}
+			}
+			else{
+				if(!rFill){
+					rFill = true;
+				}
+				else if(!gFill){
+					gFill = true;
+				}
+				else{
+					int iR = r.toInt(), iG = g.toInt(), iB = b.toInt();
+					color = QColor::fromRgb(iR, iG, iB);
+					return color.isValid();
+				}
+			}
+		}
+	}
+	return false;
+}
+
+
+bool CImtStyleUtils::GetFontHintingPreference(const QString& name, QFont::HintingPreference& hintingPreference)
+{
+	if(s_fontHintingPreferenceNamesMap.contains(name)){
+		hintingPreference = s_fontHintingPreferenceNamesMap[name];
+		return true;
+	}
+	return false;
+}
+
+
+bool CImtStyleUtils::GetFontCapitalization(const QString& name, QFont::Capitalization& capitalization)
+{
+	if(s_fontCapitalizationNamesMap.contains(name)){
+		capitalization = s_fontCapitalizationNamesMap[name];
+		return true;
+	}
+	return false;
+}
+
+
+bool CImtStyleUtils::GetFontStyle(const QString& name, QFont::Style& style)
+{
+	if(s_fontStyleNamesMap.contains(name)){
+		style = s_fontStyleNamesMap[name];
+		return true;
+	}
+	return false;
+}
+
+
+bool CImtStyleUtils::GetFontStyleStrategy(const QString& name, QFont::StyleStrategy& styleStrategy)
+{
+	if(s_fontStyleStrategyNamesMap.contains(name)){
+		styleStrategy = s_fontStyleStrategyNamesMap[name];
+		return true;
+	}
+	return false;
+}
+
+
+bool CImtStyleUtils::GetFontStyleStrategy(const QStringList& names, QFont::StyleStrategy& styleStrategy)
+{
+	int output = 0x0000;
+	for(const QString& name: names){
+		if(!s_fontStyleStrategyNamesMap.contains(name)){
+			return false;
+		}
+		output |= s_fontStyleStrategyNamesMap[name];
+	}
+	styleStrategy =  QFont::StyleStrategy(output);
+	return true;
+}
+
+
+bool CImtStyleUtils::GetFontStyleHint(const QString& name, QFont::StyleHint& styleHint)
+{
+	if(s_fontStyleHintNamesMap.contains(name)){
+		styleHint = s_fontStyleHintNamesMap[name];
+		return true;
+	}
+	return false;
+}
+
+
+bool CImtStyleUtils::GetFontSpacingType(const QString& name, QFont::SpacingType& spacingType)
+{
+	if(s_fontSpacingTypeNamesMap.contains(name)){
+		spacingType = s_fontSpacingTypeNamesMap[name];
+		return true;
+	}
+	return false;
+}
+
+
+bool CImtStyleUtils::GetFontsFromEntry(const QJsonValue& fontsEntry, QMap<QByteArray, QFont>& fonts, imtbase::CCollectionInfo* fontsCollectionInfo)
+{
+	QJsonObject jsonObject = fontsEntry.toObject();
+
+	for(QJsonObject::const_iterator fontEntry = jsonObject.constBegin(); fontEntry != jsonObject.constEnd(); ++ fontEntry){
+
+		QJsonObject fontObject = fontEntry->toObject();
+
+		QFont font;
+
+		bool Bold = false;
+		bool FixedPitch = false;
+		bool Italic = false;
+		bool Kerning = false;
+		bool Overline = false;
+		bool StrikeOut = false;
+		bool Underline = false;
+
+		int pixelSize = 32;
+		int pointSize = 16;
+		int Stretch = 100;
+		double pointSizeF = 8.5;
+		int weight = 0;
+		double wordSpacing = 1;
+
+		QFont::HintingPreference hintingPreference = QFont::PreferDefaultHinting;
+		QString s_hintingPreference;
+		QFont::Capitalization caps = QFont::MixedCase;
+		QString s_caps;
+		QFont::Style style = QFont::StyleNormal;
+		QString s_style;
+
+		QString family;
+		QString styleName;
+		QStringList families;
+
+		QFont::StyleStrategy styleStrategyS = QFont::PreferDefault;
+		QStringList sl_styleStrategyS;
+
+		QFont::StyleHint styleHint = QFont::AnyStyle;
+		QString s_styleHint;
+		QFont::StyleStrategy styleStrategy = QFont::PreferDefault;
+		QStringList sl_styleStrategy;
+
+		QFont::SpacingType spacingType = QFont::PercentageSpacing;
+		QString s_spacingType;
+		double letterSpacing = 100.;
+
+		//------------------------------------------bool
+		if(GetBoolValue(fontObject, "Bold", Bold)){
+			font.setBold(Bold);
+		}
+
+		if(GetBoolValue(fontObject, "FixedPitch", FixedPitch)){
+			font.setFixedPitch(FixedPitch);
+		}
+
+		if(GetBoolValue(fontObject, "Italic", Italic)){
+			font.setItalic(Italic);
+		}
+
+		if(GetBoolValue(fontObject, "Kerning", Kerning)){
+			font.setKerning(Kerning);
+		}
+
+		if(GetBoolValue(fontObject, "Overline", Overline)){
+			font.setOverline(Overline);
+		}
+
+		if(GetBoolValue(fontObject, "StrikeOut", StrikeOut)){
+			font.setStrikeOut(StrikeOut);
+		}
+
+		if(GetBoolValue(fontObject, "Underline", Underline)){
+			font.setUnderline(Underline);
+		}
+
+		//------------------------------------------number
+		if(GetIntValue(fontObject, "PixelSize", pixelSize)){
+			font.setPixelSize(pixelSize);
+		}
+
+		if(GetIntValue(fontObject, "PointSize", pointSize)){
+			font.setPointSize(pointSize);
+		}
+
+		if(GetIntValue(fontObject, "Stretch", Stretch)){
+			font.setStretch(Stretch);
+		}
+
+		if(GetDoubleValue(fontObject, "PointSizeF", pointSizeF)){
+			font.setPointSizeF(pointSizeF);
+		}
+
+		if(GetIntValue(fontObject, "Weight", weight)){
+			font.setWeight(weight);
+		}
+
+		if(GetDoubleValue(fontObject, "WordSpacing", wordSpacing)){
+			font.setWordSpacing(wordSpacing);
+		}
+
+		//------------------------------------------oneOf
+		if(GetStringValue(fontObject, "HintingPreference", s_hintingPreference) &&
+				GetFontHintingPreference(s_hintingPreference, hintingPreference)){
+			font.setHintingPreference(hintingPreference);
+		}
+
+		if(GetStringValue(fontObject, "HintingPreference", s_caps) &&
+				GetFontCapitalization(s_hintingPreference, caps)){
+			font.setCapitalization(caps);
+		}
+
+		if(GetStringValue(fontObject, "HintingPreference", s_style) &&
+				GetFontStyle(s_hintingPreference, style)){
+			font.setStyle(style);
+		}
+
+		//------------------------------------------String
+		if((GetStringValue(fontObject, "Family", family) || GetStringValue(fontObject, "Families", family)) && family.length()){
+			font.setFamily(family);
+		}
+
+		if(GetStringValue(fontObject, "StyleName", styleName) && styleName.length()){
+			font.setStyleName(styleName);
+		}
+
+		//------------------------------------------StringList
+#if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
+		if((GetStringListValue(fontObject, "Family", families) || GetStringListValue(fontObject, "Families", families)) && families.size()){
+			font.setFamilies(families);
+		}
+#endif
+
+		//------------------------------------------ManyOf
+		if(GetStringListValue(fontObject, "StyleStrategy", sl_styleStrategyS) &&
+				GetFontStyleStrategy(sl_styleStrategyS, styleStrategyS)){
+			font.setStyleStrategy(styleStrategyS);
+		}
+
+		//------------------------------------------Combo
+		QJsonObject comboObject;
+		if(GetObjectValue(fontObject, "StyleHint", comboObject) &&
+				GetStringValue(comboObject, "StyleHint", s_styleHint) &&
+				GetFontStyleHint(s_styleHint, styleHint) &&
+				GetStringListValue(comboObject, "StyleStrategy", sl_styleStrategy) &&
+				GetFontStyleStrategy(sl_styleStrategy, styleStrategy)){
+			font.setStyleHint(styleHint, styleStrategy);
+		}
+
+		if(GetObjectValue(fontObject, "LetterSpacing", comboObject) &&
+				GetStringValue(comboObject, "SpacingType", s_spacingType) &&
+				GetFontSpacingType(s_spacingType, spacingType) &&
+				GetDoubleValue(comboObject, "Spacing", letterSpacing)
+				){
+			font.setLetterSpacing(spacingType, letterSpacing);
+		}
+
+		if(fontsCollectionInfo != nullptr){
+			fontsCollectionInfo->InsertItem(fontEntry.key().toUtf8(), fontEntry.key(), "");
+		}
+		fonts.insert(fontEntry.key().toUtf8(), font);
+	}
+
+	return true;
+
+}
+
+
+bool CImtStyleUtils::SetVariable_(QByteArray& data, const QChar& variableBeginSymbol, const QChar& variableBeginSymbol2, const QChar& variableEndSymbol, const QMap<QByteArray, QByteArray> variables)
+{
+
+	/// points to 'beginSymbol' symbol
+	int indexOfBeginVariable = -1;
+	/// points to 'endSymbol' symbol
+	int indexOfEndVariable = -1;
+
+	for(int i = 0; i < data.length(); ++i){
+
+		QChar symbol(data.at(i));
+		if(symbol == variableBeginSymbol && i+1 < data.length()){
+
+			QChar nextSymbol = data.at(i+1);
+			if(nextSymbol == variableBeginSymbol2){
+				indexOfBeginVariable = i;
+
+				for(int j = i+1; j < data.length(); ++j){
+					QChar endSymbol = data.at(j);
+
+					if(endSymbol == variableEndSymbol){
+						indexOfEndVariable = j;
+						break;
+					}
+				}
+			}
+		}
+
+		if(indexOfBeginVariable > -1 && indexOfEndVariable > indexOfBeginVariable){
+
+			int lengthOfVariable = indexOfEndVariable - indexOfBeginVariable - 2;
+			QByteArray variableName = data.mid(indexOfBeginVariable+2, lengthOfVariable);
+
+			if(variables.contains(variableName)){
+				data.replace(indexOfBeginVariable, lengthOfVariable+3, variables[variableName]);
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+}
+
+
