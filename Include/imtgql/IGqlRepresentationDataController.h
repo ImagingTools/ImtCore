714b75305903f27df8582ef3fe3a5d543ac17b9c
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-11-15 20:07:48 +0000

    Cosmetics
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3801 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtgql/IGqlRepresentationDataController.h
+++ Include/imtgql/IGqlRepresentationDataController.h
@@ -1,26 +1,29 @@
 #pragma once
 
-// ACF includes
-#include <istd/IPolymorphic.h>
-#include <istd/IChangeable.h>
 
 // ImtCore includes
 #include <imtbase/CTreeItemModel.h>
-#include <imtrest/imtrest.h>
 #include <imtgql/CGqlRequest.h>
+
 
 namespace imtgql
 {
 
 
 /**
-	Base interface for all data controllers
-	\ingroup LicenseManagement
+	Interface for GrahpQL request data representation.
 */
 class IGqlRepresentationDataController: virtual public istd::IPolymorphic
 {
 public:
+	/**
+		Get ID of the supported data model.
+	*/
 	virtual QByteArray GetModelId() const = 0;
+
+	/**
+		Create representation data for a given GraphQL request.
+	*/
 	virtual imtbase::CTreeItemModel* CreateResponse(const imtgql::CGqlRequest& gqlRequest, QString& errorMessage) const = 0;
 };
 
