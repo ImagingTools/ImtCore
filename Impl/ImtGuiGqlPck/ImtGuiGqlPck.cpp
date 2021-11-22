a1591c8d5be8fa26917cef239f78238b438d75e1
vs <vs@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-11-22 06:25:12 +0000

    ImtGuiPck added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3824 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Impl/ImtGuiGqlPck/ImtGuiGqlPck.cpp
@@ -0,0 +1,30 @@
+#include "ImtGuiGqlPck.h"
+
+
+// ACF includes
+#include <icomp/export.h>
+
+
+namespace ImtGuiGqlPck
+{
+
+
+I_EXPORT_PACKAGE(
+			"ImtGuiGqlPck",
+			"ImagingTools core framework Gql-component package",
+			IM_PROJECT("\"ImagingTools Core Framework\"") IM_COMPANY("ImagingTools"));
+
+I_EXPORT_COMPONENT(
+			PagesDataController,
+			"Pages data controller",
+			"Pages Data Controller");
+
+I_EXPORT_COMPONENT(
+			CommandsDataController,
+			"Pages data controller",
+			"Pages Data Controller");
+
+
+} // namespace ImtGuiGqlPck
+
+
---
+++ Impl/ImtGuiGqlPck/ImtGuiGqlPck.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+// ACF includes
+#include <icomp/TMakeComponentWrap.h>
+#include <icomp/TModelCompWrap.h>
+
+// ImtCore includes
+#include <imtguigql/CPagesDataControllerComp.h>
+#include <imtguigql/CCommandsDataControllerComp.h>
+
+
+/**
+	ImtGuiPck package
+*/
+namespace ImtGuiGqlPck
+{
+
+
+typedef imtguigql::CPagesDataControllerComp PagesDataController;
+typedef imtguigql::CCommandsDataControllerComp CommandsDataController;
+
+
+} // namespace ImtGuiGqlPck
+
+
