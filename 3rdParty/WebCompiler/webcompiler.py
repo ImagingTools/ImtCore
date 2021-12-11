a489a3e1ef85cced7703550a6c26b844836ea36a
vs <vs@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-12-11 05:58:49 +0000

    webcompiler updated
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3873 21205d9f-8918-2240-a3d5-c39064a2c852

--- 3rdParty/WebCompiler/webcompiler.py
+++ 3rdParty/WebCompiler/webcompiler.py
@@ -5,7 +5,7 @@ import shutil
 import subprocess
 import datetime
 
-print("Script RUN!")
+print("WEB COMPILER RUN!")
 
 
 def copy(folder_from, folder_to):
@@ -24,32 +24,24 @@ if len(sys.argv) > 3:
     for i in range(1, len(sys.argv) - 3, 2):
         copy(sys.argv[i], sys.argv[i + 1])
 
-    GraphQLRequestPath = ""
-    BIN_WEB_DIST_DIR = ""
-    BIN_WEB_DIR = ""
+    GRAPHQL_REQUEST_PATH = ""
+    BUILD_DIR = sys.argv[2]
+    BUILD_DIST_DIR = BUILD_DIR + "/dist"
+
     for dir in sys.argv:
         if dir.replace("\\", "/").endswith("ImtCore/Include/imtqml/Qml/imtqml"):
-            GraphQLRequestPath = dir + "/GraphQLRequest.js"
-        if dir.replace("\\", "/").endswith("ImtCore/Bin/web/dist"):
-            BIN_WEB_DIST_DIR = dir
-        if dir.replace("\\", "/").endswith("ImtCore/Bin/web"):
-            BIN_WEB_DIR = dir
-    if os.path.isfile(GraphQLRequestPath):
-        if not os.path.isdir(BIN_WEB_DIST_DIR):
-            print("Bin/web/dist directory not found!")
-        else:
-            print("copy from ", GraphQLRequestPath, " to ", BIN_WEB_DIST_DIR)
-            shutil.copy(GraphQLRequestPath, BIN_WEB_DIST_DIR)
+            GRAPHQL_REQUEST_PATH = dir + "/GraphQLRequest.js"
+
+    if os.path.isfile(GRAPHQL_REQUEST_PATH):
+        print("copy from ", GRAPHQL_REQUEST_PATH, " to ", BUILD_DIST_DIR)
+        shutil.copy(GRAPHQL_REQUEST_PATH, BUILD_DIST_DIR)
     else:
         print("GraphQLRequest.js file not found!")
-    if not os.path.isdir(BIN_WEB_DIR):
-        print("Bin/web directory not found!")
-    else:
-    # dir_build = sys.argv[2]
-        python_path = sys.argv[len(sys.argv) - 1]
-        process = subprocess.Popen(f'{python_path}  qmlcore/build.py', shell=True, cwd=BIN_WEB_DIR)
-        process.wait()
-        build_from = sys.argv[len(sys.argv) - 3]
-        build_to = sys.argv[len(sys.argv) - 2]
-        copy(build_from, build_to)
-print("End script!")
+
+    python_path = sys.argv[len(sys.argv) - 1]
+    process = subprocess.Popen(f'{python_path}  qmlcore/build.py', shell=True, cwd=BUILD_DIR)
+    process.wait()
+    build_from = sys.argv[len(sys.argv) - 3]
+    build_to = sys.argv[len(sys.argv) - 2]
+    copy(build_from, build_to)
+print("WEB COMPILER COMPLETED!")
