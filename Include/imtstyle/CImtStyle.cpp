2231ae1e26d83799bb7b853d7fff4cd379fe612f
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-12-21 20:27:15 +0000

    Switch of the design optimized
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3920 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtstyle/CImtStyle.cpp
+++ Include/imtstyle/CImtStyle.cpp
@@ -1,23 +1,3 @@
-/********************************************************************************
-**
-**	Copyright (C) 2017-2020 ImagingTools GmbH
-**
-**	This file is part of the ImagingTools SDK.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-**
-********************************************************************************/
-
-
 #include <imtstyle/CImtStyle.h>
 
 
@@ -129,16 +109,18 @@ CImtStyle::DesignSchema CImtStyle::GetDesignSchema() const
 void CImtStyle::SetDesignSchema(DesignSchema designSchema)
 {
 	if (m_blockerCount == 0){
-		if (qApp != nullptr){
-			BaseClass::unpolish(qApp);
-		}
+		if (m_designSchema != designSchema){
+			if (qApp != nullptr){
+				BaseClass::unpolish(qApp);
+			}
 
-		m_designSchema = designSchema;
+			m_designSchema = designSchema;
 
-		EnsureStyleSheetApplied(true);
+			EnsureStyleSheetApplied(true);
 
-		if (qApp != nullptr){
-			BaseClass::polish(qApp);
+			if (qApp != nullptr){
+				BaseClass::polish(qApp);
+			}
 		}
 	}
 }
