b6175661a6b3737e0827bd15e961e84fd7e120e9
vs <vs@21205d9f-8918-2240-a3d5-c39064a2c852>
2021-12-24 04:53:41 +0000

    CFeaturePackageListDataController added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@3946 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtguigql/CFeaturePackageListDataControllerComp.h
@@ -0,0 +1,35 @@
+#pragma once
+
+// ImtCore includes
+#include <imtgql/CGqlRepresentationDataControllerCompBase.h>
+#include <imtbase/IItemBasedRepresentationDataProvider.h>
+#include <imtbase/IObjectCollection.h>
+#include <imtgui/ICollectionViewDelegate.h>
+
+
+namespace imtguigql
+{
+
+
+class CFeaturePackagelistDataControllerComp: public imtgql::CGqlRepresentationDataControllerCompBase
+{
+public:
+	typedef imtgql::CGqlRepresentationDataControllerCompBase BaseClass;
+
+	I_BEGIN_COMPONENT(CFeaturePackagelistDataControllerComp);
+		I_REGISTER_INTERFACE(imtbase::IItemBasedRepresentationDataProvider);
+		I_ASSIGN(m_viewDelegateCompPtr, "ViewDelegate", "View delegate (corresponding with the object type) used for the collection", true, "FeaturePackageViewDelegate");
+		I_ASSIGN(m_objectCollectionCompPtr, "ObjectCollection", "Object collection", true, "ObjectCollection");
+	I_END_COMPONENT;
+
+
+// reimplemented (imtgql::IGqlRepresentationDataController)
+	virtual imtbase::CTreeItemModel* CreateResponse(const imtgql::CGqlRequest& gqlRequest, QString& errorMessage) const override;
+
+private:
+	I_REF(imtgui::ICollectionViewDelegate, m_viewDelegateCompPtr);
+	I_REF(imtbase::IObjectCollection, m_objectCollectionCompPtr);
+};
+
+
+} // namespace imtguigql
