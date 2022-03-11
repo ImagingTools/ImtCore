98de089a14a95bcfa9900333b595119d8df72c08
vs <vs@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-03-11 11:49:14 +0000

    IGqlMutationDataControllerDelegate.h added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@4198 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtgql/IGqlMutationDataControllerDelegate.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+// ACF includes
+#include <istd/IChangeable.h>
+#include <imtbase/CTreeItemModel.h>
+
+#include <imtgql/CGqlObject.h>
+
+
+namespace imtgql
+{
+
+
+/**
+	Base interface for representation data controllers
+*/
+class IGqlMutationDataControllerDelegate: virtual public istd::IPolymorphic
+{
+public:
+	virtual imtbase::CTreeItemModel* UpdateBaseModelFromRepresentation(
+				const QList<imtgql::CGqlObject>& params,
+				imtbase::CTreeItemModel* baseModel) = 0;
+};
+
+
+} // namespace imtgql
+
+
