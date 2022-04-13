757966d4f9823c599961e07922534c1049241e9e
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-04-13 06:20:53 +0000

    quazip cmake for win corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@4292 21205d9f-8918-2240-a3d5-c39064a2c852

--- Config/CMake/Quazip.cmake
+++ Config/CMake/Quazip.cmake
@@ -6,7 +6,7 @@ add_compile_definitions(QUAZIP_STATIC)
 
 
 if(WIN32)
-	target_link_libraries(${PROJECT_NAME} ${QUAZIP}/lib/${CMAKE_BUILD_TYPE}${TARGETNAME}/quazip.lib)
+	target_link_libraries(${PROJECT_NAME} ${QUAZIP}/lib/${CMAKE_BUILD_TYPE}_${TARGETNAME}/quazip.lib)
 endif()
 
 if(UNIX)
