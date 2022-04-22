417df5204b41071ac848818352fcdf4b8a5f27e7
Vladimir.Cherepanov <Vladimir.Cherepanov@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-04-22 11:01:46 +0000

    Prepare for tests
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@4346 21205d9f-8918-2240-a3d5-c39064a2c852

--- Tests/Test1/Test1Main.cpp
+++ Tests/Test1/Test1Main.cpp
@@ -1,11 +1,25 @@
 #include <itest/CStandardTestExecutor.h>
 
+
 // Qt includes
 #include <QtWidgets/QApplication>
+
+// ACF includes
+#include <ipackage/CComponentAccessor.h>
+
+// ImtCore includes
+#include <imtbase/IObjectCollection.h>
 
 
 int main(int argc, char *argv[])
 {
+	QString imtCorePath = qEnvironmentVariable("IMTCOREDIR");
+	QString registryFile = imtCorePath + "/Tests/Partitura/ImtTestVoce.arp/Test1.acc";
+	QString configFile = imtCorePath + "/Config/ImtCoreTest.awc";
+	ipackage::CComponentAccessor accessor(registryFile, configFile);
+
+	imtbase::IObjectCollection* objectCollectionPtr = nullptr;
+	objectCollectionPtr = accessor.GetComponentInterface<imtbase::IObjectCollection>();
 
 	QApplication app(argc, argv);
 	itest::CStandardTestExecutor instance;
