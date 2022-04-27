ebf75135c3a595a52856ce1ef6e74f15cd8b8d8b
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-04-27 06:12:28 +0000

    Collection tests corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@4366 21205d9f-8918-2240-a3d5-c39064a2c852

--- Tests/ObjectCollectionTest/CObjectCollectionTest.cpp
+++ Tests/ObjectCollectionTest/CObjectCollectionTest.cpp
@@ -66,31 +66,27 @@ void CObjectCollectionTest::InsertNewObjectWithRequiredParamsTest()
 void CObjectCollectionTest::InsertNewObjectWithDataTest()
 {
 	if (m_compositePtr != nullptr){
-
 		imtbase::IObjectCollection* objectCollectionPtr = m_compositePtr->GetComponentInterface<imtbase::IObjectCollection>();
-
 		if (objectCollectionPtr != nullptr){
-
 			objectCollectionPtr->ResetData();
-			imtbase::IObjectCollection::DataPtr dataInObject = new imtauth::CAccountInfo();
-			imtbase::IObjectCollection::DataPtr dataInObject2 = new imtauth::CAccountInfo();
-			imtauth::CAccountInfo* accountInfoPtr = dynamic_cast<imtauth::CAccountInfo*>(dataInObject.GetPtr());
-			imtauth::CAccountInfo* accountInfo2Ptr = dynamic_cast<imtauth::CAccountInfo*>(dataInObject2.GetPtr());
+			imtbase::IObjectCollection::DataPtr inputDataPtr = new imtauth::CAccountInfo();
+			imtbase::IObjectCollection::DataPtr referenceDataPtr;
 
 			QString nameAccount = "AccountName";
 			QString descriptionAccount = "AccountDescription";
 			QString setNameAccount;
 			QString setDescriptionAccount;
-			accountInfoPtr->SetAccountName(nameAccount);
-			accountInfoPtr->SetAccountDescription(descriptionAccount);
+			imtauth::CAccountInfo* inputImplPtr = dynamic_cast<imtauth::CAccountInfo*>(inputDataPtr.GetPtr());
+			inputImplPtr->SetAccountName(nameAccount);
+			inputImplPtr->SetAccountDescription(descriptionAccount);
 
-			QByteArray idNewObject = objectCollectionPtr->InsertNewObject(m_typeIdObjectCollection, "TestObject", "TestDescription", dataInObject);
-
+			QByteArray idNewObject = objectCollectionPtr->InsertNewObject(m_typeIdObjectCollection, "TestObject", "TestDescription", inputDataPtr);
 			if (!idNewObject.isEmpty()){
+				objectCollectionPtr->GetObjectData(idNewObject, referenceDataPtr);
 
-				objectCollectionPtr->GetObjectData(idNewObject, dataInObject2);
-				setNameAccount = accountInfo2Ptr->GetAccountName();
-				setDescriptionAccount = accountInfo2Ptr->GetAccountDescription();
+				imtauth::CAccountInfo* referenceImplPtr = dynamic_cast<imtauth::CAccountInfo*>(referenceDataPtr.GetPtr());
+				setNameAccount = referenceImplPtr->GetAccountName();
+				setDescriptionAccount = referenceImplPtr->GetAccountDescription();
 				QVERIFY2(((nameAccount == setNameAccount) && (descriptionAccount == setDescriptionAccount)), "Insert new object with data is failed");
 			}
 			else{
@@ -110,14 +106,13 @@ void CObjectCollectionTest::InsertNewObjectWithDataTest()
 void CObjectCollectionTest::InsertNewObjectWithNonExistElementTest()
 {
 	if (m_compositePtr != nullptr){
-
 		imtbase::IObjectCollection* objectCollectionPtr = m_compositePtr->GetComponentInterface<imtbase::IObjectCollection>();
-
 		if (objectCollectionPtr != nullptr){
-
 			objectCollectionPtr->ResetData();
+
 			QByteArray idNewObject = objectCollectionPtr->InsertNewObject(m_typeIdObjectCollection, "TestObject", "TestDescription", imtbase::IObjectCollection::DataPtr(), "testId");
 			QVector<QByteArray> idsInObject = objectCollectionPtr->GetElementIds();
+
 			bool checkIdIsExist = idsInObject.contains(idNewObject);
 
 			QVERIFY2(checkIdIsExist, "Insert object with set non-exist id is failed");
@@ -127,23 +122,22 @@ void CObjectCollectionTest::InsertNewObjectWithNonExistElementTest()
 			QFAIL("Object collection is nullptr");
 		}
 	}
-
 	else{
 		QFAIL("Component is not initialized");
 	}
 }
 
+
 void CObjectCollectionTest::InsertNewObjectWithExistElementTest()
 {
 	if (m_compositePtr != nullptr){
-
 		imtbase::IObjectCollection* objectCollectionPtr = m_compositePtr->GetComponentInterface<imtbase::IObjectCollection>();
-
 		if (objectCollectionPtr != nullptr){
-
 			objectCollectionPtr->ResetData();
+
 			objectCollectionPtr->InsertNewObject(m_typeIdObjectCollection, "TestObject", "TestDescription", imtbase::IObjectCollection::DataPtr(), "testId");
 			objectCollectionPtr->InsertNewObject(m_typeIdObjectCollection, "TestObject2", "TestDescription2", imtbase::IObjectCollection::DataPtr(), "testId");
+
 			QVector<QByteArray> idsInObject = objectCollectionPtr->GetElementIds();
 
 			QVERIFY2((idsInObject.size() == 2), "Insert new object with set exist id is failed");
@@ -153,52 +147,59 @@ void CObjectCollectionTest::InsertNewObjectWithExistElementTest()
 			QFAIL("Object collection is nullptr");
 		}
 	}
-
 	else{
 		QFAIL("Component is not initialized");
 	}
 }
+
 
 void CObjectCollectionTest::RemoveExistObjectTest()
 {
 	if (m_compositePtr != nullptr){
-
 		imtbase::IObjectCollection* objectCollectionPtr = m_compositePtr->GetComponentInterface<imtbase::IObjectCollection>();
 
 		if (objectCollectionPtr != nullptr){
-
+			// Reset collection:
 			objectCollectionPtr->ResetData();
+
+			// Insert two objects into the collection:
 			objectCollectionPtr->InsertNewObject(m_typeIdObjectCollection, "TestObject", "TestDescription", imtbase::IObjectCollection::DataPtr(), "testId");
 			objectCollectionPtr->InsertNewObject(m_typeIdObjectCollection, "TestObject2", "TestDescription2", imtbase::IObjectCollection::DataPtr(), "testId2");
+
+			// Remove object with the ID 'testId':
 			objectCollectionPtr->RemoveObject("testId");
 
-			QVector<QByteArray> idsInObjectAfterRemove = objectCollectionPtr->GetElementIds();
+			// Get IDs in the collection after removing of the 'testId':
+			imtbase::IObjectCollection::Ids idsInObjectAfterRemove = objectCollectionPtr->GetElementIds();
+
+			// Check if the removed object is not existing any more:
+			bool wasObjectRemoved = !idsInObjectAfterRemove.contains("testId");
+			QVERIFY2(wasObjectRemoved, "Object could not be removed");
+
+			// Check if the not removed object exists:
 			bool checkExistAnotherId = idsInObjectAfterRemove.contains("testId2");
-
-			QVERIFY2(((idsInObjectAfterRemove.size() == 1) && checkExistAnotherId), "Remove exist object is failed");
+			QVERIFY2(checkExistAnotherId, "Not affected object was removed");
 		}
-
 		else{
 			QFAIL("Object collection is nullptr");
 		}
 	}
-
 	else{
 		QFAIL("Component is not initialized");
 	}
 }
 
+
 void CObjectCollectionTest::RemoveNonExistObjectTest()
 {
 	if (m_compositePtr != nullptr){
-
 		imtbase::IObjectCollection* objectCollectionPtr = m_compositePtr->GetComponentInterface<imtbase::IObjectCollection>();
-
 		if (objectCollectionPtr != nullptr){
-
 			objectCollectionPtr->ResetData();
+
 			objectCollectionPtr->InsertNewObject(m_typeIdObjectCollection, "TestObject", "TestDescription", imtbase::IObjectCollection::DataPtr(), "testId");
 			objectCollectionPtr->InsertNewObject(m_typeIdObjectCollection, "TestObject2", "TestDescription2", imtbase::IObjectCollection::DataPtr(), "testId2");
+
 			objectCollectionPtr->RemoveObject("testId3");
 
 			QVector<QByteArray> idsInObjectAfterRemove = objectCollectionPtr->GetElementIds();
@@ -211,36 +212,30 @@ void CObjectCollectionTest::RemoveNonExistObjectTest()
 			QFAIL("Object collection is nullptr");
 		}
 	}
-
 	else{
 		QFAIL("Component is not initialized");
 	}
-
 }
 
 
 void CObjectCollectionTest::CheckSerializeTest()
 {
 	if (m_compositePtr != nullptr){
-
 		imtbase::IObjectCollection* objectCollectionPtr = m_compositePtr->GetComponentInterface<imtbase::IObjectCollection>();
-
 		if (objectCollectionPtr != nullptr){
-
 			objectCollectionPtr->ResetData();
 
 			iser::ISerializable* dataPtr = m_compositePtr->GetComponentInterface<iser::ISerializable>();
 			QString pathToFile = qEnvironmentVariable("IMTCOREDIR")+"/Tests/ObjectCollectionTest/TestData/testData.xml";
+
 			ifile::CCompactXmlFileReadArchive readArchive(pathToFile);
 			dataPtr->Serialize(readArchive);
 
-			imtbase::IObjectCollection::DataPtr dataInObject = new imtauth::CAccountInfo();
-
-			if (objectCollectionPtr->GetObjectData("{98c7bd83-2541-427a-a9d2-2427e1d32618}", dataInObject)){
-
-				imtauth::CAccountInfo* accountInfoPtr = dynamic_cast<imtauth::CAccountInfo*>(dataInObject.GetPtr());
-				QString nameAccount = accountInfoPtr->GetAccountName();
-				QString descriptionAccount = accountInfoPtr->GetAccountDescription();
+			imtbase::IObjectCollection::DataPtr inputDataPtr;
+			if (objectCollectionPtr->GetObjectData("{98c7bd83-2541-427a-a9d2-2427e1d32618}", inputDataPtr)){
+				imtauth::CAccountInfo* inputImplPtr = dynamic_cast<imtauth::CAccountInfo*>(inputDataPtr.GetPtr());
+				QString nameAccount = inputImplPtr->GetAccountName();
+				QString descriptionAccount = inputImplPtr->GetAccountDescription();
 				QVERIFY2(((nameAccount == "AccountName") && (descriptionAccount == "AccountDescription")), "Serialize data from file in ibject collection is failed");
 			}
 			else{
@@ -260,7 +255,11 @@ void CObjectCollectionTest::CheckSerializeTest()
 void CObjectCollectionTest::cleanupTestCase()
 {
 	m_typeIdObjectCollection.clear();
+
 	m_compositePtr.Reset();
 }
 
+
 I_ADD_TEST(CObjectCollectionTest);
+
+
