a1e336c147f545ef2bd73ff594a1658659362c89
Sergej.Gorbunov <Sergej.Gorbunov@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-05-05 11:15:02 +0000

    add function in CMake for run python script (generate tests in ImtCore)
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@4398 21205d9f-8918-2240-a3d5-c39064a2c852

--- Config/CMake/ImtCore.cmake
+++ Config/CMake/ImtCore.cmake
@@ -3,3 +3,27 @@ include(${ACFDIR}/Config/CMake/AcfStd.cmake)
 if(WIN32)
 	target_link_libraries(${PROJECT_NAME} advapi32 user32)
 endif()
+
+function(imtcore_generate_tests)
+        set(PYTHONEXE ${IMTCOREDIR}/3rdParty/Python/3.8/python.exe)
+	set(GENERATE_H_TESTS_FILE ${IMTCOREDIR}/Tests/ObjectCollectionTest/CGenerateObjectCollectionTest.h)
+	set(PARTITURA_DIR ${IMTCOREDIR}/Tests/ObjectCollectionTest/Partitura)
+
+	if (NOT WIN32)
+	        set(PYTHONEXE python3)
+	endif()
+
+	add_custom_command(
+	        OUTPUT ${GENERATE_H_TESTS_FILE}
+		PRE_BUILD
+		COMMAND ${PYTHONEXE} ${IMTCOREDIR}/Tests/ObjectCollectionTest/CGenerateObjectCollectionTest.py ${PARTITURA_DIR}
+		COMMENT
+		"Generate tests for ImtCore"
+	        )
+
+	add_custom_target(
+	        GenereteTestHandler${PROJECT_NAME} ALL
+		DEPENDS ${GENERATE_H_TESTS_FILE}
+	        )
+
+endfunction(imtcore_generate_tests)
