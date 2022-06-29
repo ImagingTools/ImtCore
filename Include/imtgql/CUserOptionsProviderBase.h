2d41219ff7c4e642c0a0846d76a025ecc2e63a31
Sergej.Gorbunov <Sergej.Gorbunov@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-06-29 04:14:54 +0000

    add CUserOptionsProviderBase in imtgql
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@4558 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtgql/CUserOptionsProviderBase.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+//ImtCore includes
+#include <imtgql/CGqlObject.h>
+
+
+namespace imtgql
+{
+
+
+/**
+	Client options provider
+*/
+class CUserOptionsProviderBase
+{
+
+public:
+	QByteArray GetLanguageIdFromInputParams(const QList<imtgql::CGqlObject>& inputParams) const;
+	QByteArray GetDesignSchemeIdFromInputParams(const QList<imtgql::CGqlObject>& inputParams) const;
+};
+
+
+} // namespace imtqml
