609694d4b749265972a2defd7fc2f61c56bfa3ae
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-07-13 18:32:28 +0000

    Installation script made more flexible
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@4587 21205d9f-8918-2240-a3d5-c39064a2c852

--- Install/_Create_VC15_OpenSource.bat
+++ Install/_Create_VC15_OpenSource.bat
@@ -1,9 +1,15 @@
 echo off
 
+set ACFDIR=%~dp0..\..\..\Acf
+set ACFCONFIGDIR=%~dp0..\..\..\Acf\Config
+set ACFSLNDIR=%~dp0..\..\..\AcfSln
+set IACFDIR=%~dp0..\..\..\IAcf
+set IMTCOREDIR=%~dp0..\
+
 set LICENSE_TYPE=OpenSource
-set LICENSE_DIR=Install/LGPL
-set LICENSE_INSTALL_PATH=../LGPL/License.txt
-set ACF_CONFIG_FILE=$(IMTCOREDIR)/Config/ImtCore.awc
+set LICENSE_DIR=Install\LGPL
+set LICENSE_INSTALL_PATH=..\LGPL\License.txt
+set ACF_CONFIG_FILE=$(IMTCOREDIR)\Config\ImtCore.awc
 
 cd %~dp0\..
 
