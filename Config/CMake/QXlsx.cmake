d2cb2f0eaa7dad4aa79b5aceab3e546d43e1c2ce
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-10-19 07:34:10 +0000

    add QXlsx lib
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@4912 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Config/CMake/QXlsx.cmake
@@ -0,0 +1,20 @@
+cmake_minimum_required(VERSION 3.19)
+
+if(WIN32)
+        set(XLSX_LIB_SEARCH_PATH "${CMAKE_CURRENT_LIST_DIR}/../../3rdParty/QXlsx/Lib/${CMAKE_BUILD_TYPE}${TARGETNAME}")
+else()
+        set(XLSX_LIB_SEARCH_PATH "${CMAKE_CURRENT_LIST_DIR}/../../3rdParty/QXlsx/Lib/${TARGETNAME}")
+endif()
+
+target_include_directories(${PROJECT_NAME} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/../../3rdParty/QXlsx/header)
+
+find_library(
+        XLSX_LIB_PATH
+        QXlsx
+        ${XLSX_LIB_SEARCH_PATH}
+)
+
+#target_link_libraries(${PROJECT_NAME} ${XLSX_LIB_PATH})
+
+
+
