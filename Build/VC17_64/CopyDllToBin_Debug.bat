9ed64130ef83c6bb39b0303d7ff42a7b912ce62f
Vladimir.Cherepanov <Vladimir.Cherepanov@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-10-28 13:58:09 +0000

    Qt6_VC17 build support
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@4951 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Build/VC17_64/CopyDllToBin_Debug.bat
@@ -0,0 +1,1 @@
+copy /Y %OPENCVDIR_3_3_1%\bin\DebugVC17_64\*.dll ..\..\Bin\DebugVC17_64\TestPlugins
---
+++ Build/VC17_64/CopyDllToBin_Release.bat
@@ -0,0 +1,1 @@
+copy /Y %OPENCVDIR_3_3_1%\bin\ReleaseVC17_64\*.dll ..\..\Bin\ReleaseVC17_64\TestPlugins
