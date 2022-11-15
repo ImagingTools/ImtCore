492bf9fd0ca242910db1893f6314f36e62fb6fa4
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-11-15 12:15:51 +0000

    ImtCore Quazip cmake updated
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@5096 21205d9f-8918-2240-a3d5-c39064a2c852

--- Config/CMake/Quazip.cmake
+++ Config/CMake/Quazip.cmake
@@ -4,8 +4,11 @@ include_directories(${QUAZIP})
 
 add_compile_definitions(QUAZIP_STATIC)
 
+message("CMAKE_BUILD_TYPE ${CMAKE_BUILD_TYPE}_${TARGETNAME}")
+message("QUAZIP ${QUAZIP}")
 
 if(WIN32)
+	message("WIN32")
 	target_link_libraries(${PROJECT_NAME} ${QUAZIP}/lib/${CMAKE_BUILD_TYPE}_${TARGETNAME}/quazip.lib)
 endif()
 
