3f3de46c748d00ad30c870ede618afdca16f7bfc
Evgeniy.Batulko <Evgeniy.Batulko@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-11-30 06:09:29 +0000

    add report invoice
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@5263 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtbase/imtbase.cpp
+++ Include/imtbase/imtbase.cpp
@@ -9,31 +9,32 @@ namespace imtbase
 QByteArray GetTranslation(const iqt::ITranslationManager* translationManagerPtr, const QByteArray& phrase, const QByteArray& languageId, const QByteArray& context)
 {
 	int currentIndex = -1;
+	if(translationManagerPtr != nullptr){
+		if (languageId.isEmpty()){
+			currentIndex = translationManagerPtr->GetCurrentLanguageIndex();
+		}
+		else{
+			currentIndex = iprm::FindOptionIndexById(languageId, translationManagerPtr->GetLanguagesInfo());
+		}
 
-	if (languageId.isEmpty()){
-		currentIndex = translationManagerPtr->GetCurrentLanguageIndex();
-	}
-	else{
-		currentIndex = iprm::FindOptionIndexById(languageId, translationManagerPtr->GetLanguagesInfo());
-	}
-
-	if (currentIndex >= 0){
-		QString phraseTr;
-		const QTranslator* translatorPtr = translationManagerPtr->GetLanguageTranslator(currentIndex);
-		if (translatorPtr != nullptr){
-			phraseTr = translatorPtr->translate(context, phrase);
-			if(phraseTr.isEmpty()){
-				const iqt::ITranslationManager* slaveTranslatorManager = translationManagerPtr->GetSlaveTranslationManager();
-				if(slaveTranslatorManager != nullptr){
-					GetTranslation(slaveTranslatorManager, phrase, languageId, context);
+		if (currentIndex >= 0){
+			QString phraseTr;
+			const QTranslator* translatorPtr = translationManagerPtr->GetLanguageTranslator(currentIndex);
+			if (translatorPtr != nullptr){
+				phraseTr = translatorPtr->translate(context, phrase);
+				if(phraseTr.isEmpty()){
+					const iqt::ITranslationManager* slaveTranslatorManager = translationManagerPtr->GetSlaveTranslationManager();
+					if(slaveTranslatorManager != nullptr){
+						GetTranslation(slaveTranslatorManager, phrase, languageId, context);
+					}
+					else{
+						return phrase;
+					}
 				}
-				else{
-					return phrase;
+				else
+				{
+					return phraseTr.toUtf8();
 				}
-			}
-			else
-			{
-				return phraseTr.toUtf8();
 			}
 		}
 	}
