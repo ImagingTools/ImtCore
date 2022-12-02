5bc1d65f7c274451cf16007b63eb86e1279dac9b
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2022-12-02 14:59:57 +0000

    Ej nix cmake modifier
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@5303 21205d9f-8918-2240-a3d5-c39064a2c852

--- Config/CMake/EjDoc.cmake
+++ Config/CMake/EjDoc.cmake
@@ -2,4 +2,6 @@ include_directories(${IMTCOREDIR}/3rdParty/EJ/include)
 
 if(WIN32)
 	target_link_libraries(${PROJECT_NAME} "${IMTCOREDIR}/3rdParty/EJ/libs/${CMAKE_BUILD_TYPE}_${TARGETNAME}/EjDoc.lib")
+elseif(UNIX)
+	target_link_libraries(${PROJECT_NAME} "${IMTCOREDIR}/3rdParty/EJ/libs/${CMAKE_BUILD_TYPE}_${TARGETNAME}/libEjDoc.a")
 endif()
--- Config/CMake/EjExport.cmake
+++ Config/CMake/EjExport.cmake
@@ -5,5 +5,7 @@ include_directories(${IMTCOREDIR}/3rdParty/EJ/EjExport/mini_zipper/)
 
 if(WIN32)
 	target_link_libraries(${PROJECT_NAME} "${IMTCOREDIR}/3rdParty/EJ/libs/${CMAKE_BUILD_TYPE}_${TARGETNAME}/EjExport.lib")
+elseif(UNIX)
+	target_link_libraries(${PROJECT_NAME} "${IMTCOREDIR}/3rdParty/EJ/libs/${CMAKE_BUILD_TYPE}_${TARGETNAME}/libEjExport.a")
 endif()
 
