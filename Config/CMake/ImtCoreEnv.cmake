88b8cd8130222be60f97f09c41a9102a51358343
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-01-29 11:44:35 +0000

    CMake projects corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@5808 21205d9f-8918-2240-a3d5-c39064a2c852

--- Config/CMake/ImtCoreEnv.cmake
+++ Config/CMake/ImtCoreEnv.cmake
@@ -1,47 +1,41 @@
 cmake_minimum_required(VERSION 3.1.0)
 
+
 if(NOT DEFINED IACFDIR)
 	set(IACFDIR "$ENV{IACFDIR}")
 endif()
-if(NOT DEFINED IMTROOTDIR)
-	set(IMTROOTDIR "$ENV{IMTROOTDIR}")
-endif()
+
+include(${IACFDIR}/Config/CMake/IAcfEnv.cmake)
+
 if(NOT DEFINED IMTCOREDIR)
 	set(IMTCOREDIR "$ENV{IMTCOREDIR}")
 endif()
-if(NOT DEFINED ACULADIR)
-	set(ACULADIR "$ENV{ACULADIR}")
-endif()
-if(NOT DEFINED LISADIR)
-	set(LISADIR "$ENV{LISADIR}")
-endif()
+
 if(NOT DEFINED OPENSSLDIR)
     set(OPENSSLDIR "$ENV{OPENSSLDIR}")
     if(OPENSSLDIR STREQUAL "")
         set(OPENSSLDIR "${IMTCOREDIR}/3rdParty/openssl/1.1")
     endif()
 endif()
+
 if(NOT DEFINED QUAZIP)
 	set(QUAZIP "${IMTCOREDIR}/3rdParty/quazip")
 endif()
 if(NOT DEFINED PYTHON38DIR)
 	set(PYTHON38DIR "${IMTCOREDIR}/3rdParty/Python/3.8")
 endif()
-if(NOT DEFINED ACFCONFIGDIR)
-	set(ACFCONFIGDIR "$ENV{ACFCONFIGDIR}")
+
+if(NOT DEFINED ACFPUBLICDIR)
+	set(ACFPUBLICDIR "$ENV{ACFPUBLICDIR}")
 endif()
+
 if(NOT DEFINED ZLIBDIR)
 	set(ZLIBDIR "$ENV{ZLIBDIR}")
 	if(ZLIBDIR STREQUAL "")
-		set(ZLIBDIR "${IACFDIR}/../3rdParty/zlib")
+		set(ZLIBDIR "${ACFPUBLICDIR}/3rdParty/zlib")
 	endif()
 endif()
-if(NOT DEFINED NICPDIR)
-	set(NICPDIR "${ACULADIR}/3rdParty/nicp")
-endif()
 
-
-include(${IACFDIR}/Config/CMake/IAcfEnv.cmake)
 
 include_directories("${IMTCOREDIR}/AuxInclude/${TARGETNAME}")
 include_directories("${IMTCOREDIR}/Include")
