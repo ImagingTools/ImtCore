2c5cac03cb58e34488d3d9beaf79266bd90434c2
Vladimir.Cherepanov <Vladimir.Cherepanov@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-02-03 12:33:06 +0000

    Revert quazip.cmake
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@5888 21205d9f-8918-2240-a3d5-c39064a2c852

--- Config/CMake/Quazip.cmake
+++ Config/CMake/Quazip.cmake
@@ -4,11 +4,8 @@ include_directories(${QUAZIP})
 
 add_compile_definitions(QUAZIP_STATIC)
 
-message("CMAKE_BUILD_TYPE ${CMAKE_BUILD_TYPE}_${TARGETNAME}")
-message("QUAZIP ${QUAZIP}")
 
 if(WIN32)
-	message("WIN32")
 	target_link_libraries(${PROJECT_NAME} ${QUAZIP}/lib/${CMAKE_BUILD_TYPE}_${TARGETNAME}/quazip.lib)
 endif()
 
