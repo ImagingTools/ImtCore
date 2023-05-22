23010914679beab7cab4c8dd4bce98565c2f258e
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-05-22 07:17:49 +0000

    JQML. add preventStealing for MouseArea
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@6519 21205d9f-8918-2240-a3d5-c39064a2c852

--- 3rdParty/JQML/Core/src/components/MouseArea.js
+++ 3rdParty/JQML/Core/src/components/MouseArea.js
@@ -201,7 +201,7 @@ export class MouseArea extends Item {
 		if(this.$p.enabled.val && (this.$p.pressed.val || this.$p.hoverEnabled.val)){
 			this.$fillMouse(e)
 
-			if(this.$p.pressed.val && state.view && (Math.abs(this.mouse.x-this.tempMouse.x) > 10 || Math.abs(this.mouse.y-this.tempMouse.y) > 10)){
+			if(this.$p.pressed.val && state.view && (Math.abs(this.mouse.x-this.tempMouse.x) > 10 || Math.abs(this.mouse.y-this.tempMouse.y) > 10) && !this.preventStealing){
 				// console.log('CONSOLE::', this)
 				this.pressed = false
 				this.containsMouse = false
@@ -312,7 +312,7 @@ export class MouseArea extends Item {
 		e.preventDefault()
 		if(this.$p.enabled.val && (this.$p.pressed.val || this.$p.hoverEnabled.val)){
 			this.$fillMouse(e)
-			if(this.$p.pressed.val && state.view && (Math.abs(this.mouse.x-this.tempMouse.x) > 10 || Math.abs(this.mouse.y-this.tempMouse.y) > 10)){
+			if(this.$p.pressed.val && state.view && (Math.abs(this.mouse.x-this.tempMouse.x) > 10 || Math.abs(this.mouse.y-this.tempMouse.y) > 10) && !this.preventStealing){
 				// console.log('CONSOLE::', this)
 				this.pressed = false
 				this.containsMouse = false
