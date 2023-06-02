09a412050a0163e8b4b2cb5ab452aa6109d039e1
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-06-02 08:11:54 +0000

    imtgeo/CDeviceMapClusterCollectionControllerComp.cpp
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@6644 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtgeo/CDeviceMapClusterCollectionControllerComp.cpp
+++ Include/imtgeo/CDeviceMapClusterCollectionControllerComp.cpp
@@ -17,152 +17,208 @@ namespace imtgeo
 
 
 QVariant CDeviceMapClusterCollectionControllerComp::GetObjectInformation(
-		const QByteArray &informationId,
-		const QByteArray &objectId) const
+        const QByteArray &informationId,
+        const QByteArray &objectId) const
 {
-	idoc::MetaInfoPtr metaInfo = m_objectCollectionCompPtr->GetElementMetaInfo(objectId);
-	if (metaInfo.IsValid()){
-		if (informationId == QByteArray("Added")){
-			return metaInfo->GetMetaInfo(idoc::IDocumentMetaInfo::MIT_CREATION_TIME);
-		}
-		else if (informationId == QByteArray("LastModified")){
-			return metaInfo->GetMetaInfo(idoc::IDocumentMetaInfo::MIT_MODIFICATION_TIME);
-		}
-	}
+    idoc::MetaInfoPtr metaInfo = m_objectCollectionCompPtr->GetElementMetaInfo(objectId);
+    if (metaInfo.IsValid()){
+        if (informationId == QByteArray("Added")){
+            return metaInfo->GetMetaInfo(idoc::IDocumentMetaInfo::MIT_CREATION_TIME);
+        }
+        else if (informationId == QByteArray("LastModified")){
+            return metaInfo->GetMetaInfo(idoc::IDocumentMetaInfo::MIT_MODIFICATION_TIME);
+        }
+    }
 
-	return QVariant();
+    return QVariant();
 }
 
 
 imtbase::CTreeItemModel* CDeviceMapClusterCollectionControllerComp::GetMetaInfo(
-		const imtgql::CGqlRequest& gqlRequest,
-		QString& errorMessage) const
+        const imtgql::CGqlRequest& gqlRequest,
+        QString& errorMessage) const
 {
-	istd::TDelPtr<imtbase::CTreeItemModel> rootModelPtr(new imtbase::CTreeItemModel());
-	return rootModelPtr.PopPtr();
+    istd::TDelPtr<imtbase::CTreeItemModel> rootModelPtr(new imtbase::CTreeItemModel());
+    return rootModelPtr.PopPtr();
 }
 
 
 imtbase::CTreeItemModel* CDeviceMapClusterCollectionControllerComp::ListObjects(const imtgql::CGqlRequest& gqlRequest, QString& errorMessage) const
 {
-	istd::TDelPtr<imtbase::CTreeItemModel> rootModelPtr(new imtbase::CTreeItemModel());
-	imtbase::CTreeItemModel* dataModel = nullptr;
-	imtbase::CTreeItemModel* itemsModel = nullptr;
-	imtbase::CTreeItemModel* notificationModel = nullptr;
+    istd::TDelPtr<imtbase::CTreeItemModel> rootModelPtr(new imtbase::CTreeItemModel());
+    imtbase::CTreeItemModel* dataModel = nullptr;
+    imtbase::CTreeItemModel* itemsModel = nullptr;
+    imtbase::CTreeItemModel* notificationModel = nullptr;
 
-	if (!m_objectCollectionCompPtr.IsValid()){
-		errorMessage = QObject::tr("Internal error").toUtf8();
-	}
+    //qDebug() << "ЗАПРОС КЛАСТЕРОВ";
 
-	if (!errorMessage.isEmpty()){
-		imtbase::CTreeItemModel* errorsItemModel = rootModelPtr->AddTreeModel("errors");
-		errorsItemModel->SetData("message", errorMessage);
-	}
-	else{
-		dataModel = new imtbase::CTreeItemModel();
-		itemsModel = new imtbase::CTreeItemModel();
-		notificationModel = new imtbase::CTreeItemModel();
+    //	if (!m_objectCollectionCompPtr.IsValid()){
+    //		errorMessage = QObject::tr("Internal error").toUtf8();
+    //	}
 
-		const imtgql::CGqlObject* viewParamsGql = nullptr;
-		const QList<imtgql::CGqlObject>* inputParams = gqlRequest.GetParams();
-		if (inputParams->size() > 0){
-			viewParamsGql = inputParams->at(0).GetFieldArgumentObjectPtr("viewParams");
-		}
+    if (!errorMessage.isEmpty()){
+        imtbase::CTreeItemModel* errorsItemModel = rootModelPtr->AddTreeModel("errors");
+        errorsItemModel->SetData("message", errorMessage);
+    }
+    else{
+        dataModel = new imtbase::CTreeItemModel();
+        itemsModel = new imtbase::CTreeItemModel();
+        notificationModel = new imtbase::CTreeItemModel();
 
-		imtbase::CCollectionFilter filter;
-		imtbase::CCollectionFilter filterOnParentId;
-		imtbase::CCollectionFilter filterOnTypeId;
-		int offset = 0, count = -1;
-		QString parentId = "";
-		QString typeId = "";
-		if (viewParamsGql != nullptr){
-			offset = viewParamsGql->GetFieldArgumentValue("Offset").toInt();
-			count = viewParamsGql->GetFieldArgumentValue("Count").toInt();
+        const imtgql::CGqlObject* viewParamsGql = nullptr;
+        const QList<imtgql::CGqlObject>* inputParams = gqlRequest.GetParams();
+        if (inputParams->size() > 0){
+            viewParamsGql = inputParams->at(0).GetFieldArgumentObjectPtr("viewParams");
+        }
 
-			QByteArray filterBA = viewParamsGql->GetFieldArgumentValue("FilterModel").toByteArray();
-			if (!filterBA.isEmpty()){
-				imtbase::CTreeItemModel generalModel;
-				generalModel.CreateFromJson(filterBA);
-				parentId = generalModel.GetData("ParentId").toString();
-				typeId = generalModel.GetData("TypeId").toString();
-				if (!parentId.isEmpty()){
-					QByteArrayList filteringOnParentIdInfoIds;
-					filteringOnParentIdInfoIds << "ParentId";
-					filterOnParentId.SetFilteringInfoIds(filteringOnParentIdInfoIds);
-					filterOnParentId.SetTextFilter(parentId);
-				}
-				if (!typeId.isEmpty()){
-					QByteArrayList filteringOnTypeIdInfoIds;
-					filteringOnTypeIdInfoIds << "Type";
-					filterOnTypeId.SetFilteringInfoIds(filteringOnTypeIdInfoIds);
-					filterOnTypeId.SetTextFilter(typeId);
-				}
+        //		imtbase::CCollectionFilter filter;
+        //		imtbase::CCollectionFilter filterOnParentId;
+        //		imtbase::CCollectionFilter filterOnTypeId;
+        //		int offset = 0, count = -1;
 
-				imtbase::CTreeItemModel* filterModel = generalModel.GetTreeItemModel("FilterIds");
-				if (filterModel != nullptr){
-					QByteArrayList filteringInfoIds;
-					for (int i = 0; i < filterModel->GetItemsCount(); i++){
-						QByteArray headerId = filterModel->GetData("Id", i).toByteArray();
-						if (!headerId.isEmpty()){
-							filteringInfoIds << headerId;
-						}
-					}
-					filter.SetFilteringInfoIds(filteringInfoIds);
-				}
+        double zoomLevel = 0;
+        double leftTopLat = 0;
+        double leftTopLon = 0;
+        double rigthBottomLat = 0;
+        double rigthBottomLon = 0;
 
-				QString filterText = generalModel.GetData("TextFilter").toString();
-				if (!filterText.isEmpty()){
-					filter.SetTextFilter(filterText);
-				}
-				imtbase::CTreeItemModel* sortModel = generalModel.GetTreeItemModel("Sort");
-				if (sortModel != nullptr){
-					QByteArray headerId = sortModel->GetData("HeaderId").toByteArray();
-					QByteArray sortOrder = sortModel->GetData("SortOrder").toByteArray();
-					if (!headerId.isEmpty() && !sortOrder.isEmpty()){
-						filter.SetSortingOrder(sortOrder == "ASC" ? imtbase::ICollectionFilter::SO_ASC : imtbase::ICollectionFilter::SO_DESC);
-						filter.SetSortingInfoIds(QByteArrayList() << headerId);
-					}
-				}
-			}
-		}
-		iprm::CParamsSet filterParams;
-		filterParams.SetEditableParameter("Filter", &filter);
-		filterParams.SetEditableParameter("ParentId", &filterOnParentId);
-		filterParams.SetEditableParameter("Type", &filterOnTypeId);
+        //		QString parentId = "";
+        //		QString typeId = "";
 
-		int pagesCount = std::ceil(m_objectCollectionCompPtr->GetElementsCount(&filterParams) / (double)count);
-		if (pagesCount < 0){
-			pagesCount = 1;
-		}
+        if (viewParamsGql != nullptr){
 
-		notificationModel->SetData("PagesCount", pagesCount);
+            //			offset = viewParamsGql->GetFieldArgumentValue("Offset").toInt();
+            //			count = viewParamsGql->GetFieldArgumentValue("Count").toInt();
 
-		imtbase::IObjectCollection* subCollection = m_objectCollectionCompPtr->CreateSubCollection(offset, count, &filterParams);
-		imtbase::ICollectionInfo::Ids collectionIds = subCollection->GetElementIds(0, -1);
+            QByteArray filterBA = viewParamsGql->GetFieldArgumentValue("FilterModel").toByteArray();
+            if (!filterBA.isEmpty()){
+                imtbase::CTreeItemModel generalModel;
+                generalModel.CreateFromJson(filterBA);
 
-        for (const QByteArray& collectionId : collectionIds){
-			int itemIndex = itemsModel->InsertNewItem();
+                zoomLevel = generalModel.GetData("ZoomLevel").toDouble();
+                leftTopLat = generalModel.GetData("LeftTopLat").toDouble();
+                leftTopLon = generalModel.GetData("LeftTopLon").toDouble();
+                rigthBottomLat = generalModel.GetData("RightBottomLat").toDouble();
+                rigthBottomLon = generalModel.GetData("RightBottomLon").toDouble();
 
-            const imtgeo::IAddressElementInfo* addressElementInfoPtr = dynamic_cast<const imtgeo::IAddressElementInfo*>(subCollection->GetObjectPtr(collectionId));
+                //qDebug() << "ZoomLevel_ " << zoomLevel << "leftTopLat_ " << leftTopLat << "leftTopLon_ " << leftTopLon << "rigthBottomLat_ " << rigthBottomLat << "rigthBottomLon_ " << rigthBottomLon;
 
-            QByteArray addressId = addressElementInfoPtr->GetId();
-			QString name = addressElementInfoPtr->GetName();
-            QString address = addressElementInfoPtr->GetFullAddress();
-			int typeAddressId = addressElementInfoPtr->GetType();
-			QByteArray parentId = addressElementInfoPtr->GetParentId();
-			itemsModel->SetData("Id", addressId, itemIndex);
-			itemsModel->SetData("Name", name, itemIndex);
-            itemsModel->SetData("FullAddress", address, itemIndex);
-			itemsModel->SetData("TypeId", typeAddressId, itemIndex);
-			itemsModel->SetData("ParentId", parentId, itemIndex);
-		}
-		itemsModel->SetIsArray(true);
-		dataModel->SetExternTreeModel("items", itemsModel);
-		dataModel->SetExternTreeModel("notification", notificationModel);
-	}
-	rootModelPtr->SetExternTreeModel("data", dataModel);
 
-	return rootModelPtr.PopPtr();
+                //				parentId = generalModel.GetData("ParentId").toString();
+                //				typeId = generalModel.GetData("TypeId").toString();
+                //				if (!parentId.isEmpty()){
+                //					QByteArrayList filteringOnParentIdInfoIds;
+                //					filteringOnParentIdInfoIds << "ParentId";
+                //					filterOnParentId.SetFilteringInfoIds(filteringOnParentIdInfoIds);
+                //					filterOnParentId.SetTextFilter(parentId);
+                //				}
+                //				if (!typeId.isEmpty()){
+                //					QByteArrayList filteringOnTypeIdInfoIds;
+                //					filteringOnTypeIdInfoIds << "Type";
+                //					filterOnTypeId.SetFilteringInfoIds(filteringOnTypeIdInfoIds);
+                //					filterOnTypeId.SetTextFilter(typeId);
+                //				}
+
+                //				imtbase::CTreeItemModel* filterModel = generalModel.GetTreeItemModel("FilterIds");
+                //				if (filterModel != nullptr){
+                //					QByteArrayList filteringInfoIds;
+                //					for (int i = 0; i < filterModel->GetItemsCount(); i++){
+                //						QByteArray headerId = filterModel->GetData("Id", i).toByteArray();
+                //						if (!headerId.isEmpty()){
+                //							filteringInfoIds << headerId;
+                //						}
+                //					}
+                //					filter.SetFilteringInfoIds(filteringInfoIds);
+                //				}
+
+                //				QString filterText = generalModel.GetData("TextFilter").toString();
+                //				if (!filterText.isEmpty()){
+                //					filter.SetTextFilter(filterText);
+                //				}
+                //				imtbase::CTreeItemModel* sortModel = generalModel.GetTreeItemModel("Sort");
+                //				if (sortModel != nullptr){
+                //					QByteArray headerId = sortModel->GetData("HeaderId").toByteArray();
+                //					QByteArray sortOrder = sortModel->GetData("SortOrder").toByteArray();
+                //					if (!headerId.isEmpty() && !sortOrder.isEmpty()){
+                //						filter.SetSortingOrder(sortOrder == "ASC" ? imtbase::ICollectionFilter::SO_ASC : imtbase::ICollectionFilter::SO_DESC);
+                //						filter.SetSortingInfoIds(QByteArrayList() << headerId);
+                //					}
+                //				}
+
+
+
+            }
+        }
+
+
+        //		iprm::CParamsSet filterParams;
+        //		filterParams.SetEditableParameter("Filter", &filter);
+        //		filterParams.SetEditableParameter("ParentId", &filterOnParentId);
+        //		filterParams.SetEditableParameter("Type", &filterOnTypeId);
+
+        //		int pagesCount = std::ceil(m_objectCollectionCompPtr->GetElementsCount(&filterParams) / (double)count);
+        //		if (pagesCount < 0){
+        //			pagesCount = 1;
+        //		}
+
+        //		notificationModel->SetData("PagesCount", pagesCount);
+
+        //		imtbase::IObjectCollection* subCollection = m_objectCollectionCompPtr->CreateSubCollection(offset, count, &filterParams);
+        //		imtbase::ICollectionInfo::Ids collectionIds = subCollection->GetElementIds(0, -1);
+
+        //        for (const QByteArray& collectionId : collectionIds){
+        //			int itemIndex = itemsModel->InsertNewItem();
+
+        //            const imtgeo::IAddressElementInfo* addressElementInfoPtr = dynamic_cast<const imtgeo::IAddressElementInfo*>(subCollection->GetObjectPtr(collectionId));
+
+        //            QByteArray addressId = addressElementInfoPtr->GetId();
+        //			QString name = addressElementInfoPtr->GetName();
+        //            QString address = addressElementInfoPtr->GetFullAddress();
+        //			int typeAddressId = addressElementInfoPtr->GetType();
+        //			QByteArray parentId = addressElementInfoPtr->GetParentId();
+        //			itemsModel->SetData("Id", addressId, itemIndex);
+        //			itemsModel->SetData("Name", name, itemIndex);
+        //            itemsModel->SetData("FullAddress", address, itemIndex);
+        //			itemsModel->SetData("TypeId", typeAddressId, itemIndex);
+        //			itemsModel->SetData("ParentId", parentId, itemIndex);
+        //		}
+
+
+        {//test
+            int itemIndex = 0;
+
+            itemIndex = itemsModel->InsertNewItem();
+            itemsModel->SetData("Id", itemIndex, itemIndex);
+            itemsModel->SetData("Positive", 1000, itemIndex);
+            itemsModel->SetData("Negative", 0, itemIndex);
+            itemsModel->SetData("Latitude", 55, itemIndex);
+            itemsModel->SetData("Longitude", 73.30, itemIndex);
+
+            itemIndex = itemsModel->InsertNewItem();
+            itemsModel->SetData("Id", itemIndex, itemIndex);
+            itemsModel->SetData("Positive", 500, itemIndex);
+            itemsModel->SetData("Negative", 40, itemIndex);
+            itemsModel->SetData("Latitude", 55, itemIndex);
+            itemsModel->SetData("Longitude", 73.35, itemIndex);
+
+            itemIndex = itemsModel->InsertNewItem();
+            itemsModel->SetData("Id", itemIndex, itemIndex);
+            itemsModel->SetData("Positive", 0, itemIndex);
+            itemsModel->SetData("Negative", 8, itemIndex);
+            itemsModel->SetData("Latitude", 55, itemIndex);
+            itemsModel->SetData("Longitude", 73.40, itemIndex);
+
+        }//test
+
+        itemsModel->SetIsArray(true);
+        dataModel->SetExternTreeModel("items", itemsModel);
+        dataModel->SetExternTreeModel("notification", notificationModel);
+    }
+    rootModelPtr->SetExternTreeModel("data", dataModel);
+
+    return rootModelPtr.PopPtr();
+
+    //return nullptr;
 }
 
 
