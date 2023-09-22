4b989298a698b0d3f8e8290625c749571df4b802
Vladimir.Cherepanov <Vladimir.Cherepanov@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-09-22 07:48:39 +0000

    CFileFormatConversionComp: added work object reset
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@7574 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtfile/CFileFormatConversionComp.cpp
+++ Include/imtfile/CFileFormatConversionComp.cpp
@@ -12,11 +12,13 @@ int CFileFormatConversionComp::ConvertFiles(
 			ibase::IProgressManager* progressManagerPtr) const
 {
 	if (m_persistenceCompPtr.IsValid() && m_workingObjectCompPtr.IsValid()){
-		int loadState = m_persistenceCompPtr->LoadFromFile(*m_workingObjectCompPtr, inputPath, progressManagerPtr);
-		if (loadState == ifile::IFilePersistence::OS_OK){
-			int saveState = m_persistenceCompPtr->SaveToFile(*m_workingObjectCompPtr, outputPath, progressManagerPtr);
-			if (saveState == ifile::IFilePersistence::OS_OK){
-				return iproc::IProcessor::TS_OK;
+		if (m_workingObjectCompPtr->ResetData()){
+			int loadState = m_persistenceCompPtr->LoadFromFile(*m_workingObjectCompPtr, inputPath, progressManagerPtr);
+			if (loadState == ifile::IFilePersistence::OS_OK){
+				int saveState = m_persistenceCompPtr->SaveToFile(*m_workingObjectCompPtr, outputPath, progressManagerPtr);
+				if (saveState == ifile::IFilePersistence::OS_OK){
+					return iproc::IProcessor::TS_OK;
+				}
 			}
 		}
 	}
