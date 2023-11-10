47841997e84b81cd49c010cac57eb0249a4a9657
Sergej.Zastrovnyh <Sergej.Zastrovnyh@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-11-10 02:40:53 +0000

    Preparation for GraphQL-based file repository: Respond corrected; create and clean temp path added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@7892 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtrest/CHttpFileBufferComp.cpp
+++ Include/imtrest/CHttpFileBufferComp.cpp
@@ -6,8 +6,10 @@
 #include <QtCore/QDir>
 #include <QtCore/QStandardPaths>
 #include <QtCore/QUuid>
+#include <QtCore/QTimer>
 
 // Acf includes
+#include <istd/CSystem.h>
 #include <iprm/CParamsSet.h>
 #include <ifile/CFileNameParam.h>
 
@@ -19,51 +21,98 @@ namespace imtrest
 {
 
 
+// protected methods
+
+ConstResponsePtr CHttpFileBufferComp::CreateDefaultErrorResponse(const QByteArray& errorString, IProtocolEngine::StatusCode statusCode, const CHttpRequest& request) const
+{
+	SendErrorMessage(0, errorString);
+
+	ConstResponsePtr retVal;
+
+	retVal.SetPtr(request.GetProtocolEngine().CreateResponse(
+					  request,
+					  statusCode,
+					  errorString,
+					  QByteArray("text/plain; charset=utf-8")));
+
+	return retVal;
+}
+
+
+// reimplemented (icomp::CComponentBase)
+
+void CHttpFileBufferComp::OnComponentCreated()
+{
+	BaseClass::OnComponentCreated();
+
+	if (m_tempDirectoryPathCompPtr.IsValid()){
+		istd::CSystem::EnsurePathExists(m_tempDirectoryPathCompPtr->GetPath());
+	}
+
+	/// \todo add observe the model to clean temp files immediately
+}
+
+
+void CHttpFileBufferComp::OnComponentDestroyed()
+{
+	if (m_tempDirectoryPathCompPtr.IsValid()){
+		istd::CSystem::RemoveDirectory(m_tempDirectoryPathCompPtr->GetPath());
+	}
+
+	BaseClass::OnComponentDestroyed();
+}
+
+
+// reimplemented (CHttpServletCompBase)
+
 ConstResponsePtr CHttpFileBufferComp::OnGet(
 			const QByteArray& commandId,
 			const IRequest::CommandParams& commandParams,
 			const HeadersMap& headers,
 			const CHttpRequest& request) const
 {
-	int statusCode = imtrest::IProtocolEngine::SC_OK;
+	if (!m_tempFileCollectionCompPtr.IsValid()){
+		const QString errorString = "Temp collection is invalid";
+		I_CRITICAL();
+
+		return CreateDefaultErrorResponse(errorString.toUtf8(), IProtocolEngine::SC_INTERNAL_SERVER_ERROR, request);
+	}
+
 	const QByteArray requestedFileId;
 
 	imtbase::IObjectCollection& requestCollection = *m_tempFileCollectionCompPtr;
 	const istd::IChangeable* objectPtr = requestCollection.GetObjectPtr(requestedFileId);
 	if (objectPtr == nullptr){
-		statusCode = imtrest::IProtocolEngine::SC_NOT_FOUND;
 		SendCriticalMessage(0, QString("Unable to find request with ID: '%1'").arg(QString(requestedFileId)));
 		I_CRITICAL();
 
-		return CreateDefaultErrorResponse(QT_TR_NOOP("Unable to find file"), request);
+		return CreateDefaultErrorResponse(QT_TR_NOOP("Unable to find file"),IProtocolEngine::SC_NOT_FOUND, request);
 	}
 
 	const iprm::IParamsSet* paramsSetPtr = dynamic_cast<const iprm::IParamsSet*>(objectPtr);
 	if (paramsSetPtr == nullptr){
-		statusCode = imtrest::IProtocolEngine::SC_INTERNAL_ERROR;
 		SendCriticalMessage(0, QString("Model has unsupported modelType"));
 		I_CRITICAL();
 
-		return CreateDefaultErrorResponse(QT_TR_NOOP("Model has unsupported modelType"), request);
+		return CreateDefaultErrorResponse(QT_TR_NOOP("Model has unsupported modelType"), IProtocolEngine::SC_INTERNAL_ERROR, request);
 	}
 
 	iprm::TParamsPtr<ifile::IFileNameParam> fileNameParamPtr(paramsSetPtr, "FilePath");
 	if (!fileNameParamPtr.IsValid()){
-		statusCode = imtrest::IProtocolEngine::SC_INTERNAL_ERROR;
 		SendCriticalMessage(0, QString("Model has unsupported modelType"));
 
-		return CreateDefaultErrorResponse(QT_TR_NOOP("Model has unsupported modelType"), request);
+		return CreateDefaultErrorResponse(QT_TR_NOOP("Model has unsupported modelType"), IProtocolEngine::SC_INTERNAL_ERROR,request);
 	}
 
 	QString filePath = fileNameParamPtr->GetPath();
 
 	QFile requestedFile(filePath);
 	if (!requestedFile.open(QIODevice::ReadOnly)){
-		statusCode = imtrest::IProtocolEngine::SC_INTERNAL_ERROR;
 		SendCriticalMessage(0, QString("Unable to open file: '%1'. Error: '%2'").arg(filePath, requestedFile.errorString()));
 
 		return CreateDefaultErrorResponse(
 					QString(QT_TR_NOOP("Unable to open file: '%1'. Error: '%2'")).arg(filePath, requestedFile.errorString()).toUtf8(),
+					IProtocolEngine::SC_INTERNAL_ERROR,
 					request);
 	}
 	qint64 offset = 0;
@@ -71,11 +120,10 @@ ConstResponsePtr CHttpFileBufferComp::OnGet(
 		bool isNumber = false;
 		offset = commandParams["Offset"].toLongLong(&isNumber);
 		if (!isNumber){
-			statusCode = imtrest::IProtocolEngine::SC_BAD_REQUEST;
-
 			return CreateDefaultErrorResponse(
-				QString(QT_TR_NOOP("Offset has wrong value '%1'")).arg(QString(commandParams["Offset"])).toUtf8(),
-				request);
+						QString(QT_TR_NOOP("Offset has wrong value '%1'")).arg(QString(commandParams["Offset"])).toUtf8(),
+						IProtocolEngine::SC_BAD_REQUEST,
+						request);
 		}
 	}
 
@@ -84,19 +132,19 @@ ConstResponsePtr CHttpFileBufferComp::OnGet(
 		bool isNumber = false;
 		limit = commandParams["Limit"].toLongLong(&isNumber);
 		if (!isNumber){
-			statusCode = imtrest::IProtocolEngine::SC_BAD_REQUEST;
-
 			return CreateDefaultErrorResponse(
-				QString(QT_TR_NOOP("Limit has wrong value '%1'")).arg(QString(commandParams["Limit"])).toUtf8(),
-				request);
+						QString(QT_TR_NOOP("Limit has wrong value '%1'")).arg(QString(commandParams["Limit"])).toUtf8(),
+						IProtocolEngine::SC_BAD_REQUEST,
+						request);
 		}
 	}
 
 	requestedFile.seek(offset);
 	const QByteArray payload = requestedFile.read(limit);
 
-	return ConstResponsePtr(request.GetProtocolEngine().CreateResponse(request, statusCode, payload, QByteArrayLiteral("application/octet-stream; charset=utf-8")));
+	return ConstResponsePtr(request.GetProtocolEngine().CreateResponse(request, IProtocolEngine::SC_OK, payload, QByteArrayLiteral("application/octet-stream; charset=utf-8")));
 }
+
 
 ConstResponsePtr CHttpFileBufferComp::OnPost(
 			const QByteArray& commandId,
@@ -104,7 +152,12 @@ ConstResponsePtr CHttpFileBufferComp::OnPost(
 			const HeadersMap& headers,
 			const CHttpRequest& request) const
 {
-	int statusCode = imtrest::IProtocolEngine::SC_OK;
+	if (!m_tempFileCollectionCompPtr.IsValid()){
+		const QString errorString = "Temp collection is invalid";
+		I_CRITICAL();
+
+		return CreateDefaultErrorResponse(errorString.toUtf8(), IProtocolEngine::SC_INTERNAL_SERVER_ERROR, request);
+	}
 
 	QString fileDirectoryPath = QStandardPaths::writableLocation(QStandardPaths::TempLocation) + "HttpFileBuffer";
 	if (m_tempDirectoryPathCompPtr.IsValid()){
@@ -114,22 +167,19 @@ ConstResponsePtr CHttpFileBufferComp::OnPost(
 	QFile tempFile(QDir::cleanPath(fileDirectoryPath + "/" + QUuid::createUuid().toString(QUuid::Id128)));
 	Q_ASSERT(!tempFile.exists());
 	if (!tempFile.open(QIODevice::WriteOnly)){
-		statusCode = imtrest::IProtocolEngine::SC_INTERNAL_SERVER_ERROR;
 		const QString errorString = QString(QT_TR_NOOP("Unable to save file: %1")).arg(tempFile.errorString());
-		SendErrorMessage(0, errorString);
 		I_CRITICAL();
 
-		return CreateDefaultErrorResponse(errorString.toUtf8(), request);
+		return CreateDefaultErrorResponse(errorString.toUtf8(), IProtocolEngine::SC_INTERNAL_SERVER_ERROR, request);
 	}
 
 	quint64 writtenBytes = tempFile.write(request.GetBody());
 	if (writtenBytes != request.GetBody().size()){
-		statusCode = imtrest::IProtocolEngine::SC_INTERNAL_SERVER_ERROR;
 		const QString errorString = QString(QT_TR_NOOP("Saved file is corrupted!: %1")).arg(tempFile.errorString());
 		SendCriticalMessage(0, errorString);
 		I_CRITICAL();
 
-		return CreateDefaultErrorResponse(errorString.toUtf8(), request);
+		return CreateDefaultErrorResponse(errorString.toUtf8(), IProtocolEngine::SC_INTERNAL_SERVER_ERROR, request);
 	}
 	tempFile.close();
 
@@ -149,18 +199,15 @@ ConstResponsePtr CHttpFileBufferComp::OnPost(
 		);
 
 	if (createdFileId.isEmpty()){
-		statusCode = imtrest::IProtocolEngine::SC_INTERNAL_SERVER_ERROR;
-		SendErrorMessage(0, "Unable to create file");
 		I_CRITICAL();
 
-		return CreateDefaultErrorResponse("Unable to save file", request);
+		return CreateDefaultErrorResponse("Unable to save file", IProtocolEngine::SC_INTERNAL_SERVER_ERROR, request);
 	}
 
 	const QByteArray payload = createdFileId;
 
-	return ConstResponsePtr(request.GetProtocolEngine().CreateResponse(request, statusCode, payload, QByteArrayLiteral("application/json; charset=utf-8")));
+	return ConstResponsePtr(request.GetProtocolEngine().CreateResponse(request, IProtocolEngine::SC_OK, payload, QByteArrayLiteral("application/json; charset=utf-8")));
 }
-
 
 
 } // namespace imtrest
--- Include/imtrest/CHttpFileBufferComp.h
+++ Include/imtrest/CHttpFileBufferComp.h
@@ -3,7 +3,8 @@
 
 // ImtCore includes
 #include <imtbase/CObjectCollectionComp.h>
-#include<imtrest/CHttpServletCompBase.h>
+#include <imtrest/IProtocolEngine.h>
+#include <imtrest/CHttpServletCompBase.h>
 
 
 namespace imtrest
@@ -13,18 +14,26 @@ namespace imtrest
 /**
 	Temporary storage for uploaded files to the server of files prepared to download to client
 */
-class CHttpFileBufferComp:
-			public CHttpServletCompBase
+class CHttpFileBufferComp: public CHttpServletCompBase
 {
+
 
 public:
 	typedef CHttpServletCompBase BaseClass;
 
 	I_BEGIN_COMPONENT(CHttpFileBufferComp);
+		I_ASSIGN(m_tempFileCollectionCompPtr, "ObjectCollection", "The object collection, used to store temp files paths", true, "ObjectCollection");
+		I_ASSIGN(m_tempDirectoryPathCompPtr, "TempDirectoryPath", "The path where temp files will be stored, while it is required. \nWarning: this path will be cleared on suutdown", true, "TempDirectoryPath");
 	I_END_COMPONENT;
 
 
 protected:
+	ConstResponsePtr CreateDefaultErrorResponse(const QByteArray& errorString, imtrest::IProtocolEngine::StatusCode statusCode, const imtrest::CHttpRequest& request) const;
+
+	// reimplemented (icomp::CComponentBase)
+	virtual void OnComponentCreated() override;
+	virtual void OnComponentDestroyed() override;
+
 	// reimplemented (CHttpServletCompBase)
 	virtual ConstResponsePtr OnGet(
 		const QByteArray& commandId,
@@ -37,6 +46,8 @@ protected:
 		const imtrest::IRequest::CommandParams& commandParams,
 		const HeadersMap& headers,
 		const imtrest::CHttpRequest& request) const override;
+
+
 
 private:
 	I_REF(imtbase::IObjectCollection, m_tempFileCollectionCompPtr);
