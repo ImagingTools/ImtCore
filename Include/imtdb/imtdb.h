a2b4d5ea2400699f11453105d4072128b36fd03d
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-12-11 12:22:20 +0000

    SqlEncode moved to imtdb.h
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@8163 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtdb/imtdb.h
+++ Include/imtdb/imtdb.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// Qt includes
+#include <QtCore/QString>
 
 /**
 	Interfaces and basic implementations for general database management.
@@ -7,6 +9,13 @@
 */
 namespace imtdb
 {
+
+
+QString SqlEncode(const QString& sqlQuery)
+{
+	QString retVal = sqlQuery;
+	return retVal.replace("'", "''").replace(";", "\b");
+}
 
 
 } // namespace imtdb
