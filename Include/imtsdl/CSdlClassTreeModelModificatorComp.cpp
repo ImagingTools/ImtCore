88ceceb1fcbe41b467bdcd31b324a3852f119266
Sergej.Zastrovnyh <Sergej.Zastrovnyh@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-01-30 04:31:27 +0000

    imtsdl: TreeModelModificator: rewokred: all basic moved to BaseClass
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@8630 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtsdl/CSdlClassTreeModelModificatorComp.cpp
+++ Include/imtsdl/CSdlClassTreeModelModificatorComp.cpp
@@ -2,15 +2,7 @@
 
 
 // Qt includes
-#include <QtCore/QDir>
 #include <QtCore/QRegularExpression>
-#include <QtCore/QRegularExpressionMatch>
-
-//Acf includes
-#include <istd/CSystem.h>
-
-// imtbase includes
-#include <imtbase/CTempDir.h>
 
 // imtsdl includes
 #include <imtsdl/CSdlType.h>
@@ -20,127 +12,11 @@ namespace imtsdl
 {
 
 
-int CSdlClassTreeModelModificatorComp::DoProcessing(
-			const iprm::IParamsSet* paramsPtr,
-			const istd::IPolymorphic* inputPtr,
-			istd::IChangeable* outputPtr,
-			ibase::IProgressManager* progressManagerPtr)
-{
-	Q_ASSERT(m_argumentParserCompPtr.IsValid());
-	Q_ASSERT(m_sdlTypeListCompPtr.IsValid());
+// protected methods
 
-	int retVal = iproc::IProcessor::TS_OK;
-	if (!m_argumentParserCompPtr->IsModificatorEnabled(*m_processorModificatorName)){
-		// nothing todo if our modificator is not exsits
-		return retVal;
-	}
+// reimplemented (CSdlClassModificatorBaseComp)
 
-	if (!m_argumentParserCompPtr->IsModificatorEnabled(*m_processorModificatorName)){
-		SendErrorMessage(1, QString("Variant map modificator is not enabled. Use -M%1 argument to enable it").arg(s_variantMapModificatorArgumentName));
-
-		return iproc::IProcessor::TS_INVALID;
-	}
-
-	const QString outputDirectoryPath = QDir::cleanPath(m_argumentParserCompPtr->GetOutputDirectoryPath());
-	if (outputDirectoryPath.isEmpty()){
-		SendCriticalMessage(0, "Output path is not provided");
-		I_CRITICAL();
-
-		return iproc::IProcessor::TS_INVALID;
-	}
-
-	if (!istd::CSystem::EnsurePathExists(outputDirectoryPath)){
-		SendErrorMessage(0, QString("Unable to create path '%1'").arg(outputDirectoryPath));
-		I_CRITICAL();
-
-		return iproc::IProcessor::TS_INVALID;
-	}
-
-	imtbase::CTempDir tempDir;
-	const QString tempPath = tempDir.Path();
-
-	SdlTypeList sdlTypeList = m_sdlTypeListCompPtr->GetSdlTypes();
-	for (const CSdlType& sdlType: sdlTypeList){
-		m_headerFilePtr.SetPtr(new QFile(tempPath + "/C" + sdlType.GetName() + ".h"));
-		m_sourceFilePtr.SetPtr(new QFile(tempPath + "/C" + sdlType.GetName() + ".cpp"));
-		m_originalHeaderFilePtr.SetPtr(new QFile(outputDirectoryPath + "/C" + sdlType.GetName() + ".h"));
-		m_originalSourceFilePtr.SetPtr(new QFile(outputDirectoryPath + "/C" + sdlType.GetName() + ".cpp"));
-		bool isOk = BeginClassFiles(sdlType);
-		if (!isOk){
-			AbortCurrentProcessing();
-
-			return iproc::IProcessor::TS_INVALID;
-		}
-
-		isOk = isOk && EndClassFiles(sdlType);
-		if (!isOk){
-			AbortCurrentProcessing();
-
-			return iproc::IProcessor::TS_INVALID;
-		}
-	}
-
-	return retVal;
-}
-
-
-// private methods
-
-
-
-bool CSdlClassTreeModelModificatorComp::BeginClassFiles(const CSdlType& sdlType)
-{
-	// Create new files in temp path
-	if (!m_headerFilePtr->open(QIODevice::WriteOnly)){
-		SendErrorMessage(0,
-					QString("Unable to open file: '%1'. Error: %2")
-						.arg(m_headerFilePtr->fileName(), m_headerFilePtr->errorString()));
-
-		AbortCurrentProcessing();
-
-		return false;
-	}
-
-	if (!m_sourceFilePtr->open(QIODevice::WriteOnly)){
-		SendErrorMessage(0,
-						QString("Unable to open file: '%1'. Error: %2")
-							.arg(m_sourceFilePtr->fileName(), m_sourceFilePtr->errorString()));
-
-		AbortCurrentProcessing();
-
-		return false;
-	}
-
-	// Open original files to read data
-	if (!m_originalHeaderFilePtr->open(QIODevice::ReadOnly)){
-		SendErrorMessage(0,
-						 QString("Unable to open file: '%1'. Error: %2")
-						 .arg(m_originalHeaderFilePtr->fileName(), m_originalHeaderFilePtr->errorString()));
-
-		AbortCurrentProcessing();
-
-		return false;
-	}
-
-	if (!m_originalSourceFilePtr->open(QIODevice::ReadOnly)){
-		SendErrorMessage(0,
-						 QString("Unable to open file: '%1'. Error: %2")
-						 .arg(m_originalSourceFilePtr->fileName(), m_originalSourceFilePtr->errorString()));
-
-		AbortCurrentProcessing();
-
-		return false;
-	}
-
-	bool retVal = true;
-	retVal = retVal && BeginHeaderClassFile(sdlType);
-	retVal = retVal && BeginSourceClassFile(sdlType);
-
-	return retVal;
-}
-
-
-bool CSdlClassTreeModelModificatorComp::BeginHeaderClassFile(const CSdlType& sdlType)
+bool CSdlClassTreeModelModificatorComp::ProcessHeaderClassFile(const CSdlType& sdlType)
 {
 	QTextStream ofStream(m_headerFilePtr.GetPtr());
 	QTextStream ifStream(m_originalHeaderFilePtr.GetPtr());
@@ -172,7 +48,7 @@ bool CSdlClassTreeModelModificatorComp::BeginHeaderClassFile(const CSdlType& sdl
 }
 
 
-bool CSdlClassTreeModelModificatorComp::BeginSourceClassFile(const CSdlType& sdlType)
+bool CSdlClassTreeModelModificatorComp::ProcessSourceClassFile(const CSdlType& sdlType)
 {
 	QTextStream ofStream(m_sourceFilePtr.GetPtr());
 	QTextStream ifStream(m_originalSourceFilePtr.GetPtr());
@@ -245,53 +121,7 @@ bool CSdlClassTreeModelModificatorComp::BeginSourceClassFile(const CSdlType& sdl
 }
 
 
-bool CSdlClassTreeModelModificatorComp::EndClassFiles(const CSdlType& sdlType)
-{
-	return CloseFiles();
-}
-
-
-bool CSdlClassTreeModelModificatorComp::CloseFiles()
-{
-	bool retVal = true;
-
-	retVal = m_headerFilePtr->flush();
-	Q_ASSERT(retVal);
-
-	retVal = m_sourceFilePtr->flush() && retVal;
-	Q_ASSERT(retVal);
-
-	// Close files
-	m_headerFilePtr->close();
-	m_sourceFilePtr->close();
-
-	m_originalHeaderFilePtr->close();
-	m_originalSourceFilePtr->close();
-
-	// Then move modificated files to original location
-	const QString originalPath = QFileInfo(*m_originalHeaderFilePtr).absolutePath();
-	retVal = retVal && istd::CSystem::FileMove(m_headerFilePtr->fileName(), originalPath, true);
-	Q_ASSERT(retVal);
-
-	retVal = retVal && istd::CSystem::FileMove(m_sourceFilePtr->fileName(), originalPath, true);
-	Q_ASSERT(retVal);
-
-	return retVal;
-}
-
-
-
-void CSdlClassTreeModelModificatorComp::AbortCurrentProcessing()
-{
-	m_headerFilePtr->close();
-	m_sourceFilePtr->close();
-
-	I_CRITICAL();
-
-	m_headerFilePtr->remove();
-	m_sourceFilePtr->remove();
-}
-
+// private methods
 
 void CSdlClassTreeModelModificatorComp::AddFieldWriteToModelCode(QTextStream& stream, const CSdlField& field)
 {
@@ -308,11 +138,6 @@ void CSdlClassTreeModelModificatorComp::AddFieldWriteToModelCode(QTextStream& st
 		stream << QStringLiteral("model.SetData(\"") << field.GetId() << QStringLiteral("\", ");
 		stream << FromVariantMapAccessString(field) << QStringLiteral(", modelIndex);");
 	}
-	/*
-	if (!_m_fieldsMap["Name"].isNull() && (requiredFields.isEmpty() || requiredFields.contains("Name"))){
-		model.SetData("Name", _m_fieldsMap["Name"], modelIndex);
-	}
-	*/
 	else {
 		stream << QStringLiteral("if (!");
 		stream << FromVariantMapAccessString(field);
--- Include/imtsdl/CSdlClassTreeModelModificatorComp.h
+++ Include/imtsdl/CSdlClassTreeModelModificatorComp.h
@@ -1,17 +1,8 @@
 #pragma once
 
 
-//Qt includes
-#include <QtCore/QFile>
-
-// Acf includes
-#include <istd/TDelPtr.h>
-#include <iproc/TSyncProcessorCompBase.h>
-
 // imtsdl includes
-#include <imtsdl/ISdlProcessArgumentsParser.h>
-#include <imtsdl/CGqlSchemaParser.h>
-#include <imtsdl/CSdlTools.h>
+#include <imtsdl/CSdlClassModificatorBaseComp.h>
 
 
 namespace imtsdl
@@ -21,52 +12,25 @@ namespace imtsdl
 /**
 	The C++ class modificator, adds saving and loading generated SDL types to TreeItemModel
 	Extends generated classes by add methods, allows to save and load it from/to \c imtbase::CTreeItemModel
-	\todo make a base class based on this (typical management, files, dirs, error handling...)
  */
-class CSdlClassTreeModelModificatorComp:
-			public iproc::CSyncProcessorCompBase,
-			public CGqlSchemaParser,
-			private CSdlTools
+class CSdlClassTreeModelModificatorComp: public CSdlClassModificatorBaseComp
 {
 
 public:
-	typedef iproc::CSyncProcessorCompBase BaseClass;
-	typedef CGqlSchemaParser BaseClass2;
+	typedef CSdlClassModificatorBaseComp BaseClass;
 
 	I_BEGIN_COMPONENT(CSdlClassTreeModelModificatorComp)
-		I_ASSIGN(m_processorModificatorName, "ProcessModificatorName", "The name, allows to do processing, received from arguments in 'modificators' section", true, "TreeModelAccess")
-		I_ASSIGN(m_argumentParserCompPtr, "ArgumentParser", "Command line process argument parser", true, "ArgumentParser")
-		I_ASSIGN(m_sdlTypeListCompPtr, "SdlTypeListProvider", "SDL types used to create a code", true, "SdlTypeListProvider")
 	I_END_COMPONENT;
 
-	//reimplemented(iproc::IProcessor)
-	virtual int DoProcessing(
-				const iprm::IParamsSet* paramsPtr,
-				const istd::IPolymorphic* inputPtr,
-				istd::IChangeable* outputPtr,
-				ibase::IProgressManager* progressManagerPtr = NULL) override;
+protected:
+	// reimplemented (CSdlClassModificatorBaseComp)
+	virtual bool ProcessHeaderClassFile(const CSdlType& sdlType) override;
+	virtual bool ProcessSourceClassFile(const CSdlType& sdlType) override;
 
 private:
-	bool BeginClassFiles(const CSdlType& sdlType);
-	bool BeginHeaderClassFile(const CSdlType& sdlType);
-	bool BeginSourceClassFile(const CSdlType& sdlType);
-	bool EndClassFiles(const CSdlType& sdlType);
-	bool CloseFiles();
-	void AbortCurrentProcessing();
 	/// \todo add error handling for it
 	void AddFieldWriteToModelCode(QTextStream& stream, const CSdlField& field);
 	void AddFieldReadFromModelCode(QTextStream& stream, const CSdlField& field);
-
-private:
-	I_ATTR(QString, m_processorModificatorName);
-	I_REF(ISdlProcessArgumentsParser, m_argumentParserCompPtr);
-	I_REF(ISdlTypeListProvider, m_sdlTypeListCompPtr);
-
-	istd::TDelPtr<QFile> m_headerFilePtr;
-	istd::TDelPtr<QFile> m_sourceFilePtr;
-
-	istd::TDelPtr<QFile> m_originalHeaderFilePtr;
-	istd::TDelPtr<QFile> m_originalSourceFilePtr;
 };
 
 
