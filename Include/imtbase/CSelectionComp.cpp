f3bf02fec7a9a29327ada2420fbe6fc42ea61cd7
Vladimir.Cherepanov <Vladimir.Cherepanov@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-02-02 10:48:59 +0000

    CSelectionComp corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@8707 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtbase/CSelectionComp.cpp
+++ Include/imtbase/CSelectionComp.cpp
@@ -7,6 +7,47 @@ namespace imtbase
 
 // protected methods
 
+// reimplemented (CSelection)
+
+void CSelectionComp::OnConstraintsUpdated(const istd::IChangeable::ChangeSet& changeset, const ICollectionInfo* modelPtr)
+{
+	Ids tempSelection;
+	Ids selection;
+	ICollectionInfo::Ids constraints = modelPtr->GetElementIds();
+
+	switch (*m_autoSelectioModeAttrPtr){
+	case 0:
+		selection = GetSelectedIds();
+		break;
+	case 1:
+		for (Id id : constraints){
+			selection += id;
+		}
+		break;
+	case 2:
+		break;
+	case 3:
+		for (int i = 0; i < m_autoSelectionIdsAttrPtr.GetCount(); i++){
+			selection += m_autoSelectionIdsAttrPtr[i];
+		}
+		break;
+	case 4:
+		selection = GetSelectedIds();
+		for (Id id : selection){
+			if (constraints.contains(id)){
+				tempSelection += id;
+			}
+		}
+		selection = tempSelection;
+		break;
+	default:
+		Q_ASSERT(false);
+	}
+
+	SetSelectedIds(selection);
+}
+
+
 // reimplemented (icomp::CComponentBase)
 
 void CSelectionComp::OnComponentCreated()
@@ -20,6 +61,22 @@ void CSelectionComp::OnComponentCreated()
 		break;
 	default:
 		Q_ASSERT(false);
+	}
+
+	Ids selection;
+
+	switch (GetSelectionMode()){
+	case SM_SINGLE:
+		for (int i = 0; i < m_initialSelectionIdsAttrPtr.GetCount(); i++){
+			SetSelectedIds({ m_initialSelectionIdsAttrPtr[i] });
+		}
+		break;
+	case SM_MULTI:
+		for (int i = 0; i < m_initialSelectionIdsAttrPtr.GetCount(); i++){
+			selection += m_initialSelectionIdsAttrPtr[i];
+		}
+		SetSelectedIds(selection);
+		break;
 	}
 
 	if (m_constraintsCompPtr.IsValid()){
