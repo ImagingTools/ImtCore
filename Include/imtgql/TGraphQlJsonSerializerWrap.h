cd61e10fd569a76f8f158813c7b2aab07b81f6e8
Sergej.Zastrovnyh <Sergej.Zastrovnyh@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-04-01 14:48:49 +0000

    imtgql: GraphQL Json serializer added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@9414 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtgql/TGraphQlJsonSerializerWrap.h
@@ -0,0 +1,106 @@
+﻿#pragma once
+
+
+// Qt includes
+#include <QtCore/QJsonArray>
+#include <QtCore/QJsonDocument>
+#include <QtCore/QJsonObject>
+#include <QtCore/QJsonValue>
+
+// ImtCore includes
+#include <imtgql/IGqlResponse.h>
+
+
+namespace imtgql
+{
+
+
+template <class GqlResponseBase>
+class TGraphQlJsonSerializerWrap: public virtual GqlResponseBase
+{
+public:
+	TGraphQlJsonSerializerWrap(IGqlResponse::GqlRequestPtr originalRequestPtr);
+
+	// reimplemented (IGqlResponse)
+	virtual QByteArray GetData() const override;
+	virtual typename GqlResponseBase::ErrorEntryList GetErrorList() const override;
+	virtual QStringList GetErrorMessageList() const override;
+};
+
+
+template<class GqlResponseBase>
+TGraphQlJsonSerializerWrap<GqlResponseBase>::TGraphQlJsonSerializerWrap(IGqlResponse::GqlRequestPtr originalRequestPtr)
+	: GqlResponseBase(originalRequestPtr)
+{
+}
+
+
+template <class GqlResponseBase>
+QByteArray TGraphQlJsonSerializerWrap<GqlResponseBase>::GetData() const
+{
+	QJsonDocument rawPayload = QJsonDocument::fromJson(GqlResponseBase::GetResponseData());
+	if (!rawPayload.isObject()){
+		Q_ASSERT_X(false, "GetData", "Unexpected JSON");
+
+		return QByteArray();
+	}
+
+	QJsonValue payloadDataValue = rawPayload.object().value(QStringLiteral("data"));
+
+	if (payloadDataValue.isObject()){
+		return QJsonDocument(payloadDataValue.toObject()).toJson();
+	}
+	if (payloadDataValue.isArray()){
+		return QJsonDocument(payloadDataValue.toArray()).toJson();
+	}
+
+	Q_ASSERT_X(false, "GetData", "Unexpected 'data' JSON");
+
+	return QByteArray();
+}
+
+
+template <class GqlResponseBase>
+typename GqlResponseBase::ErrorEntryList TGraphQlJsonSerializerWrap<GqlResponseBase>::GetErrorList() const
+{
+	QJsonDocument rawPayload = QJsonDocument::fromJson(GqlResponseBase::GetResponseData());
+	if (!rawPayload.isObject()){
+		Q_ASSERT_X(false, "GetErrorList", "Unexpected JSON");
+
+		return typename GqlResponseBase::ErrorEntryList();
+	}
+
+	QJsonValue payloadErrorsValue = rawPayload.object().value(QStringLiteral("errors"));
+
+	Q_ASSERT_X(payloadErrorsValue.isArray(), "GetErrorList", "Unexpected 'errors' JSON");
+
+	const QJsonArray payloadErrorsArray = payloadErrorsValue.toArray();
+	typename GqlResponseBase::ErrorEntryList retVal;
+	for(const QJsonValue& errorEntry: payloadErrorsArray){
+		Q_ASSERT_X(errorEntry.isObject(), "GetErrorList", "Unexpected error entry");
+
+		retVal << errorEntry.toObject().toVariantMap();
+	}
+
+	return retVal;
+}
+
+
+template <class GqlResponseBase>
+QStringList TGraphQlJsonSerializerWrap<GqlResponseBase>::GetErrorMessageList() const
+{
+	QStringList retVal;
+	const typename GqlResponseBase::ErrorEntryList errors = GetErrorList();
+	for (const QVariantMap& errorEntry: errors){
+		Q_ASSERT(errorEntry.contains(QStringLiteral("message")));
+
+		retVal << errorEntry[QStringLiteral("message")].toString();
+	}
+
+	return retVal;
+}
+
+
+} // namespace imtqml
+
+
