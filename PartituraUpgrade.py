7b1661fbca18688766a0b832117ebc77df41ac28
Sergej.Zastrovnyh <Sergej.Zastrovnyh@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-05-02 04:31:29 +0000

    partitura upgrade script extended
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@9737 21205d9f-8918-2240-a3d5-c39064a2c852

--- PartituraUpgrade.py
+++ PartituraUpgrade.py
@@ -1,28 +1,35 @@
-import pathlib, os, subprocess, re
+import pathlib, os, subprocess, re, argparse
 
 
-currentDir = os.path.dirname(os.path.realpath(__file__))
-svnProcess = subprocess.Popen(
-			['svn', 'info', '--show-item', 'revision', currentDir], 
-			stdin=subprocess.PIPE,
-			stdout=subprocess.PIPE)
-svnProcessOutput = svnProcess.communicate()
-svnRevision = svnProcessOutput[0].decode()
-svnRevision = svnRevision.replace('\n', '')
-print (f'SVN revision: {svnRevision}')
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Helper script to upgrade partitura")
+	parser.add_argument('-D', '--working-directory', required=False, help='The directory in which you want to find version template files')
+	args = parser.parse_args()
 
-rootSearchPath = os.path.dirname(os.path.realpath(__file__))
-xtrAttrFileList = pathlib.Path(rootSearchPath).rglob('*.acc.xtrsvn')
+	if (not args.working_directory is None )and (len(args.working_directory) > 0):
+		workingDirectory = args.working_directory
+	else:
+		workingDirectory = os.path.dirname(os.path.realpath(__file__))
 
+	print (f'Lookup starts at: {workingDirectory}')
 
+	svnProcess = subprocess.Popen(
+				['svn', 'info', '--show-item', 'revision', workingDirectory], 
+				stdin=subprocess.PIPE,
+				stdout=subprocess.PIPE)
+	svnProcessOutput = svnProcess.communicate()
+	svnRevision = svnProcessOutput[0].decode()
+	svnRevision = svnRevision.replace('\n', '')
+	print (f'SVN revision: {svnRevision}')
 
-for xtrAttrFilePath in xtrAttrFileList:
-	print(f'processing: {xtrAttrFilePath}')
-	with open(xtrAttrFilePath, 'r') as xtrAttrFile:
-		xtrAttrFileData = xtrAttrFile.read()
-		outputFilePath = re.sub(r'.xtrsvn$', r'', str(xtrAttrFilePath))
-		print (f'creating {outputFilePath}')
-		with open(outputFilePath, 'w') as accOutputFile:
-			accOutputFileData = xtrAttrFileData.replace('$WCREV$', svnRevision)
-			print (f'writting {outputFilePath}')
-			accOutputFile.write(accOutputFileData)
+	xtrAttrFileList = pathlib.Path(workingDirectory).rglob('*.acc.xtrsvn')
+	for xtrAttrFilePath in xtrAttrFileList:
+		print(f'processing: {xtrAttrFilePath}')
+		with open(xtrAttrFilePath, 'r') as xtrAttrFile:
+			xtrAttrFileData = xtrAttrFile.read()
+			outputFilePath = re.sub(r'.xtrsvn$', r'', str(xtrAttrFilePath))
+			print (f'creating {outputFilePath}')
+			with open(outputFilePath, 'w') as accOutputFile:
+				accOutputFileData = xtrAttrFileData.replace('$WCREV$', svnRevision)
+				print (f'writting {outputFilePath}')
+				accOutputFile.write(accOutputFileData)
