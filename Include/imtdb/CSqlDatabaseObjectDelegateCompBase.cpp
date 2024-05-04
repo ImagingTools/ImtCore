b35848cec78f3b64c14cf1106e8a45fc2e707a31
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-05-04 02:45:58 +0000

    CSqlDatabaseObjectDelegateCompBase.cpp corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@9780 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtdb/CSqlDatabaseObjectDelegateCompBase.cpp
+++ Include/imtdb/CSqlDatabaseObjectDelegateCompBase.cpp
@@ -73,18 +73,12 @@ QByteArray CSqlDatabaseObjectDelegateCompBase::GetCountQuery(const iprm::IParams
 		}
 	}
 
-	QByteArray tableSchema;
-
-	if (m_tableSchemaAttrPtr.IsValid()){
-		tableSchema = *m_tableSchemaAttrPtr;
-	}
-
-	if (tableSchema.isEmpty()){
+	if (!m_tableSchemaAttrPtr.IsValid()){
 		return QString("SELECT COUNT(*) FROM \"%1\" %2").arg(qPrintable(*m_tableNameAttrPtr)).arg(filterQuery).toUtf8();
 	}
 
 	return QString("SELECT COUNT(*) FROM %0.\"%1\" %2")
-			.arg(qPrintable(tableSchema))
+			.arg(qPrintable(*m_tableSchemaAttrPtr))
 			.arg(qPrintable(*m_tableNameAttrPtr))
 			.arg(filterQuery)
 			.toUtf8();
@@ -98,12 +92,9 @@ QByteArray CSqlDatabaseObjectDelegateCompBase::GetSelectionQuery(
 			const iprm::IParamsSet* paramsPtr) const
 {
 	if (!objectId.isEmpty()){
-		QByteArray tableSchema;
 		if (m_tableSchemaAttrPtr.IsValid()){
-			tableSchema = *m_tableSchemaAttrPtr;
-
 			return QString("SELECT * FROM %0.\"%1\" WHERE \"%2\" = '%3'")
-				.arg(qPrintable(tableSchema))
+				.arg(qPrintable(*m_tableSchemaAttrPtr))
 				.arg(qPrintable(*m_tableNameAttrPtr))
 				.arg(qPrintable(*m_objectIdColumnAttrPtr))
 				.arg(qPrintable(objectId))
@@ -208,17 +199,12 @@ QVariant CSqlDatabaseObjectDelegateCompBase::GetElementInfoFromRecord(const QSql
 
 QByteArray CSqlDatabaseObjectDelegateCompBase::CreateResetQuery(const imtbase::IObjectCollection& /*collection*/) const
 {
-	QByteArray tableSchema;
-	if (m_tableSchemaAttrPtr.IsValid()){
-		tableSchema = *m_tableSchemaAttrPtr;
-	}
-
-	if (tableSchema.isEmpty()){
+	if (!m_tableSchemaAttrPtr.IsValid()){
 		return QString("DELETE FROM \"%1\";").arg(qPrintable(*m_tableNameAttrPtr)).toUtf8();
 	}
 
 	return QString("DELETE FROM %0.\"%1\";")
-			.arg(qPrintable(tableSchema))
+			.arg(qPrintable(*m_tableSchemaAttrPtr))
 			.arg(qPrintable(*m_tableNameAttrPtr))
 			.toUtf8();
 }
@@ -250,17 +236,12 @@ QByteArray CSqlDatabaseObjectDelegateCompBase::CreateCollectionItemMetaInfoQuery
 
 QString CSqlDatabaseObjectDelegateCompBase::GetBaseSelectionQuery() const
 {
-	QByteArray tableSchema ;
-	if (m_tableSchemaAttrPtr.IsValid()){
-		tableSchema = *m_tableSchemaAttrPtr;
-	}
-
-	if (tableSchema.isEmpty()){
+	if (!m_tableSchemaAttrPtr.IsValid()){
 		return QString("SELECT * FROM \"%1\"").arg(qPrintable(*m_tableNameAttrPtr));
 	}
 
 	return QString("SELECT * FROM %0.\"%1\"")
-			.arg(qPrintable(tableSchema))
+			.arg(qPrintable(*m_tableSchemaAttrPtr))
 			.arg(qPrintable(*m_tableNameAttrPtr));
 }
 
