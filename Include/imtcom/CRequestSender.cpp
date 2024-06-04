e8d7e8900bfca159a6015787875c6109dab4a5c4
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-06-04 18:10:16 +0000

    CRequestSender ignore ssl error on get/post
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@9951 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtcom/CRequestSender.cpp
+++ Include/imtcom/CRequestSender.cpp
@@ -17,6 +17,8 @@ QNetworkReply* CRequestSender::DoSyncGet(const QNetworkRequest& request, int tim
 
 	QNetworkReply* replyPtr = networkOperation.networkManagerPtr->get(request);
 	if (replyPtr != nullptr){
+        replyPtr->ignoreSslErrors();
+
 		networkOperation.connectionLoop.exec(QEventLoop::ExcludeUserInputEvents);
 		networkOperation.timer.stop();
 
@@ -51,14 +53,17 @@ QNetworkReply* CRequestSender::DoSyncPost(const QNetworkRequest& request, const 
 {
 	NetworkOperation networkOperation(timeout);
 
-	QNetworkReply* replyPtr = networkOperation.networkManagerPtr->post(request, data);
+    QNetworkReply* replyPtr = networkOperation.networkManagerPtr->post(request, data);
+
 	if (replyPtr != nullptr){
+        replyPtr->ignoreSslErrors();
+
 		networkOperation.connectionLoop.exec(QEventLoop::ExcludeUserInputEvents);
 		networkOperation.timer.stop();
 
-		if (replyPtr->isRunning()){
-			replyPtr->abort();
-		}
+        if (replyPtr->isRunning()){
+            replyPtr->abort();
+        }
 	}
 
 	return replyPtr;
