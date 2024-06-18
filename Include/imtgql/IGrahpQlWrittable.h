6cf5d18d1ddd233ccd5c3e88573fed18dc923140
Sergej.Zastrovnyh <Sergej.Zastrovnyh@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-06-18 05:47:11 +0000

    imtgql: self writtable classes for GraphQL object added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@10078 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtgql/IGrahpQlWrittable.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+
+// ACF includes
+#include <istd/IPolymorphic.h>
+
+// ImtCore includes
+#include <imtgql/CGqlRequest.h>
+
+
+namespace imtgql
+{
+
+
+class IGrahpQlWrittable: virtual public istd::IPolymorphic
+{
+	virtual bool WriteToGraphQlObject(imtgql::CGqlObject& request) const = 0;
+};
+
+
+} // namespace imtqml
+
+
