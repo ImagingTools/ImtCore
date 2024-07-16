3a3fadc9db89a30f827c46afb5a018a98cdbecb6
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-07-16 05:07:05 +0000

    Mdbx.cmake added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@10408 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Config/CMake/Mdbx.cmake
@@ -0,0 +1,7 @@
+include_directories(${IMTCOREDIR}/3rdParty/libmdbx/include)
+
+if(WIN32)
+	target_link_libraries(${PROJECT_NAME} "${IMTCOREDIR}/3rdParty/libmdbx/libs/${CMAKE_BUILD_TYPE}_${TARGETNAME}/libmdbx.lib")
+elseif(UNIX)
+	target_link_libraries(${PROJECT_NAME} "${IMTCOREDIR}/3rdParty/libmdbx/libs/${COMPILER_CODE}/libmdbx.a")
+endif()
