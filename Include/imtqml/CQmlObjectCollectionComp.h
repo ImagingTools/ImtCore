3adce3340de94036e0e7a38361e3de4578539575
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-07-23 19:35:18 +0000

    Cosmetics
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@10509 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtqml/CQmlObjectCollectionComp.h
+++ Include/imtqml/CQmlObjectCollectionComp.h
@@ -17,27 +17,22 @@ namespace imtqml
 class CQmlObjectCollectionComp:
 			public QObject,
 			public ilog::CLoggerComponentBase
-
 {
 	Q_OBJECT
-
 public:
-    typedef ilog::CLoggerComponentBase BaseClass;
+	typedef ilog::CLoggerComponentBase BaseClass;
 
-    I_BEGIN_COMPONENT(CQmlObjectCollectionComp);
+	I_BEGIN_COMPONENT(CQmlObjectCollectionComp);
 		I_ASSIGN(m_objectCollectionCompPtr, "ObjectCollection", "Object collection", true, "ObjectCollection");
-    I_END_COMPONENT;
+	I_END_COMPONENT;
 
 	Q_INVOKABLE virtual int GetElementsCount(QString filterParams = "");
 	Q_INVOKABLE virtual imtqml::CQmlObjectCollectionIterator* CreateObjectCollectionIterator(int offset = 0, int count = -1, QString selectionParams = "");
 
-
 private:
 	iprm::CParamsSet* GetFilters(QString filterParams);
 
-    I_REF(imtbase::IObjectCollection, m_objectCollectionCompPtr);
-
-
+	I_REF(imtbase::IObjectCollection, m_objectCollectionCompPtr);
 };
 
 
