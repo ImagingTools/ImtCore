ab02519e247716ad2066416ede4101c3be1e27ac
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-07-25 15:59:42 +0000

    HTTP redirect simplified
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@10546 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtrest/CHttpRedirectComp.cpp
+++ Include/imtrest/CHttpRedirectComp.cpp
@@ -13,25 +13,45 @@ namespace imtrest
 
 // reimplemented (IProtocolEngine)
 
-bool CHttpRedirectComp::IsCommandSupported(const QByteArray& /*commandId*/) const
+bool CHttpRedirectComp::IsCommandSupported(const QByteArray& commandId) const
 {
-	return true;
+	return commandId.isEmpty();
 }
 
 
 ConstResponsePtr CHttpRedirectComp::ProcessRequest(const IRequest& request, const QByteArray& /*subCommandId*/) const
 {
-	QByteArray body;
-	body = R"(<head> <meta http-equiv="refresh" content="0; URL=)";
-	body.append(*m_redirectToCommandIdAttrPtr);
-	body.append(R"(" /></head>)");
+	QByteArray commandId = request.GetCommandId();
+	if (commandId.startsWith('/')) {
+		commandId = commandId.remove(0, 1);
+	}
+
+	if (commandId.endsWith('/')) {
+		commandId = commandId.remove(commandId.length() - 1, 1);
+	}
+
+	if (commandId.isEmpty()) {
+		QByteArray body;
+		body = R"(<head> <meta http-equiv="refresh" content="0; URL=)";
+		body.append(*m_redirectToCommandIdAttrPtr);
+		body.append(R"(" /></head>)");
+
+		ConstResponsePtr retval = ConstResponsePtr(request.GetProtocolEngine()
+			.CreateResponse(
+				request,
+				IProtocolEngine::SC_MOVED_PERMANENTLY,
+				body,
+				"text/html; charset=UTF-8"));
+
+		return retval;
+	}
 
 	ConstResponsePtr retval = ConstResponsePtr(request.GetProtocolEngine()
-				.CreateResponse(
-							request,
-							IProtocolEngine::SC_MOVED_PERMANENTLY,
-							body,
-							"text/html; charset=UTF-8"));
+		.CreateResponse(
+			request,
+			IProtocolEngine::SC_NOT_FOUND,
+			QByteArray(),
+			"text/html; charset=UTF-8"));
 
 	return retval;
 }
