fd5946961f40177f7cc8768facfe884db3355d38
Sergej.Zastrovnyh <Sergej.Zastrovnyh@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-08-26 17:25:05 +0000

    imtcom PCK: add SSL configuration components
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@10876 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/ImtComPck/ImtComPck.cpp
+++ Impl/ImtComPck/ImtComPck.cpp
@@ -29,6 +29,47 @@ I_EXPORT_COMPONENT(
 			"Async connection checker",
 			"Async Connection Checker");
 
+I_EXPORT_COMPONENT(
+			SslConfiguration,
+			"The SSL Configuration is used by networking classes to relay information about an open SSL connection and to allow the application to control certain features of that connection.",
+			"SSL Secure Connection Server Client Network Networking" IM_AUTHOR("Sergey Zastrovnyh"));
+
+I_EXPORT_COMPONENT(
+			SslKey,
+			"The SSL Configuration's key",
+			"SSL Secure Connection Server Client Network Networking Key" IM_AUTHOR("Sergey Zastrovnyh"));
+
+I_EXPORT_COMPONENT(
+			SslCertificate,
+			"The SSL Configuration's certificate",
+			"SSL Secure Connection Server Client Network Networking Certificate" IM_AUTHOR("Sergey Zastrovnyh"));
+
+I_EXPORT_COMPONENT(
+			SslEncodingFormatOptionList,
+			"The SSL key's encoding format",
+			"SSL Secure Connection Server Client Network Networking" IM_AUTHOR("Sergey Zastrovnyh"));
+
+I_EXPORT_COMPONENT(
+			SslKeyAlgorithmOptionList,
+			"The SSL key's algorithm",
+			"SSL Secure Connection Server Client Network Networking" IM_AUTHOR("Sergey Zastrovnyh"));
+
+I_EXPORT_COMPONENT(
+			SslKeyTypeOptionList,
+			"The SSL key's type",
+			"SSL Secure Connection Server Client Network Networking" IM_AUTHOR("Sergey Zastrovnyh"));
+
+I_EXPORT_COMPONENT(
+			SslProtocolOptionList,
+			"The SSL Configuration's protocol",
+			"SSL Secure Connection Server Client Network Networking" IM_AUTHOR("Sergey Zastrovnyh"));
+
+I_EXPORT_COMPONENT(
+			SslVerifyModeOptionList,
+			"The SSL Configuration's verify mode",
+			"SSL Secure Connection Server Client Network Networking" IM_AUTHOR("Sergey Zastrovnyh"));
+
+
 
 } // namespace ImtComPck
 
--- Impl/ImtComPck/ImtComPck.h
+++ Impl/ImtComPck/ImtComPck.h
@@ -10,6 +10,14 @@
 #include <imtcom/CInternetConnectionCheckerComp.h>
 #include <imtcom/CSimpleConnectionCheckerComp.h>
 #include <imtcom/CAsyncConnectionCheckerComp.h>
+#include <imtcom/CSslConfigurationComp.h>
+#include <imtcom/CSslKeyComp.h>
+#include <imtcom/CSslCertificateComp.h>
+#include <imtcom/CSslEncodingFormatOptionList.h>
+#include <imtcom/CSslKeyAlgorithmOptionList.h>
+#include <imtcom/CSslKeyTypeOptionList.h>
+#include <imtcom/CSslProtocolOptionList.h>
+#include <imtcom/CSslVerifyModeOptionList.h>
 
 
 /**
@@ -22,6 +30,29 @@ namespace ImtComPck
 typedef icomp::TModelCompWrap<imtcom::CInternetConnectionCheckerComp> InternetConnectionChecker;
 typedef imtcom::CSimpleConnectionCheckerComp SimpleConnectionChecker;
 typedef icomp::TModelCompWrap<imtcom::CAsyncConnectionCheckerComp> AsyncConnectionChecker;
+typedef icomp::TModelCompWrap<imtcom::CSslConfigurationComp> SslConfiguration;
+typedef icomp::TModelCompWrap<imtcom::CSslKeyComp> SslKey;
+typedef icomp::TModelCompWrap<imtcom::CSslCertificateComp> SslCertificate;
+typedef icomp::TModelCompWrap<
+	icomp::TMakeComponentWrap<
+		imtcom::CSslEncodingFormatOptionList,
+		iprm::IOptionsManager, iprm::IOptionsList, iprm::ISelectionParam>> SslEncodingFormatOptionList;
+typedef icomp::TModelCompWrap<
+	icomp::TMakeComponentWrap<
+		imtcom::CSslKeyAlgorithmOptionList,
+		iprm::IOptionsManager, iprm::IOptionsList, iprm::ISelectionParam>> SslKeyAlgorithmOptionList;
+typedef icomp::TModelCompWrap<
+	icomp::TMakeComponentWrap<
+		imtcom::CSslKeyTypeOptionList,
+		iprm::IOptionsManager, iprm::IOptionsList, iprm::ISelectionParam>> SslKeyTypeOptionList;
+typedef icomp::TModelCompWrap<
+	icomp::TMakeComponentWrap<
+		imtcom::CSslProtocolOptionList,
+		iprm::IOptionsManager, iprm::IOptionsList, iprm::ISelectionParam>> SslProtocolOptionList;
+typedef icomp::TModelCompWrap<
+	icomp::TMakeComponentWrap<
+		imtcom::CSslVerifyModeOptionList,
+		iprm::IOptionsManager, iprm::IOptionsList, iprm::ISelectionParam>> SslVerifyModeOptionList;
 
 
 } // namespace ImtComPck
