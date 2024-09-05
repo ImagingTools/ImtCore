14bc2ab1ff9a41bd2113b3b1e5aa6cf7b8d15d0a
Sergej.Zastrovnyh <Sergej.Zastrovnyh@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-09-05 06:39:51 +0000

    imtsdl: QRC persistance added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@10993 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtsdl/CQtResourceFilePersistenceComp.h
@@ -0,0 +1,67 @@
+#pragma once
+
+
+// Qt includes
+#include <QtCore/QFileInfo>
+
+// ACF includes
+#include <ilog/TLoggerCompWrap.h>
+#include <ifile/IFilePersistence.h>
+#include <ifile/IFilePersistenceInfo.h>
+#include <iprm/IParamsManager.h>
+
+
+namespace imtsdl
+{
+
+
+class CQtResourceFilePersistenceComp:
+			public ilog::CLoggerComponentBase,
+			virtual public ifile::IFilePersistence,
+			virtual public ifile::IFilePersistenceInfo
+{
+public:
+
+
+public:
+	typedef ilog::CLoggerComponentBase BaseClass;
+
+	I_BEGIN_COMPONENT(CQtResourceFilePersistenceComp)
+		I_REGISTER_INTERFACE(ifile::IFileTypeInfo)
+		I_REGISTER_INTERFACE(ifile::IFilePersistence)
+		I_REGISTER_INTERFACE(ifile::IFilePersistenceInfo)
+	I_END_COMPONENT
+
+	// reimplemented (ifile::IFilePersistence)
+	virtual bool IsOperationSupported(
+		const istd::IChangeable* dataObjectPtr,
+		const QString* filePathPtr = NULL,
+		int flags = -1,
+		bool beQuiet = true) const override;
+	virtual int LoadFromFile(
+		istd::IChangeable& data,
+		const QString& filePath = QString(),
+		ibase::IProgressManager* progressManagerPtr = NULL) const override;
+	virtual int SaveToFile(
+		const istd::IChangeable& data,
+		const QString& filePath = QString(),
+		ibase::IProgressManager* progressManagerPtr = NULL) const override;
+
+	// reimplemented (ifile::IFileTypeInfo)
+	virtual bool GetFileExtensions(QStringList& result, const istd::IChangeable* dataObjectPtr = NULL, int flags = -1, bool doAppend = false) const override;
+	virtual QString GetTypeDescription(const QString* extensionPtr = NULL) const override;
+
+	// reimplemented (ifile::IFilePersistenceInfo)
+	virtual QString GetLastFilePath(OperationType operationType = OT_UNKNOWN, PathType pathType = PT_COMPLETE) const override;
+
+protected:
+	QString GetPathForType(const QFileInfo& fileInfo, PathType pathType) const;
+
+private:
+
+	mutable QFileInfo m_lastOpenInfo;
+	mutable QFileInfo m_lastSaveInfo;
+};
+
+
+} // namespace imtsdl
