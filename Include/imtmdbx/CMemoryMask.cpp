9be9f08e2aacea553baa7e72b792f875a67abe5f
Alexander.Vechter <Alexander.Vechter@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-10-22 12:07:05 +0000

    imtmdbx/CMemoryMask.cpp (add new class)
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@11600 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtmdbx/CMemoryMask.cpp
@@ -0,0 +1,159 @@
+#include <imtmdbx/CMemoryMask.h>
+
+// Qt includes
+#include <QtCore/QDebug>
+
+
+namespace imtmdbx
+{
+
+int getItemCount = 0;
+
+CMemoryMask::CMemoryMask()
+{
+
+}
+
+
+bool CMemoryMask::GetUnit(quint64 position)
+{
+    quint64 offset = position / 64;
+    quint64 bitPosition = position % 64;
+
+    quint64 item;
+
+    if (!m_cache.contains(offset)){
+        item = 0;
+    }
+    else {
+        item = m_cache.value(offset);
+    }
+
+    return (item >> bitPosition) & (quint64)1;
+}
+
+
+bool CMemoryMask::SetUnit(quint64 position, bool unit)
+{
+    quint64 offset = position / 64;
+    quint64 bitPosition = position % 64;
+
+    quint64 item;
+
+    if (!m_cache.contains(offset)){
+        item = 0;
+    }
+    else {
+        item = m_cache.value(offset);
+    }
+
+    if(unit){
+        item = item | ((quint64)1 << bitPosition);
+    }
+    else {
+        item = item & ~((quint64)1 << bitPosition);
+    }
+
+    //item = (item & ~((quint64)1 << bitPosition)) | ((quint64)unit << bitPosition);
+
+    SetItem(offset, item);
+
+    return true;
+}
+
+
+bool CMemoryMask::GetItem(quint64 offset, quint64& item)
+{
+    if (!m_cache.contains(offset)){
+        item = 0;
+    }
+    else {
+        item = m_cache.value(offset);
+    }
+
+    return true;
+}
+
+
+bool CMemoryMask::SetItem(quint64 offset, quint64 item)
+{
+    m_cache.insert(offset, item);
+
+    return true;
+}
+
+
+bool CMemoryMask::GetNearestOffset(quint64& offset, quint64 startOffset)
+{
+    if(m_cache.isEmpty()){
+        return false;
+    }
+
+    QList<quint64> mapKeys = m_cache.keys();
+
+    quint64 biggestKey = mapKeys.at(mapKeys.length() -1);
+    if(startOffset > biggestKey){
+        return false;
+    }
+
+    for(int i = 0; i < mapKeys.length(); i++){
+        quint64 key = mapKeys.at(i);
+        if(key >= startOffset){
+            offset = key;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+bool CMemoryMask::GetNextItemOffset(quint64& offset, quint64 startOffset)
+{
+    if(m_cache.isEmpty()){
+        return false;
+    }
+
+    QList<quint64> mapKeys = m_cache.keys();
+
+    quint64 biggestKey = mapKeys.at(mapKeys.length() -1);
+    if(startOffset > biggestKey){
+        return false;
+    }
+
+    for(int i = 0; i < mapKeys.length(); i++){
+        quint64 key = mapKeys.at(i);
+        if(key > startOffset){
+            offset = key;
+            return true;
+        }
+    }
+    return false;
+}
+
+
+bool CMemoryMask::GetPreviosItemOffset(quint64& offset, quint64 startOffset)
+{
+    if(m_cache.isEmpty()){
+        return false;
+    }
+
+    QList<quint64> mapKeys = m_cache.keys();
+
+    quint64 smallestKey = mapKeys.at(0);
+    if(startOffset < smallestKey){
+        return false;
+    }
+
+    for(int i = mapKeys.length() -1; i >= 0; i--){
+        quint64 key = mapKeys.at(i);
+        if(key < startOffset){
+            offset = key;
+            return true;
+        }
+    }
+    return false;
+}
+
+
+}//namespace imtmdbx
---
+++ Include/imtmdbx/CMemoryMask.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// ImtCore includes
+#include <imtmdbx/IMask.h>
+#include <imtmdbx/CDocumentTable.h>
+
+// Qt includes
+#include <QtCore/QMap>
+
+
+namespace imtmdbx
+{
+
+extern int getItemCount;
+
+class CMemoryMask: virtual public IMask
+{
+public:
+    CMemoryMask();
+
+	virtual bool GetUnit(quint64 position) override;
+	virtual bool SetUnit(quint64 position, bool unit) override;
+
+
+protected:
+	virtual bool GetItem(quint64 offset, quint64& item) override;
+	virtual bool SetItem(quint64 offset, quint64 item) override;
+	virtual bool GetNearestOffset(quint64& offset, quint64 startOffset = 0) override;
+	virtual bool GetNextItemOffset(quint64& offset, quint64 startOffset = -1) override;
+	virtual bool GetPreviosItemOffset(quint64& offset, quint64 startOffset = 0xffffffff) override;
+	QMap<quint64,quint64> m_cache;
+};
+
+}//namespace imtmdbx
