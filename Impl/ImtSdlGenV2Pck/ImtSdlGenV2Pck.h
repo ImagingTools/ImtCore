575882c4ff5ea5cf5a9378a57b7e4cef303d5d54
Sergej.Zastrovnyh <Sergej.Zastrovnyh@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-11-15 09:04:53 +0000

    ImtSdlGenPckV2: cosmetics; missing components registered
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@11920 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/ImtSdlGenV2Pck/ImtSdlGenV2Pck.h
+++ Impl/ImtSdlGenV2Pck/ImtSdlGenV2Pck.h
@@ -6,10 +6,13 @@
 
 #include <imtsdlgenv2/CSdlProcessArgumentsParserComp.h>
 #include <imtsdlgenv2/CSdlClassCodeGeneratorComp.h>
-#include <imtsdlgenv2/CSdlClassGqlModificatorComp.h>
 #include <imtsdlgenv2/CSdlClassJsonModificatorComp.h>
 #include <imtsdlgenv2/CSdlClassTreeModelModificatorComp.h>
 #include <imtsdlgenv2/CSdlGeneralManagerComp.h>
+#include <imtsdlgenv2/CSdlClassGqlModificatorComp.h>
+#include <imtsdlgenv2/CGqlWrapClassCodeGeneratorComp.h>
+#include <imtsdlgenv2/CGqlHandlerBaseClassGeneratorComp.h>
+#include <imtsdlgenv2/CGqlCollectionControllerBaseClassGeneratorComp.h>
 
 /**
 	ImtSdlPck package
@@ -24,6 +27,9 @@ typedef imtsdlgenv2::CSdlClassCodeGeneratorComp SdlClassCodeGenerator;
 typedef imtsdlgenv2::CSdlClassTreeModelModificatorComp SdlClassTreeModelModificator;
 typedef imtsdlgenv2::CSdlClassGqlModificatorComp SdlClassGqlModificator;
 typedef imtsdlgenv2::CSdlClassJsonModificatorComp SdlClassJsonModificator;
+typedef imtsdlgenv2::CGqlWrapClassCodeGeneratorComp GqlWrapClassCodeGenerator;
+typedef imtsdlgenv2::CGqlHandlerBaseClassGeneratorComp GqlHandlerBaseClassGenerator;
+typedef imtsdlgenv2::CGqlCollectionControllerBaseClassGeneratorComp GqlCollectionControllerBaseClassGenerator;
 
 
 } // namespace ImtSdlGenV2Pck
