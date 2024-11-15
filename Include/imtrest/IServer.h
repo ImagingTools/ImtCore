75571d9a2362bc26e59c8498edc138bce690f4d5
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-11-15 14:39:10 +0000

    IServer.h added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@11930 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtrest/IServer.h
@@ -0,0 +1,41 @@
+#pragma once
+
+
+// ACF includes
+#include <istd/IPolymorphic.h>
+
+
+namespace imtrest
+{
+
+
+/**
+	Interface describing manage of server.
+*/
+class IServer: virtual public istd::IPolymorphic
+{
+public:
+	enum ServerStatus
+	{
+		SS_NOT_STARTED,
+		SS_LISTENING
+	};
+
+	/**
+		Start server
+	*/
+	virtual bool StartServer() = 0;
+	/**
+	   Stop server
+	*/
+	virtual bool StopServer() = 0;
+	/**
+	   Get current server status
+	*/
+	virtual ServerStatus GetServerStatus() const = 0;
+};
+
+
+} // namespace imtrest
+
+
