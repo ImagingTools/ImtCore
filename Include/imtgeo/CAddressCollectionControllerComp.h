fde1656366d2e89799e5701e01832d597649e5bf
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-11-25 12:02:55 +0000

    VK: Chronos legacy collection controllers now inherit CLegacyObjectCollectionControllerCompBase
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@12086 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtgeo/CAddressCollectionControllerComp.h
+++ Include/imtgeo/CAddressCollectionControllerComp.h
@@ -2,29 +2,29 @@
 
 
 // ImtCore includes
-#include <imtservergql/CObjectCollectionControllerCompBase.h>
+#include <imtservergql/CLegacyObjectCollectionControllerCompBase.h>
 
 
 namespace imtgeo
 {
 
 
-class CAddressCollectionControllerComp: public imtservergql::CObjectCollectionControllerCompBase
+class CAddressCollectionControllerComp: public imtservergql::CLegacyObjectCollectionControllerCompBase
 {
 public:
-	typedef imtservergql::CObjectCollectionControllerCompBase BaseClass;
+	typedef imtservergql::CLegacyObjectCollectionControllerCompBase BaseClass;
 
-    I_BEGIN_COMPONENT(CAddressCollectionControllerComp);
-        I_ASSIGN(m_addressTypeCollectionPtr, "AddressTypeComponent", "The string address type collection", true, "AddressTypeCollection");
-    I_END_COMPONENT;
+	I_BEGIN_COMPONENT(CAddressCollectionControllerComp);
+		I_ASSIGN(m_addressTypeCollectionPtr, "AddressTypeComponent", "The string address type collection", true, "AddressTypeCollection");
+	I_END_COMPONENT;
 
 protected:
-    I_REF(imtbase::IObjectCollection, m_addressTypeCollectionPtr);
+	I_REF(imtbase::IObjectCollection, m_addressTypeCollectionPtr);
 
-	// reimplemented (imtservergql::CObjectCollectionControllerCompBase)
-    virtual QVariant GetObjectInformation(const QByteArray& informationId, const QByteArray& objectId) const;
-    virtual imtbase::CTreeItemModel* GetMetaInfo(const imtgql::CGqlRequest& gqlRequest, QString& errorMessage) const override;
-    virtual imtbase::CTreeItemModel* ListObjects(const imtgql::CGqlRequest& gqlRequest, QString& errorMessage) const override;
+	// reimplemented (imtservergql::CLegacyObjectCollectionControllerCompBase)
+	virtual QVariant GetObjectInformation(const QByteArray& informationId, const QByteArray& objectId) const override;
+	virtual imtbase::CTreeItemModel* GetMetaInfo(const imtgql::CGqlRequest& gqlRequest, QString& errorMessage) const override;
+	virtual imtbase::CTreeItemModel* ListObjects(const imtgql::CGqlRequest& gqlRequest, QString& errorMessage) const override;
 };
 
 
