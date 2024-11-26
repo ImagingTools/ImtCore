cba951af1e2435ec21298a75f174b172679e1659
Sergej.Zastrovnyh <Sergej.Zastrovnyh@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-11-26 04:59:10 +0000

    imtlicgql: cosmetics
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@12096 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtlicgql/CProductControllerComp.h
+++ Include/imtlicgql/CProductControllerComp.h
@@ -20,31 +20,36 @@ public:
 	I_END_COMPONENT;
 
 protected:
+	// reimplemented (sdl::imtlic::Products::V1_0::CGraphQlHandlerCompBase)
 	virtual sdl::imtlic::Products::V1_0::CProductsListPayload OnProductsList(
-		const sdl::imtlic::Products::V1_0::CProductsListGqlRequest& productsListRequest,
-		const ::imtgql::CGqlRequest& gqlRequest,
-		QString& errorMessage) const override;
-	virtual sdl::imtlic::Products::V1_0::CProductDataPayload OnProductItem(
-		const sdl::imtlic::Products::V1_0::CProductItemGqlRequest& productItemRequest,
-		const ::imtgql::CGqlRequest& gqlRequest,
-		QString& errorMessage) const override;
-	virtual sdl::imtlic::Products::V1_0::CProductExportResponse OnProductsExport(
-		const sdl::imtlic::Products::V1_0::CProductsExportGqlRequest& productsExportRequest,
-		const ::imtgql::CGqlRequest& gqlRequest,
-		QString& errorMessage) const override;
-	virtual ::sdl::imtbase::ImtCollection::V1_0::CUpdatedNotificationPayload OnProductUpdate(
-		const sdl::imtlic::Products::V1_0::CProductUpdateGqlRequest& productUpdateRequest,
-		const ::imtgql::CGqlRequest& gqlRequest,
-		QString& errorMessage) const override;
-	virtual ::sdl::imtbase::ImtCollection::V1_0::CAddedNotificationPayload OnProductAdd(
-		const sdl::imtlic::Products::V1_0::CProductAddGqlRequest& productAddRequest,
-		const ::imtgql::CGqlRequest& gqlRequest,
-		QString& errorMessage) const override;
-	virtual sdl::imtlic::Products::V1_0:: CProductImportResponse OnProductsImport(
-		const sdl::imtlic::Products::V1_0::CProductsImportGqlRequest& productsImportRequest,
-		const ::imtgql::CGqlRequest& gqlRequest,
-		QString& errorMessage) const override;
+				const sdl::imtlic::Products::V1_0::CProductsListGqlRequest& productsListRequest,
+				const ::imtgql::CGqlRequest& gqlRequest,
+				QString& errorMessage) const override;
 
+	virtual sdl::imtlic::Products::V1_0::CProductDataPayload OnProductItem(
+				const sdl::imtlic::Products::V1_0::CProductItemGqlRequest& productItemRequest,
+				const ::imtgql::CGqlRequest& gqlRequest,
+				QString& errorMessage) const override;
+
+	virtual sdl::imtlic::Products::V1_0::CProductExportResponse OnProductsExport(
+				const sdl::imtlic::Products::V1_0::CProductsExportGqlRequest& productsExportRequest,
+				const ::imtgql::CGqlRequest& gqlRequest,
+				QString& errorMessage) const override;
+
+	virtual ::sdl::imtbase::ImtCollection::V1_0::CUpdatedNotificationPayload OnProductUpdate(
+				const sdl::imtlic::Products::V1_0::CProductUpdateGqlRequest& productUpdateRequest,
+				const ::imtgql::CGqlRequest& gqlRequest,
+				QString& errorMessage) const override;
+
+	virtual ::sdl::imtbase::ImtCollection::V1_0::CAddedNotificationPayload OnProductAdd(
+				const sdl::imtlic::Products::V1_0::CProductAddGqlRequest& productAddRequest,
+				const ::imtgql::CGqlRequest& gqlRequest,
+				QString& errorMessage) const override;
+
+	virtual sdl::imtlic::Products::V1_0:: CProductImportResponse OnProductsImport(
+				const sdl::imtlic::Products::V1_0::CProductsImportGqlRequest& productsImportRequest,
+				const ::imtgql::CGqlRequest& gqlRequest,
+				QString& errorMessage) const override;
 };
 
 
