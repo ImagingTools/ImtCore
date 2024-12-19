2ceacff7c2a0f6de68bd99d2a8bae6b23249e9cd
Vladimir.Cherepanov <Vladimir.Cherepanov@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-12-19 04:15:37 +0000

    TSdlBasedMetaInfoDelegate adapted for gmg
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@12365 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtdbgql/TSdlBasedMetaInfoDelegate.h
+++ Include/imtdbgql/TSdlBasedMetaInfoDelegate.h
@@ -70,6 +70,58 @@ idoc::MetaInfoPtr TSdlBasedMetaInfoDelegate<MetaInfoRepresentation>::FromJsonRep
 }
 
 
+template<class MetaInfoRepresentation>
+class TGmgSdlBasedMetaInfoDelegate : virtual public imtdb::IJsonBasedMetaInfoDelegate
+{
+public:
+	virtual QByteArray ToJsonRepresentation(const idoc::IDocumentMetaInfo& metaInfo) const override;
+	virtual idoc::MetaInfoPtr FromJsonRepresentation(const QByteArray& data) const override;
+
+protected:
+	virtual bool FillRepresentation(MetaInfoRepresentation& metaInfoRepresentation, const idoc::IDocumentMetaInfo& metaInfo) const = 0;
+	virtual bool FillMetaInfo(idoc::IDocumentMetaInfo& metaInfo, const MetaInfoRepresentation& metaInfoRepresentation) const = 0;
+};
+
+
+template<class MetaInfoRepresentation>
+QByteArray TGmgSdlBasedMetaInfoDelegate<MetaInfoRepresentation>::ToJsonRepresentation(const idoc::IDocumentMetaInfo& metaInfo) const
+{
+	QByteArray retVal;
+
+	MetaInfoRepresentation representation;
+	if (FillRepresentation(representation, metaInfo)){
+		QJsonObject object;
+		if (representation.WriteToJsonObject(object)){
+			QJsonDocument document(object);
+			retVal = document.toJson(QJsonDocument::Compact);
+		}
+	}
+
+	return retVal;
+}
+
+
+template<class MetaInfoRepresentation>
+idoc::MetaInfoPtr TGmgSdlBasedMetaInfoDelegate<MetaInfoRepresentation>::FromJsonRepresentation(const QByteArray& data) const
+{
+	QJsonDocument document;
+	QJsonParseError error;
+	document = document.fromJson(data, &error);
+	if (document.isObject() && error.error == QJsonParseError::NoError){
+		QJsonObject object = document.object();
+		MetaInfoRepresentation representation;
+		if (MetaInfoRepresentation::ReadFromJsonObject(representation, object)){
+			idoc::MetaInfoPtr retVal(new imod::TModelWrap<idoc::CStandardDocumentMetaInfo>());
+			if (FillMetaInfo(*retVal, representation)){
+				return retVal;
+			}
+		}
+	}
+
+	return idoc::MetaInfoPtr();
+}
+
+
 } // namespace imtdbgql
 
 
