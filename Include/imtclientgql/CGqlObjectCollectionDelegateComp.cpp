87950e34feb024b406e56956de1e507d7b5dd4fc
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-01-20 17:52:46 +0000

    Changes from Q
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@12664 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtclientgql/CGqlObjectCollectionDelegateComp.cpp
+++ Include/imtclientgql/CGqlObjectCollectionDelegateComp.cpp
@@ -584,6 +584,10 @@ CGqlObjectCollectionDelegateComp::ResponseData CGqlObjectCollectionDelegateComp:
 	imtgql::IGqlResponse::GqlRequestPtr requestPtr = response.GetOriginalRequest();
 	if (!requestPtr.isNull()){
 		data.commandId = requestPtr->GetCommandId();
+
+		if (!data.data.contains(data.commandId)) {
+			SendErrorMessage(0, "GqlResponse data don't contains " + data.commandId);
+		}
 	}
 
 	QJsonDocument document = QJsonDocument::fromJson(response.GetResponseData());
