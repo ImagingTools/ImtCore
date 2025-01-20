efd35688303faf079b6097b06666851a47e5e833
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-01-20 17:50:44 +0000

    Changes from Q
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@12663 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtbase/ICollectionObjectLink.h
+++ Include/imtbase/ICollectionObjectLink.h
@@ -24,16 +24,23 @@ public:
 	template <class Object>
 	inline bool GetLinkedObject(istd::TSmartPtr<Object>& dataObject) const
 	{
+		imtbase::IObjectCollection::DataPtr dataPtr;
+		if (GetLinkedObject(dataPtr)){
+			dataObject.SetCastedOrRemove(dataPtr->CloneMe());
+
+			return dataObject.IsValid();
+		}
+
+		return false;
+	}
+
+	inline bool GetLinkedObject(imtbase::IObjectCollection::DataPtr& dataObject) const
+	{
 		const imtbase::IObjectCollection* collectionPtr = GetCollection();
 		if (collectionPtr != nullptr){
 			QByteArray objectId = GetObjectUuid();
 			if (!objectId.isEmpty()){
-				imtbase::IObjectCollection::DataPtr dataPtr;
-				if (collectionPtr->GetObjectData(objectId, dataPtr)){
-					dataObject.SetCastedOrRemove(dataPtr->CloneMe());
-
-					return dataObject.IsValid();
-				}
+				return collectionPtr->GetObjectData(objectId, dataObject);
 			}
 		}
 
