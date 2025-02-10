f25e990a57bae154828ebd756df3cb382c3767fb
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-02-10 09:52:10 +0000

    Authorization options SDL corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@12939 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtauthgql/CAuthorizationOptionsControllerComp.cpp
+++ Include/imtauthgql/CAuthorizationOptionsControllerComp.cpp
@@ -22,19 +22,19 @@ sdl::imtauth::AuthorizationOptions::CUserManagementPayload CAuthorizationOptions
 	sdl::imtauth::AuthorizationOptions::CUserManagementPayload payload;
 	int index = m_selectionParamCompPtr->GetSelectedOptionIndex();
 
-	sdl::imtauth::AuthorizationOptions::UserMode userMode =
-		sdl::imtauth::AuthorizationOptions::UserMode::STRONG_USER_MANAGEMENT;
+	sdl::imtauth::AuthorizationOptions::UserManagementMode userMode =
+		sdl::imtauth::AuthorizationOptions::UserManagementMode::STRONG_USER_MANAGEMENT;
 	switch (index) {
 	case UM_NO_USER_MANAGEMENT:
-		userMode = sdl::imtauth::AuthorizationOptions::UserMode::NO_USER_MANAGEMENT;
+		userMode = sdl::imtauth::AuthorizationOptions::UserManagementMode::NO_USER_MANAGEMENT;
 		break;
 
 	case UM_OPTIONAL_USER_MANAGEMENT:
-		userMode = sdl::imtauth::AuthorizationOptions::UserMode::OPTIONAL_USER_MANAGEMENT;
+		userMode = sdl::imtauth::AuthorizationOptions::UserManagementMode::OPTIONAL_USER_MANAGEMENT;
 		break;
 
 	case UM_STRONG_USER_MANAGEMENT:
-		userMode = sdl::imtauth::AuthorizationOptions::UserMode::STRONG_USER_MANAGEMENT;
+		userMode = sdl::imtauth::AuthorizationOptions::UserManagementMode::STRONG_USER_MANAGEMENT;
 		break;
 
 	default:
