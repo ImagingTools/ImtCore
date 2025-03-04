acf59b73408fb1bd0f3a8d510de3a99ae57f9be6
Vladimir.Cherepanov <Vladimir.Cherepanov@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-03-04 05:58:20 +0000

    CCachedObjectCollectionComp: data meta info cache reverted
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@13240 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtbase/CCachedObjectCollectionComp.cpp
+++ Include/imtbase/CCachedObjectCollectionComp.cpp
@@ -263,8 +263,13 @@ QByteArray CCachedObjectCollectionComp::GetObjectTypeId(const Id& objectId) cons
 
 idoc::MetaInfoPtr CCachedObjectCollectionComp::GetDataMetaInfo(const Id& objectId) const
 {
-	if (m_objectCollectionCompPtr.IsValid()){
-		return m_objectCollectionCompPtr->GetDataMetaInfo(objectId);
+	QReadLocker locker(&m_lock);
+
+	for (int index = 0; index < m_cachedCollections.GetCount(); index++){
+		FilteredCollection* collectionChacheItemPtr = m_cachedCollections.GetAt(index);
+		if (collectionChacheItemPtr->cachePtr->GetElementIds().contains(objectId)){
+			return collectionChacheItemPtr->cachePtr->GetDataMetaInfo(objectId);
+		}
 	}
 
 	return idoc::MetaInfoPtr();
