415b4a1b68a10ee4073f56ec9aac459271ac9798
Vladimir.Cherepanov <Vladimir.Cherepanov@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-03-17 04:42:11 +0000

    Update cmake's from GMG
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@13333 21205d9f-8918-2240-a3d5-c39064a2c852

--- Config/CMake/OpenSSL.cmake
+++ Config/CMake/OpenSSL.cmake
@@ -1,35 +1,42 @@
-include_directories(${OPENSSLDIR})
+if (USE_FIND_PACKAGE)
+	find_package(OpenSSL)
+	target_link_libraries(${PROJECT_NAME} OpenSSL::Crypto OpenSSL::SSL)
+else()
 
-# Cross compile fix for WIN -> *NIX systems
-if (OPENSSLDIR)
-	string(REPLACE "\\" "/" OPENSSLDIR ${OPENSSLDIR})
-endif()
+	include_directories(${OPENSSLDIR})
 
-if(WIN32)
-	if(${CMAKE_CL_64} STREQUAL 1)
-		target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/x64/libcrypto.lib)
-		target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/x64/libssl.lib)
-	else()
-		target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/x86/libcrypto.lib)
-		target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/x86/libssl.lib)
-	endif()
-elseif(ANDROID)
-	target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/android/libcrypto_1_1.so)
-	target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/android/libssl_1_1.so)
-	set(ANDROID_EXTRA_LIBS
-		${OPENSSLDIR}/lib/android/libcrypto_1_1.so
-		${OPENSSLDIR}/lib/android/libssl_1_1.so
-		CACHE INTERNAL "")
-	message("ANDROID_EXTRA_LIBS ${ANDROID_EXTRA_LIBS}")
-elseif(APPLE)
-	if("${CMAKE_OSX_ARCHITECTURES}" STREQUAL "arm64")
-		target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/ClangOSX_arm64/libcrypto.a)
-	else()
-		target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/ClangOSX_x64/libcrypto.a)
+	# Cross compile fix for WIN -> *NIX systems
+	if (OPENSSLDIR)
+		string(REPLACE "\\" "/" OPENSSLDIR ${OPENSSLDIR})
 	endif()
 
-elseif(UNIX)
-	# target_link_libraries(${PROJECT_NAME} -lcrypto)
-	target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/linux/libcrypto.so)
+	if(WIN32)
+		if(${CMAKE_CL_64} STREQUAL 1)
+			target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/x64/libcrypto.lib)
+			target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/x64/libssl.lib)
+		else()
+			target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/x86/libcrypto.lib)
+			target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/x86/libssl.lib)
+		endif()
+	elseif(ANDROID)
+		target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/android/libcrypto_1_1.so)
+		target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/android/libssl_1_1.so)
+		set(ANDROID_EXTRA_LIBS
+			${OPENSSLDIR}/lib/android/libcrypto_1_1.so
+			${OPENSSLDIR}/lib/android/libssl_1_1.so
+			CACHE INTERNAL "")
+		message("ANDROID_EXTRA_LIBS ${ANDROID_EXTRA_LIBS}")
+	elseif(APPLE)
+		if("${CMAKE_OSX_ARCHITECTURES}" STREQUAL "arm64")
+			target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/ClangOSX_arm64/libcrypto.a)
+		else()
+			target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/ClangOSX_x64/libcrypto.a)
+		endif()
+
+	elseif(UNIX)
+		# target_link_libraries(${PROJECT_NAME} -lcrypto)
+		target_link_libraries(${PROJECT_NAME} ${OPENSSLDIR}/lib/linux/libcrypto.so)
+	endif()
+
 endif()
 
--- Config/CMake/Quazip.cmake
+++ Config/CMake/Quazip.cmake
@@ -1,13 +1,19 @@
-include_directories(${QUAZIP})
+if (USE_FIND_PACKAGE)
+	find_package(QuaZip REQUIRED)
+	find_package(ZLIB)
+	target_link_libraries(${PROJECT_NAME} QuaZip::QuaZip ZLIB::ZLIB)
+else()
+	include_directories(${QUAZIP})
 
-add_compile_definitions(QUAZIP_STATIC)
+	add_compile_definitions(QUAZIP_STATIC)
 
-if(WIN32)
-	target_link_libraries(${PROJECT_NAME} ${QUAZIP}/lib/${CMAKE_BUILD_TYPE}_${TARGETNAME}/quazip.lib)
+	if(WIN32)
+		target_link_libraries(${PROJECT_NAME} ${QUAZIP}/lib/${CMAKE_BUILD_TYPE}_${TARGETNAME}/quazip.lib)
+	endif()
+
+	if(UNIX)
+		target_link_libraries(${PROJECT_NAME} ${QUAZIP}/lib/${TARGETNAME}/libquazip.a)
+	endif()
+
+	include(${IACFDIR}/Config/CMake/zlib.cmake)
 endif()
-
-if(UNIX)
-	target_link_libraries(${PROJECT_NAME} ${QUAZIP}/lib/${TARGETNAME}/libquazip.a)
-endif()
-
-include(${IACFDIR}/Config/CMake/zlib.cmake)
