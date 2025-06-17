30bf0a2db1bc157bd560953cd7fdd007b7dd4f99
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-06-17 09:18:27 +0000

    imtlic Init.h corrected (???)
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@14112 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtlic/Init.h
+++ Include/imtlic/Init.h
@@ -6,14 +6,46 @@
 template <class ApplicationComposite, class AppInitializer, void (*ProductFiller)(imtlic::IProductInfo&) = nullptr, bool autoInit = false>
 int ProductFeatureRun(int argc, char* argv[])
 {
-	ApplicationComposite instance;
+	AppInitializer::Init();
 
-	imtlic::IProductInfo* productInfoPtr = instance.template GetInterface<imtlic::IProductInfo>();
-	if (productInfoPtr != nullptr && ProductFiller != nullptr){
-		ProductFiller(*productInfoPtr);
+	QDirIterator dirIter(":/Style");
+	while (dirIter.hasNext()){
+		qDebug() << dirIter.filePath();
+
+		dirIter.next();
 	}
 
-	return Run<ApplicationComposite, AppInitializer, autoInit>(argc, argv);
+	QDirIterator dirIter3(":/Icons");
+	while (dirIter3.hasNext()){
+		qDebug() << dirIter3.filePath();
+
+		dirIter3.next();
+	}
+
+	QDirIterator dirIter2(":/Styles");
+	while (dirIter2.hasNext()){
+		qDebug() << dirIter2.filePath();
+
+		dirIter2.next();
+	}
+
+	ApplicationComposite instance;
+
+	if (autoInit){
+		instance.EnsureAutoInitComponentsCreated();
+	}
+
+	ibase::IApplication* applicationPtr = instance.template GetInterface<ibase::IApplication>();
+	if (applicationPtr != nullptr){
+		imtlic::IProductInfo* productInfoPtr = instance.template GetInterface<imtlic::IProductInfo>();
+		if (productInfoPtr != nullptr && ProductFiller != nullptr){
+			ProductFiller(*productInfoPtr);
+		}
+
+		return applicationPtr->Execute(argc, argv);
+	}
+
+	return -1;
 }
 
 
