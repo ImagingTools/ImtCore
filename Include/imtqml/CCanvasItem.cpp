03419d25af4144c82810ed5b77b4b4391074ee2d
Alexander.Vechter <Alexander.Vechter@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-09-09 05:34:59 +0000

    ImtCore. imtqml/CCanvasItem added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@14774 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Include/imtqml/CCanvasItem.cpp
@@ -0,0 +1,300 @@
+#include <imtqml/CCanvasItem.h>
+
+// Qt includes
+#include <QtCore/QFile>
+#include <QtCore/QDebug>
+#include <QtGui/QPainter>
+#include <QtGui/QPen>
+
+
+namespace imtqml
+{
+
+CCanvasItem::CCanvasItem(QQuickItem *parent):QQuickPaintedItem(parent),
+	m_lineWidth(1), m_image(100,100,QImage::Format_ARGB32), m_pixmap(100,100), m_painterPath()
+{
+	//QObject::connect(this, SIGNAL(widthChanged()), this, SLOT(onWidthChanged()));
+	//QObject::connect(this, SIGNAL(heightChanged()), this, SLOT(onHeightChanged()));
+}
+
+
+void CCanvasItem::paint(QPainter *painter)
+{
+	painter->drawPixmap(0, 0, m_pixmap);
+	//qDebug() << "PAINT____________";
+}
+
+void CCanvasItem::stroke()
+{
+	if(!m_pixmap){
+		//qDebug() << "stroke_____returned!";
+		return;
+	}
+	QPainter painter(&m_pixmap);
+	painter.setFont(m_qfont);
+
+	// painter.setPen(QPen(QColor(m_strokeStyle), m_lineWidth, Qt::SolidLine,
+	// 					Qt::RoundCap, Qt::RoundJoin));
+
+	painter.setPen(QPen(QColor(m_strokeStyle), m_lineWidth, Qt::SolidLine,
+						Qt::FlatCap, Qt::MiterJoin));
+
+	painter.drawPath(m_painterPath);
+	//qDebug() << "stroke_____" << m_lineWidth << m_strokeStyle << m_pixmap.width() << m_pixmap.height();
+}
+
+void CCanvasItem::fill()
+{
+	if(!m_pixmap){
+		return;
+	}
+	QPainter painter(&m_pixmap);
+	painter.setFont(m_qfont);
+	painter.setBrush(QBrush(QColor(m_fillStyle)));
+	painter.drawPath(m_painterPath);
+
+}
+
+void CCanvasItem::beginPath()
+{
+	m_painterPath.clear();
+}
+
+void CCanvasItem::save()
+{
+
+}
+
+void CCanvasItem::restore()
+{
+
+}
+
+void CCanvasItem::clip()
+{
+
+}
+
+void CCanvasItem::requestPaint()
+{
+	emit paintRequired();
+}
+
+void CCanvasItem::closePath()
+{
+	//m_painterPath.clear();
+}
+
+QSize CCanvasItem::measureText(QString string)
+{
+	QFontMetrics fontMetrix(m_qfont);
+	int length = fontMetrix.horizontalAdvance(string);
+	return QSize(length, 0);
+}
+
+void CCanvasItem::arc(double x, double y, double radius, double startAngle, double endAngle, bool anticlockwise)
+{
+	if(!m_pixmap){
+		return;
+	}
+
+	double size = 2*radius;
+
+	m_painterPath.moveTo(x + radius, y);
+	m_painterPath.arcTo(x - radius, y - radius, size , size, 180 * startAngle / M_PI, 180 * endAngle / M_PI);
+}
+
+void CCanvasItem::fillRect(double x, double y, double width, double height)
+{
+	if(!m_pixmap){
+		return;
+	}
+	beginPath();
+	m_painterPath.addRect(x, y, width, height);
+	QPainter painter(&m_pixmap);
+
+	painter.setBrush(QBrush(QColor(m_fillStyle)));
+	painter.drawPath(m_painterPath);
+	closePath();
+
+}
+
+
+void CCanvasItem::moveTo(double x, double y)
+{
+	m_startPoint = QPointF(x,y);
+	m_painterPath.moveTo(x, y);
+	//qDebug() << "MOVE_TO:: " << x << y;
+}
+
+void CCanvasItem::lineTo(double x, double y)
+{
+	if(!m_pixmap){
+		return;
+	}
+	//qDebug()<< "LINE_TO:: " << x << " " << y;
+	m_painterPath.lineTo(x, y);
+}
+
+void CCanvasItem::fillText(QString text, double x, double y)
+{
+	if(!m_pixmap){
+		return;
+	}
+	QPainter painter(&m_pixmap);
+
+	// beginPath();
+	// m_painterPath.addText(x, y, font, text);
+	painter.setFont(m_qfont);
+	//painter.setBrush(QBrush(QColor(m_fillStyle)));
+
+	// painter.setPen(QPen(QColor(m_strokeStyle), m_lineWidth, Qt::SolidLine,
+	// 					Qt::RoundCap, Qt::RoundJoin));
+	painter.setPen(QPen(QColor(m_strokeStyle), m_lineWidth, Qt::SolidLine,
+						Qt::FlatCap, Qt::MiterJoin));
+	//painter.drawPath(m_painterPath);
+	// closePath();
+	painter.drawText(x, y, text);
+}
+
+void CCanvasItem::text(QString text, double x, double y)
+{
+	if(!m_pixmap){
+		return;
+	}
+
+	m_painterPath.addText(x, y, m_qfont, text);
+}
+
+
+
+void CCanvasItem::setPixmapSize(double width, double height)
+{
+	m_pixmap = QPixmap(width, height);
+}
+
+double CCanvasItem::lineWidth() const
+{
+	return m_lineWidth;
+}
+
+void CCanvasItem::setLineWidth(double newLineWidth)
+{
+	if (qFuzzyCompare(m_lineWidth, newLineWidth))
+		return;
+	m_lineWidth = newLineWidth;
+	emit lineWidthChanged();
+}
+
+void CCanvasItem::onWidthChanged()
+{
+	//qDebug() << "WIDTH_CHANGED____" << width();
+	//m_pixmap = QPixmap(width(), height());
+	//draw();
+	//drawRect();
+}
+
+void CCanvasItem::onHeightChanged()
+{
+	//qDebug() << "HEIGHT_CHANGED____" << height();
+	//m_pixmap = QPixmap(width(), height());
+	//draw();
+	//drawRect();
+}
+
+QString CCanvasItem::fillStyle() const
+{
+	return m_fillStyle;
+}
+
+void CCanvasItem::setFillStyle(const QString &newFillStyle)
+{
+	if (m_fillStyle == newFillStyle)
+		return;
+	m_fillStyle = newFillStyle;
+	emit fillStyleChanged();
+}
+
+QString CCanvasItem::strokeStyle() const
+{
+	return m_strokeStyle;
+}
+
+void CCanvasItem::setStrokeStyle(const QString &newStrokeStyle)
+{
+	if (m_strokeStyle == newStrokeStyle)
+		return;
+	m_strokeStyle = newStrokeStyle;
+	emit strokeStyleChanged();
+}
+
+void CCanvasItem::draw(/*QPainter *painter*/)
+{
+	//qDebug() << "DRAW____";
+}
+
+
+
+QString CCanvasItem::font() const
+{
+	return m_font;
+}
+
+void CCanvasItem::setFont(const QString &newFont)
+{
+	if (m_font == newFont)
+		return;
+	m_font = newFont;
+	emit fontChanged();
+
+	int pixelSize = m_font.section("px ",0, 0).toInt();
+	QString fontFamily = m_font.section("px ",1, 1);
+	m_qfont.setPixelSize(pixelSize);
+	m_qfont.setFamily(fontFamily);
+}
+
+QString CCanvasItem::lineCap() const
+{
+	return m_lineCap;
+}
+
+void CCanvasItem::setLineCap(const QString &newLineCap)
+{
+	if (m_lineCap == newLineCap)
+		return;
+	m_lineCap = newLineCap;
+	emit lineCapChanged();
+}
+
+QString CCanvasItem::lineJoin() const
+{
+	return m_lineJoin;
+}
+
+void CCanvasItem::setLineJoin(const QString &newLineJoin)
+{
+	if (m_lineJoin == newLineJoin)
+		return;
+	m_lineJoin = newLineJoin;
+	emit lineJoinChanged();
+}
+
+double CCanvasItem::globalAlpha() const
+{
+	return m_globalAlpha;
+}
+
+void CCanvasItem::setGlobalAlpha(double newGlobalAlpha)
+{
+	if (qFuzzyCompare(m_globalAlpha, newGlobalAlpha))
+		return;
+	m_globalAlpha = newGlobalAlpha;
+	emit globalAlphaChanged();
+}
+
+
+
+
+} // namespace imtqml
+
+
---
+++ Include/imtqml/CCanvasItem.h
@@ -0,0 +1,119 @@
+#pragma once
+
+
+// Qt includes
+#include <QtCore/QObject>
+#include <QtCore/QByteArray>
+#include <QtQuick/QQuickPaintedItem>
+#include <QtGui/QImage>
+#include <QtGui/QPixmap>
+#include <QtGui/QPainter>
+#include <QtGui/QPainterPath>
+#include <QtGui/QFont>
+#include <QtGui/QFontMetrics>
+
+#include <istd/TDelPtr.h>
+
+
+namespace imtqml
+{
+
+
+class CCanvasItem : public QQuickPaintedItem
+{
+	Q_OBJECT
+
+	Q_PROPERTY(double lineWidth READ lineWidth WRITE setLineWidth NOTIFY lineWidthChanged)
+	Q_PROPERTY(QString fillStyle READ fillStyle WRITE setFillStyle NOTIFY fillStyleChanged)
+	Q_PROPERTY(QString strokeStyle READ strokeStyle WRITE setStrokeStyle NOTIFY strokeStyleChanged)
+	Q_PROPERTY(QString font READ font WRITE setFont NOTIFY fontChanged)
+	Q_PROPERTY(QString lineCap READ lineCap WRITE setLineCap NOTIFY lineCapChanged)
+	Q_PROPERTY(QString lineJoin READ lineJoin WRITE setLineJoin NOTIFY lineJoinChanged)
+	Q_PROPERTY(double globalAlpha READ globalAlpha WRITE setGlobalAlpha NOTIFY globalAlphaChanged)
+
+public:
+	CCanvasItem(QQuickItem *parent = nullptr);
+	Q_INVOKABLE void paint(QPainter *painter);
+	Q_INVOKABLE void draw(/*QPainter *painter*/);
+	Q_INVOKABLE void fillRect(double x, double y, double width, double height);
+	Q_INVOKABLE void moveTo(double x, double y);
+	Q_INVOKABLE void lineTo(double x, double y);
+	Q_INVOKABLE void fillText(QString text,double x, double y);
+	Q_INVOKABLE void text(QString text,double x, double y);
+	Q_INVOKABLE void arc(double x, double y, double radius, double startAngle, double endAngle, bool anticlockwise);
+	Q_INVOKABLE void setPixmapSize(double width, double height);
+	Q_INVOKABLE QSize measureText(QString string);
+	Q_INVOKABLE void stroke();
+	Q_INVOKABLE void fill();
+	Q_INVOKABLE void beginPath();
+	Q_INVOKABLE void closePath();
+	Q_INVOKABLE void save();
+	Q_INVOKABLE void restore();
+	Q_INVOKABLE void clip();
+	Q_INVOKABLE void requestPaint();
+;
+
+	double lineWidth() const;
+	void setLineWidth(double newLineWidth);
+
+	QString fillStyle() const;
+	void setFillStyle(const QString &newFillStyle);
+
+	QString strokeStyle() const;
+	void setStrokeStyle(const QString &newStrokeStyle);
+
+	QString font() const;
+	void setFont(const QString &newFont);
+
+	QString lineCap() const;
+	void setLineCap(const QString &newLineCap);
+
+	QString lineJoin() const;
+	void setLineJoin(const QString &newLineJoin);
+
+	double globalAlpha() const;
+	void setGlobalAlpha(double newGlobalAlpha);
+
+Q_SIGNALS:
+	void paintRequired();
+
+	void lineWidthChanged();
+
+	void fillStyleChanged();
+
+	void strokeStyleChanged();
+
+	void fontChanged();
+
+	void lineCapChanged();
+
+	void lineJoinChanged();
+
+	void globalAlphaChanged();
+
+public slots:
+	void onWidthChanged();
+	void onHeightChanged();
+
+private:
+	QImage m_image;
+	QPixmap m_pixmap;
+	double m_lineWidth;
+	QPointF m_startPoint;
+	QString m_fillStyle;
+	QPainterPath m_painterPath;
+	QString m_strokeStyle;
+	QString m_font;
+	QFont m_qfont;
+	QString m_lineCap;
+	QString m_lineJoin;
+	double m_globalAlpha;
+};
+
+
+} // namespace imtqml
+
+
+Q_DECLARE_METATYPE(imtqml::CCanvasItem*)
+
+
