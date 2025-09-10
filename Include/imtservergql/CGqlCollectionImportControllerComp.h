eac8bfb73a11d0b1e3c6d3d5663cabbd0c5bba32
Vladimir.Cherepanov <Vladimir.Cherepanov@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-09-10 03:40:19 +0000

    Working on collection import
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@14786 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtservergql/CGqlCollectionImportControllerComp.h
+++ Include/imtservergql/CGqlCollectionImportControllerComp.h
@@ -52,6 +52,64 @@ private:
 };
 
 
+template<typename GqlRequest>
+bool CGqlCollectionImportControllerComp::FillSessionInfo(
+	const GqlRequest& request,
+	imtservergql::ICollectionImportController::SessionInfo& sessionInfo,
+	QString& errorMessage)
+{
+	auto sdlImportParams = request.GetRequestedArguments().input;
+	if (sdlImportParams.Version_1_0){
+		auto sdlImportParamsV1_0 = *sdlImportParams.Version_1_0;
+
+		if (sdlImportParamsV1_0.sessionId && sdlImportParamsV1_0.collectionId && sdlImportParamsV1_0.fileList && sdlImportParamsV1_0.fileList->count() > 0){
+			sessionInfo.sessionId = *sdlImportParamsV1_0.sessionId;
+			sessionInfo.collectionId = *sdlImportParamsV1_0.collectionId;
+
+			for (const auto& fileInfo : *sdlImportParamsV1_0.fileList){
+				if (fileInfo.fileId && fileInfo.fileName && fileInfo.fileSize && fileInfo.objectTypeId){
+					sessionInfo.files.emplace_back();
+					sessionInfo.files.back().id = *fileInfo.fileId;
+					sessionInfo.files.back().name = *fileInfo.fileName;
+					sessionInfo.files.back().size = *fileInfo.fileSize;
+					sessionInfo.files.back().objectTypeId = *fileInfo.objectTypeId;
+				}
+				else{
+					errorMessage = tr("Invalid file info in the request");
+
+					return false;
+				}
+			}
+
+			return true;
+		}
+		else{
+			errorMessage = tr("Invalid import request");
+		}
+	}
+	else{
+		errorMessage = tr("Invalid import request");
+	}
+
+	return false;
+}
+
+
+template<typename GqlRequest>
+QByteArray CGqlCollectionImportControllerComp::GetCancelSessionId(
+	const GqlRequest& request)
+{
+	auto sdlSessionId = request.GetRequestedArguments().input;
+	if (sdlSessionId.Version_1_0 && sdlSessionId.Version_1_0->sessionId){
+		QByteArray sessionId = *sdlSessionId.Version_1_0->sessionId;
+
+		return sessionId;
+	}
+
+	return QByteArray();
+}
+
+
 } // namespace imtservergql
 
 
