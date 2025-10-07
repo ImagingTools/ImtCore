1b60cf61a86d803d36502ff41e9e5444814fc48f
Artur.Menshikov <Artur.Menshikov@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-10-07 04:25:42 +0000

    JQ. update alias
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@15002 21205d9f-8918-2240-a3d5-c39064a2c852

--- 3rdParty/JQ/QtQml/Alias.js
+++ 3rdParty/JQ/QtQml/Alias.js
@@ -11,7 +11,7 @@ class Alias extends Property {
      * @returns {Object}
      */
     static simpleGet(target, name, meta){      
-        return target[name].getter.call(target)
+        return this.getter(target, name)
     }
 
     /**
@@ -21,7 +21,7 @@ class Alias extends Property {
      * @param {Object} meta
      */
     static simpleSet(target, name, value, meta){
-        let oldValue = target[name].getter.call(target)
+        let oldValue = this.getter(target, name)
         let currentValue = oldValue
 
         if(typeof value === 'function'){
@@ -50,32 +50,61 @@ class Alias extends Property {
      */
     static init(target, name, func, propName){
         target.__self[name] = {
-            getter: function(){   
-                let obj = func()
-                if(!target.__self[name].connected){
-                    target.__self[name].connected = true
-                    if(obj instanceof QBaseObject){
-                        Signal.get(obj, propName+'Changed').connect((oldValue, newValue)=>{
-                            Signal.get(target, name+'Changed')(oldValue, newValue)
-                        })
-                    }
-                }       
-                return obj[propName]
-            },
-            setter: function(val){
-                let obj = func()
-                if(!target.__self[name].connected){
-                    target.__self[name].connected = true
-                    if(obj instanceof QBaseObject){
-                        Signal.get(obj, propName+'Changed').connect((oldValue, newValue)=>{
-                            Signal.get(target, name+'Changed')(oldValue, newValue)
-                        })
-                    }
-                } 
-                obj[propName] = val
-            },
+
+            // getter: function(){   
+            //     let obj = func()
+            //     if(!target.__self[name].connected){
+            //         target.__self[name].connected = true
+            //         if(obj instanceof QBaseObject){
+            //             Signal.get(obj, propName+'Changed').connect((oldValue, newValue)=>{
+            //                 Signal.get(target, name+'Changed')(oldValue, newValue)
+            //             })
+            //         }
+            //     }       
+            //     return obj[propName]
+            // },
+            // setter: function(val){
+            //     let obj = func()
+            //     if(!target.__self[name].connected){
+            //         target.__self[name].connected = true
+            //         if(obj instanceof QBaseObject){
+            //             Signal.get(obj, propName+'Changed').connect((oldValue, newValue)=>{
+            //                 Signal.get(target, name+'Changed')(oldValue, newValue)
+            //             })
+            //         }
+            //     } 
+            //     obj[propName] = val
+            // },
+            func: func,
+            propName: propName,
             connected: false,
         }
+    }
+
+    static prepare(target, name, obj, propName){
+        if(!target.__self[name].connected){
+            target.__self[name].connected = true
+            if(obj instanceof QBaseObject){
+                Signal.get(obj, propName+'Changed').connect((oldValue, newValue)=>{
+                    Signal.get(target, name+'Changed')(oldValue, newValue)
+                })
+            }
+        } 
+    }
+    static getter(target, name){
+        let obj = target.__self[name].func()
+        this.prepare(target, name, obj, target.__self[name].propName)
+        return obj[target.__self[name].propName]
+    }
+    static setter(target, name, value){
+        let obj = target.__self[name].func()
+        this.prepare(target, name, obj, target.__self[name].propName)
+        obj[target.__self[name].propName] = value
+    }
+    static resetter(target, name){
+        let obj = target.__self[name].func()
+        this.prepare(target, name, obj, target.__self[name].propName)
+        obj[target.__self[name].propName] = obj[target.__self[name].propName]
     }
 
     /**
@@ -113,7 +142,18 @@ class Alias extends Property {
             
         }
         
-        return target[name].getter.call(target)
+        return this.getter(target, name)
+    }
+
+    /**
+     * @param {Object} target 
+     * @param {String} name
+     * @param {*} value
+     * @param {Object} meta
+     */
+    static reset(target, name, value, meta){
+        this.resetter(target, name)
+        return super.reset(target, name, value, meta)
     }
 
     /**
@@ -123,7 +163,7 @@ class Alias extends Property {
      * @param {Object} meta
      */
     static set(target, name, value, meta){
-        let oldValue = target[name].getter.call(target)
+        let oldValue = this.getter(target, name)
         let currentValue = oldValue
 
         if(typeof value === 'function'){
@@ -145,7 +185,7 @@ class Alias extends Property {
         }  
 
         if(oldValue !== currentValue){
-            target[name].setter.call(target, value)
+            this.setter(target, name, value)
             // Signal.get(target, name + 'Changed')(oldValue, currentValue)
         }
 
