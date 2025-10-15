7ebd50cf8dda3bb863bb823d81b717135067fe0b
Artur.Menshikov <Artur.Menshikov@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-10-15 12:24:19 +0000

    JQ. update BaseClass (test)
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@15093 21205d9f-8918-2240-a3d5-c39064a2c852

--- 3rdParty/JQ/QtQml/BaseClass.js
+++ 3rdParty/JQ/QtQml/BaseClass.js
@@ -264,6 +264,8 @@ class BaseClass extends QtObject {
 		let isFirst = true
 		for (let i = 0; i < list.length; i++) {
 			let key = list[i]
+			if(key === '__typename' && this[key] === '') continue
+			
 			if (this[key] == null && this._internal.containceInRemoved(key)){
 				continue
 			}
