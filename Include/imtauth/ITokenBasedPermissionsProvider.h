f5bd8de64930dec04b2d52cc935374514bb428ab
kl <kl@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-12-01 20:01:23 +0000

    Comments added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@15373 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtauth/ITokenBasedPermissionsProvider.h
+++ Include/imtauth/ITokenBasedPermissionsProvider.h
@@ -9,9 +9,15 @@ namespace imtauth
 {
 
 
+/**
+	Interface for getting the list of permission-IDs based on an access token.
+*/
 class ITokenBasedPermissionsProvider: virtual public istd::IPolymorphic
 {
 public:
+	/**
+		Get the permissions for a given access token.
+	*/
 	virtual const QByteArrayList GetPermissions(const QByteArray& token) const = 0;
 };
 
