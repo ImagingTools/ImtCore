06aeb917ef8def55d2c5f9aa654a12389831f382
Vladimir.Cherepanov <Vladimir.Cherepanov@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-12-17 13:39:16 +0000

    IDocumentManager comments added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@15529 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtdoc/IDocumentManager.h
+++ Include/imtdoc/IDocumentManager.h
@@ -94,9 +94,21 @@ public:
 	static const QByteArray CN_DOCUMENT_CLOSED;
 	typedef DocumentClosedNotification DocumentClosedInfo;
 
+	/**
+		Get a list of open document instances for a given user-ID
+	*/
 	virtual DocumentList GetOpenedDocumentList(const QByteArray& userId) const = 0;
+
+	/**
+		Create a document of the given type (documentTypeId) for the given user-ID
+		/return		Document instance ID in the document manager
+	*/
 	virtual QByteArray CreateNewDocument(const QByteArray& userId, const QByteArray& documentTypeId) = 0;
-	/*
+
+	/**
+		Open a document from a given URL for a given user-ID
+		/return		Document instance ID in the document manager
+
 		From file:
 			file:///etc/fstab					- *nix style path
 			file:///c:/pagefile.sys				- Windows style path
@@ -104,16 +116,56 @@ public:
 			collection:///objectId				- for single collection document manager (or default collection)
 	*/
 	virtual QByteArray OpenDocument(const QByteArray& userId, const QUrl& url) = 0;
+
+	/**
+		Get name of the document with the given user-ID and document-ID
+	*/
 	virtual OperationStatus GetDocumentName(const QByteArray& userId, const QByteArray& documentId, QString& documentName) const = 0;
+
+	/**
+		Set name of the document with the given user-ID and document-ID
+	*/
 	virtual OperationStatus SetDocumentName(const QByteArray& userId, const QByteArray& documentId, const QString& documentName) = 0;
+
+	/**
+		Get document data pointer with the given user-ID and document-ID
+	*/
 	virtual const istd::IChangeable* GetDocumentPtr(const QByteArray& userId, const QByteArray& documentId) const = 0;
+
+	/**
+		Get document data with the given user-ID and document-ID
+	*/
 	virtual OperationStatus GetDocumentData(const QByteArray & userId, const QByteArray & documentId, istd::IChangeableSharedPtr& documentPtr) const = 0;
+
+	/**
+		Set document data with the given user-ID and document-ID
+	*/
 	virtual OperationStatus SetDocumentData(const QByteArray& userId, const QByteArray& documentId, const istd::IChangeable& document) = 0;
+
+	/**
+		Save document with the given user-ID and document-ID
+	*/
 	virtual OperationStatus SaveDocument(const QByteArray& userId, const QByteArray& documentId, const QString& documentName) = 0;
+
+	/**
+		Close document with the given user-ID and document-ID
+	*/
 	virtual OperationStatus CloseDocument(const QByteArray& userId, const QByteArray& documentId) = 0;
+
+	/**
+		Get UndoManager for the document with the given user-ID and document-ID
+	*/
 	virtual OperationStatus GetDocumentUndoManager(
 		const QByteArray& userId, const QByteArray& documentId, idoc::IUndoManager*& undoManagerPtr) const = 0;
+
+	/**
+		Register an obserer for the document with the given user-ID and document-ID
+	*/
 	virtual OperationStatus RegisterDocumentObserver(const QByteArray& userId, const QByteArray& documentId, imod::IObserver& observer) = 0;
+
+	/**
+		Unregister an obserer for the document with the given user-ID and document-ID
+	*/
 	virtual OperationStatus UnregisterDocumentObserver(const QByteArray& userId, const QByteArray& documentId, imod::IObserver& observer) = 0;
 };
 
