fc98bc9e1a7125621039906b7ec8b91a6b805a11
Sergej.Zastrovnyh <Sergej.Zastrovnyh@21205d9f-8918-2240-a3d5-c39064a2c852>
2026-01-10 11:15:34 +0000

    ImtCore: imtbase: narrow_cast added
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@15606 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/imtbase/imtbase.h
+++ Include/imtbase/imtbase.h
@@ -1,6 +1,9 @@
 #pragma once
 
 
+// C++ includes
+#include <limits>
+
 // Qt includes
 #include <QtCore/QList>
 
@@ -13,6 +16,27 @@
 namespace imtbase
 {
 
+/**
+	\brief This is a similar implementation from the Guidelines Support Library (GSL), which is used deliberately for explicit type narrowing. 
+	 \details 
+		Meaning "I know thatthere is a narrowing here, and I take responsibility for myself" 
+		Makes a clear narrowing
+		DOES NOT check for overflow (in release mode)
+		Just a static_cast, but semantically expressive 
+		\note Use it carefully. Only for old code or 3rd party functions which can't be changed 
+		\code{.cpp} 
+			QList<int> list; 
+			int numberOfElements = list.size(); ///< generates warning 
+			int iNumberOfElements = narrow_cast<int>(list.size()); ///< no warning => explicit narrowing 
+		\endcode
+*/
+template <class T, class U>
+constexpr T narrow_cast(U v) noexcept
+{
+	// Q_ASSERT(v <= std::numeric_limits<T>::max());
+
+	return static_cast<T>(v);
+}
 
 /**
 	\todo Move to imtrest
