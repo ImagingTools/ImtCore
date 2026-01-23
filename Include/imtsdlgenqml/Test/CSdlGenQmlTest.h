c63ffaae1b70ae82e9d158384f705be81d3497fb
Sergey Zastrovnyh <sergeizastrovnyh@yandex.com>
2026-01-23 09:26:50 +0600

    SDL-QML test added

---
+++ Include/imtsdlgenqml/Test/CSdlGenQmlTest.h
@@ -0,0 +1,33 @@
+#pragma once
+
+
+// Qt includes
+#include <QtCore/QObject>
+#include <QtCore/QDir>
+
+// ImtCore includes
+#include <imtsdl/ISdlEditableProcessArgumentsParser.h>
+
+
+namespace imtsdlgenqmltest
+{
+
+
+class CSdlGenQmlTest : public QObject
+{
+	Q_OBJECT
+
+private Q_SLOTS:
+	void initTestCase();
+
+	void TestComplexCollectionFilter();
+
+	void cleanupTestCase();
+
+private:
+	QDir m_tempOutputDir;
+
+};
+
+
+} //namespace imtsdlgenqmltest
