69cce635e1a6d0d6cdd2535b2d186ca96178522c
Roman Kucherenko <kucherenkord@gmail.com>
2026-03-24 10:29:15 +0600

    Tree model optimization

--- Include/imtservergql/CHttpGraphQLServletComp.cpp
+++ Include/imtservergql/CHttpGraphQLServletComp.cpp
@@ -189,8 +189,8 @@ imtrest::ConstResponsePtr CHttpGraphQLServletComp::OnPost(
 
 			dataModelPtr->SetExternTreeModel(gqlCommand,
 											 sourceData ?
-												 sourceData->CopyMe() :
-												 sourceItemModelPtr->CopyMe());
+												 sourceData :
+												 sourceItemModelPtr.GetPtr());
 
 			iser::CJsonMemWriteArchive archive(nullptr, false);
 			if (rootModel.SerializeModel(archive)) {
