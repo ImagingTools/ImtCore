14df46b1a2b117b04373b201f5ad755e40b5b9e3
Vladislav Krishkevich <mail.mr.vlad@me.com>
2026-07-16 21:56:42 +0600

    restored an include in ImtCore cmake for legacy mode

--- Config/CMake/ImtCore.cmake
+++ Config/CMake/ImtCore.cmake
@@ -2,3 +2,7 @@ if(WIN32)
 	target_link_libraries(${PROJECT_NAME} ${ACF_LIBRARY_LINK_SCOPE} advapi32 user32)
 endif()
 
+if(NOT ACF_MODERN_CMAKE)
+	include(${ACFDIR}/Config/CMake/AcfStd.cmake)
+endif()
+
