b178bcc8ee4d795ce0b65e0b8fd1d8dc2a1e4118
Sergey Zastrovnyh <sergeizastrovnyh@yandex.com>
2026-08-20 16:04:42 +0600

    Refactor CMake target_link_libraries to use PRIVATE scope
    - Updated multiple CMakeLists.txt files across various projects to change the target_link_libraries from using ${ACF_PACKAGE_LINK_SCOPE} and ${ACF_APPLICATION_LINK_SCOPE} to PRIVATE. This change enhances encapsulation by limiting the visibility of linked libraries to the target itself, preventing unintended linkage in dependent targets.

--- Config/CMake/ImtCore.cmake
+++ Config/CMake/ImtCore.cmake
@@ -1,5 +1,5 @@
 if(WIN32)
-	target_link_libraries(${PROJECT_NAME} ${ACF_LIBRARY_LINK_SCOPE} advapi32 user32)
+	target_link_libraries(${PROJECT_NAME} PUBLIC advapi32 user32)
 endif()
 
 if(NOT ACF_MODERN_CMAKE)
